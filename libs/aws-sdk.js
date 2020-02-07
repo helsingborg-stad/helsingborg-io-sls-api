5049d0d18236e33537d7daba7b9b9292c445b88c
Dan <mail@dannilsson.se>
2020-02-07 15:36:42 +0100

    fix missing

--- libs/aws-sdk.js
+++ libs/aws-sdk.js
@@ -2,6 +2,6 @@ import aws from 'aws-sdk';
 
 const awsWrapped = process.env.IS_LOCAL
   ? aws
-  : aws // Can be something other productions stuff wrapped around aws
+  : aws; // Can be something other productions stuff wrapped around aws
 
 export default awsWrapped;
