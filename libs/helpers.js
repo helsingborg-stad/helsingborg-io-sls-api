21726cf4c5be6f63cd186ac6424c45c76c135b4d
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-02-13 21:45:21 +0100

    refactor to helper

--- libs/helpers.js
+++ libs/helpers.js
@@ -1,4 +1,4 @@
 export const to = (promise) =>
   promise
-    .then(response => ({ ok: true, response }))
-    .catch(error => Promise.resolve({ ok: false, error }));
+    .then(response => ({ ok: true, result: response }))
+    .catch(error => Promise.resolve({ ok: false, result: error }));
