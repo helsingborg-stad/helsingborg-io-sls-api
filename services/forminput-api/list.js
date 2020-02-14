4066436ab29e5372cbaea82a72e8e794c626d0b4
Dan <mail@dannilsson.se>
2020-02-14 17:09:28 +0100

    list database by userId

--- services/forminput-api/list.js
+++ services/forminput-api/list.js
@@ -1,9 +1,29 @@
-// list (GET)
-export const main = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ data: 'LIST' }),
+import logger from '@financial-times/lambda-logger';
+import * as dynamoDb from '../../libs/dynamoDb';
+import { success, failure } from '../../libs/response';
+import { to } from '../../libs/helpers';
+
+// list items (GET)
+export const main = async (event, context) => {
+  const { userId } = event.queryStringParameters;
+
+  const params = {
+    TableName: 'submissions',
+    KeyConditionExpression: 'userId = :user',
+    ExpressionAttributeValues: { ':user': userId },
   };
 
-  callback(null, response);
+  const { ok, result } = await to(dynamoDb.call('query', params));
+  logger.info(result);
+
+  if (!ok) {
+    logger.error(result);
+    return failure({ status: ok, error: result });
+  }
+
+  if (result.Items.length < 1) {
+    return failure({ status: false, error: 'Items not found.' });
+  }
+
+  return success(result.Items);
 };
