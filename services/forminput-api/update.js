8b5eca90299eca34bef26f038316be172273c11b
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-02-17 10:01:02 +0100

    add update

--- services/forminput-api/update.js
+++ services/forminput-api/update.js
@@ -1,9 +1,35 @@
+import logger from '@financial-times/lambda-logger';
+import * as dynamoDb from '../../libs/dynamoDb';
+import { success, failure } from '../../libs/response';
+import { to } from '../../libs/helpers';
+
 // update (PUT)
-export const main = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ body: 'UPDATED!' }),
+export const main = async (event, context) => {
+  const data = JSON.parse(event.body);
+
+  const { userId } = event.queryStringParameters;
+  const { submissionId } = event.pathParameters;
+
+  const params = {
+    TableName: 'submissions',
+    Key: {
+      userId,
+      submissionId,
+    },
+    UpdateExpression: 'SET answers = :answers, modified = :modified',
+    ExpressionAttributeValues: {
+      ':answers': data.answers || null,
+      ':modified': Date.now(),
+    },
+    ReturnValues: 'ALL_NEW',
   };
 
-  callback(null, response);
+  const { ok, result } = await to(dynamoDb.call('update', params));
+
+  if (!ok) {
+    logger.error(result);
+    return failure({ status: ok, error: result });
+  }
+
+  return success({ status: true });
 };
