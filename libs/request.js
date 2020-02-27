6c750061cca9c7439c5dde3a33ca633581028bec
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-02-27 07:48:19 +0100

    linting

--- libs/request.js
+++ libs/request.js
@@ -4,13 +4,15 @@ import https from 'https';
 export const requestClient = (
   options,
   timeout = 5000,
-  contentType = "application/json"
-) => axios.create({
-  httpsAgent: new https.Agent({ ...options }),
+  contentType = 'application/json',
+) =>
+  axios.create({
+    httpsAgent: new https.Agent({ ...options }),
     headers: {
-      "Content-Type": contentType
+      'Content-Type': contentType,
     },
-    timeout
-});
+    timeout,
+  });
 
-export const call = (client, method, url, payload) => client[method](url,payload);
+export const call = (client, method, url, payload) =>
+  client[method](url, payload);
