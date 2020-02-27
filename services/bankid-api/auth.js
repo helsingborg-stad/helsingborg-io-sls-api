cdd87917ad234c8a6df1705a57248714bbd367f5
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-02-27 07:49:14 +0100

    refactor using to

--- services/bankid-api/auth.js
+++ services/bankid-api/auth.js
@@ -1,24 +1,27 @@
 import { failure, success } from '../../libs/response';
+import { to } from '../../libs/helpers';
 import * as request from '../../libs/request';
 import * as bankId from './helpers/bankId';
 
-export const main = async (event) => {
-  try {
-    const { endUserIp, personalNumber } = JSON.parse(event.body);
+export const main = async event => {
+  const payload = {
+    endUserIp: '127.0.0.1',
+    personalNumber: '195611260629',
+  };
 
-    const payload = { endUserIp, personalNumber };
+  const [bOk, bankIdClient] = await to(bankId.client());
 
-    const bankIdClient = await bankId.client();
-
-    const data = await request.call(
-      bankIdClient,
-      'post',
-      bankId.url('/auth'),
-      payload
-    );
-
-    return success({ status: true, body: data });
-  } catch (error) {
-    return failure({ status: false });
+  if (!bOk) {
+    return failure({ status: false, error: bankIdClient });
   }
+
+  const [rOk, data] = await to(
+    request.call(bankIdClient, 'post', bankId.url('/auth'), payload),
+  );
+
+  if (!rOk) {
+    return failure({ status: false, error: data });
+  }
+
+  return success({ status: true, body: data });
 };
