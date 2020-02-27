b95ffc98615ea5ddd59a499291df0d2fe6096d49
Dan <mail@dannilsson.se>
2020-02-27 07:57:32 +0100

    get bucket name from env

--- services/bankid-api/helpers/bankId.js
+++ services/bankid-api/helpers/bankId.js
@@ -2,16 +2,22 @@ import { requestClient } from '../../../libs/request';
 import * as certs from './certificates';
 
 const {
-  // BANKID_API_URL,
+  BANKID_API_URL,
   BANKID_PASSPHRASE,
 } = process.env;
 
-// export const url = path => `${BANKID_API_URL}${path}`;
+export const url = path => `${BANKID_API_URL}${path}`;
 
 export const client = async () => {
   try {
-    const bankIdCa = await certs.read(`helsingborg-io-sls-certificates-dev`, 'bankd.crt');
-    const bankIdPfx = await certs.read(`helsingborg-io-sls-certificates-dev`, 'FPTestcert2.pfx');
+    const bankIdCa = await certs.read(
+      `${process.env.resourcesStage}-CertificatesBucket`,
+      'bankd.crt',
+    );
+    const bankIdPfx = await certs.read(
+      `${process.env.resourcesStage}-CertificatesBucket`,
+      'FPTestcert2.pfx',
+    );
 
     const clientOptions = {
       ca: bankIdCa.Body,
@@ -19,8 +25,9 @@ export const client = async () => {
       passphrase: BANKID_PASSPHRASE,
       rejectUnauthorized: false
     };
+
     return requestClient(clientOptions);
   } catch (error) {
-    console.log(error);
+    throw error;
   }
 };
