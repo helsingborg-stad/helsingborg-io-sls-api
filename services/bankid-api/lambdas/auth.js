76981f6e2e3836e3c1f90e5937794a087d680759
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-03-02 16:06:03 +0100

    move lambdas to new folder called lambdas

--- services/bankid-api/lambdas/auth.js
+++ services/bankid-api/lambdas/auth.js
@@ -1,7 +1,7 @@
-import { failure, success } from '../../libs/response';
-import { to } from '../../libs/helpers';
-import * as request from '../../libs/request';
-import * as bankId from './helpers/bankId';
+import { failure, success } from '../../../libs/response';
+import { to } from '../../../libs/helpers';
+import * as request from '../../../libs/request';
+import * as bankId from '../helpers/bankId';
 
 export const main = async event => {
   const { endUserIp, personalNumber } = JSON.parse(event.body);
