e8eb35232119e7b469b8f042066f6e86052cbac6
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-03-03 11:15:55 +0100

    make linter happy

--- services/bankid-api/helpers/ssmParameters.js
+++ services/bankid-api/helpers/ssmParameters.js
@@ -1,22 +1,22 @@
-import AWS from "aws-sdk";
+import AWS from 'aws-sdk';
 
 const ssm = new AWS.SSM();
 
-export const getConfig = (name) =>
+export const getConfig = name =>
   new Promise((resolve, reject) => {
     ssm.getParameter(
       {
         Name: name,
-        WithDecryption: true
+        WithDecryption: true,
       },
       (err, data) => {
         if (err) {
           console.log(err, err.stack);
           reject(err);
         } else {
-          const param = JSON.parse(data.Parameter.Value)
+          const param = JSON.parse(data.Parameter.Value);
           resolve(param);
         }
-      }
+      },
     );
   });
