d5fc3be6b00909cff5fbc3c34bb28e69043bdb82
Teddy Andersson <teddyandersson95@gmail.com>
2020-03-03 10:28:39 +0100

    Implementation of ssm-params as enviorment vars

--- services/bankid-api/lambdas/auth.js
+++ services/bankid-api/lambdas/auth.js
@@ -2,8 +2,13 @@ import { failure, success } from '../../../libs/response';
 import { to } from '../../../libs/helpers';
 import * as request from '../../../libs/request';
 import * as bankId from '../helpers/bankId';
+import { getConfig } from '../helpers/ssmParameters';
+
+const SSMParams = getConfig("/bankidEnvs/dev");
 
 export const main = async event => {
+  const bankidSSMparams = await SSMParams;
+
   const { endUserIp, personalNumber } = JSON.parse(event.body);
 
   const payload = {
@@ -11,19 +16,20 @@ export const main = async event => {
     personalNumber,
   };
 
-  const [bankIdOk, bankIdClient] = await to(bankId.client());
+  const [bankIdOk, bankIdClient] = await to(bankId.client(bankidSSMparams));
+
 
   if (!bankIdOk) {
     return failure({ status: false, error: bankIdClient });
   }
 
-  const [dataOk, { data }] = await to(
-    request.call(bankIdClient, 'post', bankId.url('/auth'), payload),
+  const [dataOk, response] = await to(
+    request.call(bankIdClient, 'post', bankId.url(bankidSSMparams.apiUrl, '/auth'), payload),
   );
 
   if (!dataOk) {
-    return failure({ status: false });
+    return failure({ status: false, error: response });
   }
 
-  return success({ status: true, body: data });
+  return success({ status: true, body: response.data });
 };
