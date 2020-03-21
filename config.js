d0ecd397dc8753246e3ea490cfaecff2fc59d61f
Dan Nilsson <mail@dannilsson.se>
2020-03-22 00:27:06 +0100

    add bankid global config file

--- config.js
+++ config.js
@@ -3,8 +3,20 @@ const stage = process.env.stage;
 const resourcesStage = process.env.resourcesStage;
 
 const stageConfigs = {
-  dev: {},
-  prod: {},
+  dev: {
+    bankId: {
+      envsKeyName: '/bankidEnvs/dev',
+      caName: 'bankid.ca',
+      pfxName: 'FPTestcert2.pfx',
+    },
+  },
+  prod: {
+    bankId: {
+      envsKeyName: '/bankidEnvs/prod',
+      caName: 'bankid.ca',
+      pfxName: 'FPTestcert2.pfx',
+    },
+  },
 };
 
 const config = stageConfigs[stage] || stageConfigs.dev;
