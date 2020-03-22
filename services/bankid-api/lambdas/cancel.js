a1fb382501f2891673689be14e4054cfe3914bc2
Dan Nilsson <mail@dannilsson.se>
2020-03-22 19:20:25 +0100

    refactor reading envs key name from config

--- services/bankid-api/lambdas/cancel.js
+++ services/bankid-api/lambdas/cancel.js
@@ -2,20 +2,21 @@ import to from 'await-to-js';
 import snakeCaseKeys from 'snakecase-keys';
 import { throwError } from '@helsingborg-stad/npm-api-error-handling';
 
+import config from '../../../config';
 import params from '../../../libs/params';
 import * as response from '../../../libs/response';
 import * as request from '../../../libs/request';
 import * as bankId from '../helpers/bankId';
 
-const SSMParams = params.read('/bankidEnvs/dev');
+const SSMParams = params.read(config.bankId.envsKeyName);
 
 export const main = async event => {
   const { orderRef } = JSON.parse(event.body);
-  const bankidSSMParams = await SSMParams;
+  const bankIdSSMparams = await SSMParams;
 
   const payload = { orderRef };
 
-  const [error, bankIdCancelResponse] = await to(sendBankIdCancelRequest(bankidSSMParams, payload));
+  const [error, bankIdCancelResponse] = await to(sendBankIdCancelRequest(bankIdSSMparams, payload));
   if (!bankIdCancelResponse) response.failure(error);
 
   return response.success({
