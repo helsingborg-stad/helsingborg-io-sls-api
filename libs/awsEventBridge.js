9efc0222c759570c0432ff4f0fd8b94060cffb96
Teddy Andersson <teddyandersson95@gmail.com>
2020-03-24 10:15:35 +0100

    Moved putEvent helper to root libs

--- libs/awsEventBridge.js
+++ libs/awsEventBridge.js
@@ -2,6 +2,12 @@ import AWS from 'aws-sdk';
 
 const eventBridge = new AWS.EventBridge();
 
+/**
+ *
+ * @param {Object} Detail The detailed payload for the event.
+ * @param {String} DetailType Free-form string used to decide what fields to expect in the event detail.
+ * @param {String} Source The source of the event.
+ */
 export function putEvent(Detail, DetailType, Source) {
   const params = {
     Entries: [
