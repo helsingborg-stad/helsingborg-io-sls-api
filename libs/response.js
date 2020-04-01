e777158218b2b6059127af0d62321a781d953a5a
Teddy Andersson <teddyandersson95@gmail.com>
2020-04-01 17:00:41 +0200

    Added more precise error handling to all bankId requests

--- libs/response.js
+++ libs/response.js
@@ -2,15 +2,19 @@ export function success(body) {
   return buildResponse(200, body);
 }
 
-export function failure(error) {
+export function failure(error, stackTrace) {
+  const { stage } = process.env;
+
   const errorBody = {
     status: error.status.toString(),
     code: error.status.toString(),
     title: error.name,
     detail: error.detail,
     message: error.message,
-    stack: error.stack,
   };
+
+  const addStackTrace = ['dev'].includes(stage);
+  if (addStackTrace || stackTrace) errorBody.stack = error.stack;
 
   return buildResponse(error.status, errorBody);
 }
