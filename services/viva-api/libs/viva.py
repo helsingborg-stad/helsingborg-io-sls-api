3132d58e210583014866e56b6201ff6b9d8acd3e
Dan Nilsson <mail@dannilsson.se>
2020-04-01 11:17:37 +0200

    split classes to separate files

--- services/viva-api/libs/viva.py
+++ services/viva-api/libs/viva.py
@@ -32,15 +32,11 @@ class Viva(object):
         transport = Transport(session=session)
 
         wsdl_url = self._params["wsdl_url"] + "/" + wsdl + "?WSDL"
-
-        client = Client(
-            wsdl=wsdl_url,
-            transport=transport
-        )
+        client = Client(wsdl=wsdl_url, transport=transport)
 
         return client.service
 
-    def get_service(self, service_wsdl):
+    def _get_service(self, service_wsdl):
         return self._get_client(service_wsdl)
 
     def _get_cookie(self):
@@ -54,25 +50,3 @@ class Viva(object):
         )
 
         return response.cookies[self._params["cookie_auth_name"]]
-
-
-class MyPages(Viva):
-
-    def __init__(self, usr, pnr, wsdl="MyPages"):
-        super(MyPages, self).__init__()
-
-        self._service = self.get_service(wsdl)
-        self.person_info = self.get_person_info(usr, pnr)
-        self.person_cases = self.get_person_cases(usr, pnr)
-
-    def get_person_info(self, usr, pnr):
-        response = self._service.PERSONINFO(
-            USER=usr,
-            PNR=pnr,
-            RETURNAS="xml"
-        )
-
-        return response
-
-    def get_person_cases(self, usr, pnr):
-        raise NotImplementedError()
