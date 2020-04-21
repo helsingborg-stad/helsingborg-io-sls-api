ee18e395804c8cf6ddcebc136404131f6921c83c
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-04-21 14:34:33 +0200

    add xml to json parser

--- services/viva-api/libs/my_pages.py
+++ services/viva-api/libs/my_pages.py
@@ -1,23 +1,35 @@
 import json
+import xmltodict
 from libs.viva import Viva
+
 
 class MyPages(Viva):
 
     def __init__(self, usr, pnr, wsdl="MyPages"):
         super(MyPages, self).__init__()
 
-        self._service = self._get_service(wsdl)
-        self.person_info = self.get_person_info(usr, pnr)
-        self.person_cases = self.get_person_cases(usr, pnr)
+        self.usr = usr
+        self.pnr = pnr
 
-    def get_person_info(self, usr, pnr):
-        response = self._service.PERSONINFO(
-            USER=usr,
-            PNR=pnr,
+        self._service = self._get_service(wsdl)
+        self.person_info = self.get_person_info()
+        self.person_cases = self.get_person_cases()
+
+    def get_person_info(self):
+        response_info = self._service.PERSONINFO(
+            USER=self.usr,
+            PNR=self.pnr,
             RETURNAS="xml"
         )
 
-        return response
+        return xmltodict.parse(response_info)
 
-    def get_person_cases(self, usr, pnr):
-        raise NotImplementedError()
+    def get_person_cases(self):
+        response_cases = self._service.PERSONCASES(
+            USER=self.usr,
+            PNR=self.pnr,
+            RETURNAS="xml",
+            SYSTEM=1
+        )
+
+        return xmltodict.parse(response_cases)
