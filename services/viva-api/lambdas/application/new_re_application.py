17cd48f94132a1053bec4ac38d405e5d713d2771
Dan Nilsson <mail@dannilsson.se>
2020-04-24 20:19:27 +0200

    uncomment code

--- services/viva-api/lambdas/application/new_re_application.py
+++ services/viva-api/lambdas/application/new_re_application.py
@@ -6,7 +6,6 @@ except ImportError:
 import json
 
 from libs.viva_application import VivaApplication
-from libs.my_pages import MyPages
 
 
 def main(event, context):
@@ -14,58 +13,50 @@ def main(event, context):
     usr = "19450817T5959"
     pnr = "19450817T5959"
 
-    vivaApplication = VivaApplication(
-        usr, pnr, my_pages=MyPages(
-            usr=usr, pnr=pnr
+    vivaApplication = VivaApplication(usr=usr, pnr=pnr)
 
-        )
+    new_re_application_repsonse = vivaApplication.new_re_application(
+        key="123",
+        user=usr,
+        person_case="",
+        period={
+            "START": "2020-04-01",
+            "END": "2020-04-30"
+        },
+        re_application={
+            "RAWDATA": "Testing CREATE NEW APPLICATION",
+            "RAWDATATYPE": "",
+            "CLIENT": {
+                "PNUMBER": "19450817T5959",
+                "FNAME": "Ole",
+                "LNAME": "Doff",
+                "ADDRESSES": [
+                    {
+                        "ADDRESS": {
+                            "TYPE": "FB",
+                            "ADDRESS": "Galagatan 20",
+                            "CO": "",
+                            "ZIP": "222 20",
+                            "CITY": "Helsingborg"
+                        }
+                    }
+                ],
+                "EMAIL": {
+                    "EMAIL": "vegard@nodomain.com",
+                    "NOTIFY": True
+                },
+                "FOREIGNCITIZEN": False,
+                "RESIDENCEPERMITTYPE": "",
+                "RESIDENCEPERMITDATE": "",
+                "CIVILSTATUS": "",
+                "ALTCIVILSTATUS": ""
+            },
+            "OTHER": "HEPP"
+        }
     )
 
-    person_cases = vivaApplication.person_cases
-    print(f"Person cases:{person_cases}")
-
-    # new_re_application_repsonse = vivaApplication.new_re_application(
-    #     key="123",
-    #     user=usr,
-    #     person_case="",
-    #     period={
-    #         "START": "2020-04-01",
-    #         "END": "2020-04-30"
-    #     },
-    #     re_application={
-    #         "RAWDATA": "Testing CREATE NEW APPLICATION",
-    #         "RAWDATATYPE": "",
-    #         "CLIENT": {
-    #             "PNUMBER": "19450817T5959",
-    #             "FNAME": "Ole",
-    #             "LNAME": "Doff",
-    #             "ADDRESSES": [
-    #                 {
-    #                     "ADDRESS": {
-    #                         "TYPE": "FB",
-    #                         "ADDRESS": "Galagatan 20",
-    #                         "CO": "",
-    #                         "ZIP": "222 20",
-    #                         "CITY": "Helsingborg"
-    #                     }
-    #                 }
-    #             ],
-    #             "EMAIL": {
-    #                 "EMAIL": "vegard@nodomain.com",
-    #                 "NOTIFY": True
-    #             },
-    #             "FOREIGNCITIZEN": False,
-    #             "RESIDENCEPERMITTYPE": "",
-    #             "RESIDENCEPERMITDATE": "",
-    #             "CIVILSTATUS": "",
-    #             "ALTCIVILSTATUS": ""
-    #         },
-    #         "OTHER": "HEPP"
-    #     }
-    # )
-
     body = {
-        "data": "new_re_application_repsonse",
+        "data": new_re_application_repsonse,
     }
 
     response = {
