94c752cbd27e9452980f46a7809019762413524a
Teddy Andersson <teddyandersson95@gmail.com>
2020-05-14 11:36:46 +0200

    Refactored function to not use hasOwnProperty

--- libs/validateKeys.js
+++ libs/validateKeys.js
@@ -1,11 +1,10 @@
 // A helper  function for validation of keys in an object
 
 export function validateKeys(obj, keys) {
-  for (const key in keys) {
-    if (!Object.prototype.hasOwnProperty.call(obj, keys[key])) {
+  for (const i in keys) {
+    if (!Object.keys(obj).find(item => item === keys[i])) {
       return false;
     }
   }
-
   return true;
 }
