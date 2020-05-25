b27c2b65b895404bb1d7fbd43b91d06ed4879c02
Teddy Andersson <teddyandersson95@gmail.com>
2020-05-25 17:02:51 +0200

    lambda boilerplate for creating form logic

--- services/forms-api/lambdas/addFormLogics.js
+++ services/forms-api/lambdas/addFormLogics.js
@@ -1,11 +1,14 @@
 import * as response from '../../../libs/response';
-
 /**
  * Function for adding logics to an existing form in dynamodb.
  */
 
-export async function main(event) {
-  return response.success({
-    message: 'This is the add form logics lambda response',
+export async function main() {
+  return response.failure({
+    status: 501,
+    code: 501,
+    title: 'Not Implemented',
+    detail: 'The server does not support the functionality required to fulfill the request.',
+    message: 'The server does not support the functionality required to fulfill the request.',
   });
 }
