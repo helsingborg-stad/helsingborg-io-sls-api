7105dce7909d29ed0e9eeb80cf20aed129075a30
Jacob Winding <jacobkwinding@gmail.com>
2020-06-29 18:15:28 +0200

    Added update logic, allowing for updating answers and status on an existing case

--- services/cases-api/lambdas/update.js
+++ services/cases-api/lambdas/update.js
@@ -6,32 +6,72 @@ import * as response from '../../../libs/response';
 import * as dynamoDb from '../../../libs/dynamoDb';
 
 /**
- * Handler function for updateing user case by id from dynamodb
+ * Handler function for updating user case by id from dynamodb
+ * Can update the data (i.e. the answers), and change the status of the case.
  */
-export const main = async event => {
-  const { id } = event.pathParameters;
+export async function main(event) {
+  const { caseId } = event.pathParameters;
+  const userId = event.headers.Authorization;
+  const casePartitionKey = `USER#${userId}`;
+  const caseSortKey = `${casePartitionKey}#CASE#${caseId}`;
 
-  // todo: implement condition params
+  const requestBody = JSON.parse(event.body);
+  let keyCounter = 0;
+  let UpdateExpression = 'SET ';
+  const ExpressionAttributeNames = {};
+  const ExpressionAttributeValues = {};
+  //if we sent a status, then we update it.
+  if (requestBody.status) {
+    UpdateExpression += '#status = :newStatus';
+    ExpressionAttributeNames['#status'] = 'status';
+    ExpressionAttributeValues[':newStatus'] = requestBody.status;
+  }
+  const data = requestBody.data || {};
+  //update the data (answers) only if we've sent some.
+  if (Object.keys(data).length > 0) {
+    if (requestBody.status) {
+      UpdateExpression += ', ';
+    }
+    ExpressionAttributeNames['#data'] = 'data';
+    const numberValues = Object.keys(data).length;
+    for (const key in data) {
+      UpdateExpression +=
+        '#data.#key' +
+        keyCounter +
+        ' = :value' +
+        keyCounter +
+        (keyCounter === numberValues - 1 ? '' : ', '); //No comma on the last line.
+      ExpressionAttributeNames['#key' + keyCounter] = key;
+      ExpressionAttributeValues[':value' + keyCounter] = data[key];
+      keyCounter++;
+    }
+  }
   const params = {
     TableName: config.cases.tableName,
     Key: {
-      id,
+      PK: casePartitionKey,
+      SK: caseSortKey,
     },
+    UpdateExpression,
+    ExpressionAttributeNames,
+    ExpressionAttributeValues,
+    ReturnValues: 'ALL_NEW',
   };
 
-  const [error, casesUpdateResponse] = await to(sendCasesUpdateRequest(params));
-  if (!casesUpdateResponse) return response.failure(error);
+  const [error, queryResponse] = await to(sendUpdateCaseRequest(params));
+  if (error) return response.failure(error);
 
-  return response.success(201, {
-    type: 'casesUpdate',
-    attributes: {
-      ...casesUpdateResponse,
-    },
+  return response.success(200, {
+    caseId,
+    personalNumber: userId,
+    type: queryResponse.Attributes.type,
+    status: queryResponse.Attributes.status,
+    data: queryResponse.Attributes.data,
   });
-};
+}
 
-async function sendCasesUpdateRequest(params) {
-  const [dbError, dbResponse] = await to(dynamoDb.call('updateItem', params));
-  if (!dbResponse) throwError(dbError);
-  return dbResponse;
+async function sendUpdateCaseRequest(params) {
+  const [error, result] = await to(dynamoDb.call('update', params));
+  if (error) throwError(error.statusCode);
+  return result;
 }
