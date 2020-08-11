457ab178cadd98b800ae0ad340b5101d586f897a
Jacob Winding <jacobkwinding@gmail.com>
2020-08-11 13:21:59 +0200

    fix(user-api): add 404 message, and format response to camelcase

--- services/user-api/lambdas/get.js
+++ services/user-api/lambdas/get.js
@@ -1,12 +1,14 @@
 import to from 'await-to-js';
-import snakeCaseKeys from 'snakecase-keys';
+import camelCase from 'camelcase';
 import { throwError } from '@helsingborg-stad/npm-api-error-handling';
 
 import config from '../../../config';
 import * as response from '../../../libs/response';
 import * as dynamoDb from '../../../libs/dynamoDb';
 
-// get users (GET)
+/**
+ * Get the user with the personal number specified in the path
+ */
 export const main = async event => {
   const { personalNumber } = event.pathParameters;
 
@@ -18,13 +20,26 @@ export const main = async event => {
   };
 
   const [error, userGetResponse] = await to(sendUserGetRequest(params));
-  if (!userGetResponse) return response.failure(error);
+  if (error) return response.failure(error);
 
-  return response.success(200, {
+  //convert to camelCase
+  const attributes = {};
+  Object.keys(userGetResponse).forEach(key => {
+    attributes[camelCase(key)] = userGetResponse[key];
+  });
+
+  //Returning no results from the db is not an error, so we need to check for this separately,
+  //and return a 404 if no results were found
+  if (Object.keys(attributes).length === 0) {
+    return response.buildResponse(404, {
+      type: 'userGet',
+      errorMessage: 'No user with that personal number found in the database.',
+    });
+  }
+
+  return response.buildResponse(200, {
     type: 'userGet',
-    attributes: {
-      ...snakeCaseKeys(userGetResponse),
-    },
+    attributes,
   });
 };
 
