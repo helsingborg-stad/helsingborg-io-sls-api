4f7279de8e534c17e6a0e95044da15ac41104559
Teddy Andersson <teddyandersson95@gmail.com>
2020-08-20 13:05:08 +0200

    fix: authorizer ruturns a 401 when unauthorized

--- services/auth/token/lambdas/authorize.js
+++ services/auth/token/lambdas/authorize.js
@@ -4,14 +4,13 @@ import generateIAMPolicy from '../helpers/generateIAMPolicy';
 
 export async function main(event) {
   const { authorizationToken } = event;
-  let isAllowed = true;
   const [error, decodedToken] = await to(verifyToken(authorizationToken));
   if (error) {
-    isAllowed = false;
+    // By thorwing this error AWS returns a 401 response with the message unauthorized
+    throw Error('Unauthorized');
   }
 
-  const effect = isAllowed ? 'Allow' : 'Deny';
-  const IAMPolicy = generateIAMPolicy(decodedToken.personalNumber, effect, '*');
+  const IAMPolicy = generateIAMPolicy(decodedToken.personalNumber, 'Allow', '*');
 
   return IAMPolicy;
 }
