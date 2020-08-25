f968bb357c961ac3fd829be31faa5e1b84c7e98e
Teddy Andersson <teddyandersson95@gmail.com>
2020-08-25 17:11:37 +0200

    fix: checking if authorization header contains bearer prefix or not

--- services/auth/token/lambdas/authorize.js
+++ services/auth/token/lambdas/authorize.js
@@ -4,7 +4,12 @@ import generateIAMPolicy from '../helpers/generateIAMPolicy';
 
 export async function main(event) {
   const { authorizationToken } = event;
-  const [error, decodedToken] = await to(verifyToken(authorizationToken));
+
+  const token = authorizationToken.includes('Bearer')
+    ? authorizationToken.substr(authorizationToken.indexOf(' ') + 1)
+    : authorizationToken;
+
+  const [error, decodedToken] = await to(verifyToken(token));
   if (error) {
     // By thorwing this error AWS returns a 401 response with the message unauthorized
     throw Error('Unauthorized');
