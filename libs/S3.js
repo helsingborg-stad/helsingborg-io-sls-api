64750727b9d8ce3636637d2e527369db7aa1df56
Teddy Andersson <teddyandersson95@gmail.com>
2020-09-03 16:30:35 +0200

    feat: fixed issues with aws credentials in serverless configuration

--- libs/S3.js
+++ libs/S3.js
@@ -1,6 +1,6 @@
-import * as AWS from 'aws-sdk';
+import AWS from 'aws-sdk';
 
-const s3Client = new AWS.S3({ region: 'eu-north-1' });
+const s3Client = new AWS.S3();
 
 async function getSignedUrl(bucket, method, params) {
   return s3Client.getSignedUrl(method, {
