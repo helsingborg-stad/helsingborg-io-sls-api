a5a6e5125562bcea7de8a93d55e853321c7e30b1
Teddy Andersson <teddyandersson95@gmail.com>
2020-09-04 16:15:41 +0200

    feat: create presigned post

--- libs/S3.js
+++ libs/S3.js
@@ -1,6 +1,6 @@
-import AWS from 'aws-sdk';
+import S3 from 'aws-sdk/clients/s3';
 
-const s3Client = new AWS.S3();
+const s3Client = new S3();
 
 async function getSignedUrl(bucket, method, params) {
   return s3Client.getSignedUrl(method, {
@@ -9,8 +9,46 @@ async function getSignedUrl(bucket, method, params) {
   });
 }
 
-const S3 = {
-  getSignedUrl,
-};
+/**
+ * Use AWS SDK to create pre-signed POST data.
+ * We also put a default file size limit (100B - 10MB).
+ * @param bucketName
+ * @param key
+ * @param contentType
+ * @param expireTime
+ * @param fileMinSize
+ * @param fileMaxSize
+ * @returns {Promise<object>}
+ */
+function createPresignedPost({
+  bucketName,
+  key,
+  contentType,
+  expireTime,
+  fileMinSize = 10,
+  fileMaxSize = 10000000,
+}) {
+  const params = {
+    Expires: expireTime,
+    Bucket: bucketName,
+    Conditions: [['content-length-range', fileMinSize, fileMaxSize]], // 100Byte - 10MB
+    Fields: {
+      'Content-Type': contentType,
+      key,
+    },
+  };
+  return new Promise((resolve, reject) => {
+    s3Client.createPresignedPost(params, (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(data);
+    });
+  });
+}
 
-export default S3;
+export default {
+  getSignedUrl,
+  createPresignedPost,
+};
--- services/user-api/lambdas/uploadAttachments.js
+++ services/user-api/lambdas/uploadAttachments.js
@@ -1,4 +1,5 @@
 import to from 'await-to-js';
+import { v4 as uuid } from 'uuid';
 import S3 from '../../../libs/S3';
 import * as response from '../../../libs/response';
 import { BadRequestError } from '@helsingborg-stad/npm-api-error-handling';
@@ -31,26 +32,37 @@ export async function main(event) {
   }
 
   // TODO: Decide the filename convention when generating a url.
-  // TODO: Check if filename can be set to a random id despite the name of the upload file from client.
-  // The path to where we want to upload a file in the s3 bucket.
-  const s3Key = `${decodedToken.personalNumber}/${fileName}`;
 
-  // TODO: Check if we can set a file size limit in these params.
+  // The path to where we want to upload a file in the s3 bucket.
+  const s3Key = `${decodedToken.personalNumber}/${uuid()}_${fileName}`;
+
+  // // TODO: Check if we can set a file size limit in these params.
+  // const params = {
+  //   ContentType: mime,
+  //   ACL: 'public-read',
+  //   Key: s3Key,
+  //   Expires: 60 * 10,
+  // };
+
   const params = {
-    ContentType: mime,
-    ACL: 'public-read',
-    Key: s3Key,
-    Expires: 60 * 10,
+    bucketName: 'hbg-attachments',
+    key: s3Key,
+    contentType: mime,
+    expireTime: 60,
   };
 
   // Request pre signed upload url from aws s3.
-  const [error, uploadUrl] = await to(S3.getSignedUrl('hbg-attachments', 'putObject', params));
+  const [error, uploadUrl] = await to(S3.createPresignedPost(params));
   if (error) return response.failure(error);
+
+  // // Request pre signed upload url from aws s3.
+  // const [error, uploadUrl] = await to(S3.getSignedUrl('hbg-attachments', 'putObject', params));
+  // if (error) return response.failure(error);
 
   return response.success(200, {
     type: 'userAttachment',
     attributes: {
-      uploadUrl,
+      ...uploadUrl,
     },
   });
 }
