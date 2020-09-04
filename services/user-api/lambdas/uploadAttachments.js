0da900514d63dde1bc51e33dd58ba5b6c6f63877
Teddy Andersson <teddyandersson95@gmail.com>
2020-09-04 08:40:53 +0200

    feat: added error handling

--- services/user-api/lambdas/uploadAttachments.js
+++ services/user-api/lambdas/uploadAttachments.js
@@ -1,5 +1,6 @@
-import * as response from '../../../libs/response';
+import to from 'await-to-js';
 import S3 from '../../../libs/S3';
+import * as response from '../../../libs/response';
 import { BadRequestError } from '@helsingborg-stad/npm-api-error-handling';
 import { decodeToken } from '../../../libs/token';
 
@@ -10,18 +11,31 @@ const allowedMimes = ['image/jpeg', 'image/png', 'image/jpg'];
  * Get the user with the personal number specified in the path
  */
 export async function main(event) {
-  // eslint-disable-next-line no-console
   const decodedToken = decodeToken(event);
 
   const { fileName, mime } = JSON.parse(event.body);
 
+  if (!fileName) {
+    // Check if fileName exsits in event body
+    return response.failure(new BadRequestError('Could not find key "fileName" in request body'));
+  }
+
+  if (!mime) {
+    // Check if mimeType exists in event body.
+    return response.failure(new BadRequestError('Could not find key "mime" in request body'));
+  }
+
   if (!allowedMimes.includes(mime)) {
+    // Check if passed mimeType is a fileformat that we allow.
     return response.failure(new BadRequestError(`The mimeType ${mime} is not allowed`));
   }
 
   // TODO: Decide the filename convention when generating a url.
+  // TODO: Check if filename can be set to a random id despite the name of the upload file from client.
+  // The path to where we want to upload a file in the s3 bucket.
   const s3Key = `${decodedToken.personalNumber}/${fileName}`;
 
+  // TODO: Check if we can set a file size limit in these params.
   const params = {
     ContentType: mime,
     ACL: 'public-read',
@@ -29,13 +43,14 @@ export async function main(event) {
     Expires: 60 * 10,
   };
 
-  const uploadUrl = await S3.getSignedUrl('hbg-attachments', 'putObject', params);
+  // Request pre signed upload url from aws s3.
+  const [error, uploadUrl] = await to(S3.getSignedUrl('hbg-attachments', 'putObject', params));
+  if (error) return response.failure(error);
 
   return response.success(200, {
     type: 'userAttachment',
     attributes: {
       uploadUrl,
-      filePath: s3Key,
     },
   });
 }
