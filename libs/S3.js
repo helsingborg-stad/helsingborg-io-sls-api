c51f71f7e8a0b329f5dfd4868b1bde1bb6720b71
Teddy Andersson <teddyandersson95@gmail.com>
2020-09-07 11:44:20 +0200

    feat: updated s3 helper lib

--- libs/S3.js
+++ libs/S3.js
@@ -2,9 +2,16 @@ import S3 from 'aws-sdk/clients/s3';
 
 const s3Client = new S3();
 
-async function getSignedUrl(bucket, method, params) {
+/**
+ * Use AWS SDK to create pre-signed upload url.
+ * @param bucketName
+ * @param method
+ * @param params
+ * @returns {string}
+ */
+async function getSignedUrl(bucketName, method, params) {
   return s3Client.getSignedUrl(method, {
-    Bucket: bucket,
+    Bucket: bucketName,
     ...params,
   });
 }
