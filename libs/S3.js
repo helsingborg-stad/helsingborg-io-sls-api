88ca7ea5ff890f22ff21195b6a129408fe3cc9d1
Teddy Andersson <teddyandersson95@gmail.com>
2020-09-07 11:45:25 +0200

    fix: removed unused export from s3 helper lib

--- libs/S3.js
+++ libs/S3.js
@@ -16,46 +16,6 @@ async function getSignedUrl(bucketName, method, params) {
   });
 }
 
-/**
- * Use AWS SDK to create pre-signed POST data.
- * We also put a default file size limit (100B - 10MB).
- * @param bucketName
- * @param key
- * @param contentType
- * @param expireTime
- * @param fileMinSize
- * @param fileMaxSize
- * @returns {Promise<object>}
- */
-function createPresignedPost({
-  bucketName,
-  key,
-  contentType,
-  expireTime,
-  fileMinSize = 10,
-  fileMaxSize = 10000000,
-}) {
-  const params = {
-    Expires: expireTime,
-    Bucket: bucketName,
-    Conditions: [['content-length-range', fileMinSize, fileMaxSize]], // 100Byte - 10MB
-    Fields: {
-      'Content-Type': contentType,
-      key,
-    },
-  };
-  return new Promise((resolve, reject) => {
-    s3Client.createPresignedPost(params, (err, data) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(data);
-    });
-  });
-}
-
 export default {
   getSignedUrl,
-  createPresignedPost,
 };
