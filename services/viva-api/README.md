9f3d86b8aab51d2b5fc95e4492441d6c72ad94e0
Dan Nilsson <mail@dannilsson.se>
2020-09-11 17:27:55 +0200

    remove viva api - obsoleted
    refactor to microservice using event
    see viva-ms service

--- services/viva-api/README.md
+++
@@ -1,5 +0,0 @@
-# Viva API
-
-## Requirements
-
-## Usage
--- services/viva-api/lambdas/applications.js
+++
@@ -1,7 +0,0 @@
-import * as response from '../../../libs/response';
-
-export const main = async event =>
-  response.buildResponse(200, {
-    type: 'vivaApplications',
-    vivaResponse: 'Not implemented',
-  });
