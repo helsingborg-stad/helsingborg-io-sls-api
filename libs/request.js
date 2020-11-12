dfb6d195b37ef33989ce741c1642f555f9e98be1
Dan Nilsson <mail@dannilsson.se>
2020-11-12 14:50:53 +0100

    feat: 🎸 add header args
    improve requestClient to take in custom headers at instance creation

--- libs/request.js
+++ libs/request.js
@@ -1,11 +1,30 @@
 import axios from 'axios';
 import https from 'https';
 
-export const requestClient = (options, timeout = 5000, contentType = 'application/json') =>
+/**
+ * Helper function to create an AXIOS request client
+ *
+ * Using the HTTPS node module as AXIOS agent
+ *
+ * https://github.com/axios/axios
+ *
+ * @param {obj} options See https.Agent https://nodejs.org/api/https.html#https_class_https_agent
+ * @param {obj} headers Add headers to axios constructor
+ * @param {number} timeout When request should time out (milliseconds)
+ * @param {string} contentType
+ * @returns AxiosInstance
+ */
+export const requestClient = (
+  options,
+  headers = {},
+  timeout = 5000,
+  contentType = 'application/json'
+) =>
   axios.create({
     httpsAgent: new https.Agent({ ...options }),
     headers: {
       'Content-Type': contentType,
+      ...headers,
     },
     timeout,
   });
