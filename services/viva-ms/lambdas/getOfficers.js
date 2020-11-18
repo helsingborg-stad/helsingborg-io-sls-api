c27a2da02f83782f80c0f2d64338c1a85f2f4fdc
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-11-18 15:00:26 +0100

    feat: 🎸 add base imports

--- services/viva-ms/lambdas/getOfficers.js
+++ services/viva-ms/lambdas/getOfficers.js
@@ -1,5 +1,14 @@
+import AWS from 'aws-sdk';
 import to from 'await-to-js';
+import { throwError } from '@helsingborg-stad/npm-api-error-handling';
 
 import config from '../../../config';
+import params from '../../../libs/params';
+import hash from '../../../libs/helperHashEncode';
 
-export async function main(event) {}
+import { CASE_PROVIDER_VIVA } from '../../../libs/constants';
+import * as request from '../../../libs/request';
+
+const SSMParams = params.read(config.vada.envsKeyName);
+
+export const main = async event => {};
