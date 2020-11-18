d0233c52eb5e5036aa33e5a48d79502a1811573d
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-11-18 15:25:38 +0100

    feat: 🎸 add dynamodb data converter

--- services/viva-ms/lambdas/getOfficers.js
+++ services/viva-ms/lambdas/getOfficers.js
@@ -11,9 +11,20 @@ import * as request from '../../../libs/request';
 
 const SSMParams = params.read(config.vada.envsKeyName);
 
+const dynamoDbConverter = AWS.DynamoDB.Converter;
+
 /**
  * Handler reacting on event stream triggered by DynamoDB cases table
  * Fetches officeres information on an applicant via the Viva API adapter (VADA)
  * Sends the officers data into the eventbridge
  */
-export const main = async event => {};
+export const main = async event => {
+  const [record] = event.Records;
+
+  if (record.dynamodb.NewImage === undefined) {
+    return null;
+  }
+
+  // Make DynamoDb data readable by the Viva API adapter (VADA)
+  const unmarshalledData = dynamoDbConverter.unmarshall(record.dynamodb.NewImage);
+};
