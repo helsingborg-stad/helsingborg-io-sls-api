f349a50be443e2b42ea873af6547726a3aba0b3e
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-11-18 15:14:56 +0100

    docs: ✏️ add comments to main lambda function
    Describing the main purpose of this lambda
    
    docs: ✏️ fix gramma about viva

--- services/viva-ms/lambdas/getOfficers.js
+++ services/viva-ms/lambdas/getOfficers.js
@@ -11,4 +11,9 @@ import * as request from '../../../libs/request';
 
 const SSMParams = params.read(config.vada.envsKeyName);
 
+/**
+ * Handler reacting on event stream triggered by DynamoDB cases table
+ * Fetches officeres information on an applicant via the Viva API adapter (VADA)
+ * Sends the officers data into the eventbridge
+ */
 export const main = async event => {};
