e75d7edb8c0d27b050cdcbb3a90b537ecb2d7e32
Dan Nilsson <mail@dannilsson.se>
2020-12-03 13:59:04 +0100

    feat: 🎸 add constant to hold EXPIRATION hours

--- libs/constants.js
+++ libs/constants.js
@@ -2,3 +2,4 @@ export const CASE_ITEM_TYPE = 'CASE';
 export const CASE_STATUS_SUBMITTED = 'submitted';
 export const CASE_STATUS_ONGOING = 'ongoing';
 export const CASE_PROVIDER_VIVA = 'VIVA';
+export const CASE_EXPIRATION_HOURS = 72;
