8d4de619ea770f0e84e038d107889dbd9337ae3d
Teddy Andersson <teddyandersson95@gmail.com>
2021-01-13 16:36:27 +0100

    feat(authorize.js): retrival and passing secret to verifyToken function
    N

--- services/auth/token/lambdas/authorize.js
+++ services/auth/token/lambdas/authorize.js
@@ -2,6 +2,10 @@ import to from 'await-to-js';
 
 import generateIAMPolicy from '../helpers/generateIAMPolicy';
 import { verifyToken } from '../../../../libs/token';
+import config from '../../../../config';
+import secrets from '../../../../libs/secrets';
+
+const CONFIG_AUTH_SECRETS_ACCESS_TOKEN = config.auth.secrets.accessToken;
 
 export async function main(event) {
   const { authorizationToken } = event;
@@ -10,7 +14,14 @@ export async function main(event) {
     ? authorizationToken.substr(authorizationToken.indexOf(' ') + 1)
     : authorizationToken;
 
-  const [error, decodedToken] = await to(verifyToken(token));
+  const [getSecretError, secret] = await to(
+    secrets.get(CONFIG_AUTH_SECRETS_ACCESS_TOKEN.name, CONFIG_AUTH_SECRETS_ACCESS_TOKEN.keyName)
+  );
+  if (getSecretError) {
+    throw Error('Unauthorized');
+  }
+
+  const [error, decodedToken] = await to(verifyToken(token, secret));
   if (error) {
     // By thorwing this error AWS returns a 401 response with the message unauthorized
     throw Error('Unauthorized');
