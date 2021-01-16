487577fad286da80a088f630dc4617e8677aed7c
Dan Nilsson <mail@dannilsson.se>
2021-01-16 20:56:11 +0100

    feat: 🎸 add lambdas syncWorkflow, syncPeriod

---
+++ services/viva-ms/lambdas/syncWorkflow.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+// import AWS from 'aws-sdk';
+// import to from 'await-to-js';
+// import { throwError } from '@helsingborg-stad/npm-api-error-handling';
+
+// import config from '../../../config';
+// import params from '../../../libs/params';
+// import * as dynamoDb from '../../../libs/dynamoDb';
+
+// const dynamoDbConverter = AWS.DynamoDB.Converter;
+
+export const main = async event => {
+  console.log('event', event);
+  // const { user } = event.detail;
+  return true;
+};
