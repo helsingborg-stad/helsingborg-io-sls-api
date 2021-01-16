db04eb6ecdf13dd20d9fb70718b9de1b88c836e5
Dan Nilsson <mail@dannilsson.se>
2021-01-16 20:54:10 +0100

    refactor: 💡 rename lambda to syncApplicationStatus

--- services/viva-ms/lambdas/syncApplicationStatus.js
+++ services/viva-ms/lambdas/syncApplicationStatus.js
@@ -7,20 +7,21 @@ import config from '../../../config';
 import params from '../../../libs/params';
 import hash from '../../../libs/helperHashEncode';
 import * as request from '../../../libs/request';
+// import * as dynamoDb from '../../../libs/dynamoDb';
 
 const SSMParams = params.read(config.vada.envsKeyName);
 
 export const main = async event => {
-  const { user: personalNumber } = event.detail;
+  const { user } = event.detail;
 
   const [applicationStatusError, applicationStatusResponse] = await to(
-    sendApplicationStatusRequest(personalNumber)
+    sendApplicationStatusRequest(user.personalNumber)
   );
   if (applicationStatusError) {
-    return console.error('(Viva-ms) syncStatus', applicationStatusError);
+    return console.error('(Viva-ms) syncApplicationStatus', applicationStatusError);
   }
 
-  console.log('(Viva-ms) syncStatus', applicationStatusResponse);
+  console.log('(Viva-ms) syncApplicationStatus', applicationStatusResponse);
 
   return true;
 };
@@ -32,28 +33,37 @@ async function sendApplicationStatusRequest(personalNumber) {
   const personalNumberEncoded = hash.encode(personalNumber, hashSalt, hashSaltLength);
 
   const { vadaUrl, xApiKeyToken } = ssmParams;
-  const requestClient = request.requestClient({}, { 'x-api-key': xApiKeyToken });
+  const authorizedRequestClient = request.requestClient({}, { 'x-api-key': xApiKeyToken });
 
-  const vadaApplicationStatusUrl = `${vadaUrl}/application/status/${personalNumberEncoded}`;
+  const vadaApplicationStatusUrl = `${vadaUrl}/applications/${personalNumberEncoded}/status`;
 
-  const [error, vadaApplicationStatusResponse] = await to(
-    request.call(requestClient, 'get', vadaApplicationStatusUrl, null)
+  const [requestError, vadaApplicationStatusResponse] = await to(
+    request.call(authorizedRequestClient, 'get', vadaApplicationStatusUrl)
   );
 
-  if (error) {
-    if (error.response) {
+  if (requestError) {
+    if (requestError.response) {
       // The request was made and the server responded with a
       // status code that falls out of the range of 2xx
-      throwError(error.response.status, error.response.data.message);
-    } else if (error.request) {
+      throwError(requestError.response.status, requestError.response.data.message);
+    } else if (requestError.request) {
       // The request was made but no response was received
       // `error.request` is an instance of http.ClientRequest in node.js
-      throwError(500, error.request.message);
+      throwError(500, requestError.request.message);
     } else {
       // Something happened in setting up the request that triggered an Error
-      throwError(500, error.message);
+      throwError(500, requestError.message);
     }
   }
 
   return vadaApplicationStatusResponse.data;
 }
+
+// async function sendStatusUpdateDbRequest(params) {
+//   const [error, result] = await to(dynamoDb.call('update', params));
+//   if (error) {
+//     throwError(500, error.message);
+//   }
+
+//   return result;
+// }
