ab0d8d673cb96cfc9e825df69891e0358a3494a6
Jacob Winding <jacobkwinding@gmail.com>
2021-01-18 13:10:35 +0100

    feat(attachments-list): remove unnecessary export

--- services/users-api/lambdas/getAttachmentList.ts
+++ services/users-api/lambdas/getAttachmentList.ts
@@ -3,7 +3,7 @@ import { buildResponse } from '../../../libs/response';
 import { decodeToken } from '../../../libs/token';
 import S3 from 'aws-sdk/clients/s3';
 
-export const s3Client = new S3();
+const s3Client = new S3();
 const bucketName = process.env.BUCKET_NAME;
 
 export const main = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
