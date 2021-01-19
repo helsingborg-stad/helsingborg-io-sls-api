f77c1669ddf6094c464cdd2249068d7d0c462d27
Jacob Winding <jacobkwinding@gmail.com>
2021-01-19 11:47:00 +0100

    feat(attachments-list): minor refactoring to improve code structure

--- services/users-api/lambdas/getAttachmentList.ts
+++ services/users-api/lambdas/getAttachmentList.ts
@@ -1,7 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { AWSError } from 'aws-sdk/lib/error';
+import to from 'await-to-js';
+import S3 from 'aws-sdk/clients/s3';
+
 import { buildResponse } from '../../../libs/response';
 import { decodeToken } from '../../../libs/token';
-import S3 from 'aws-sdk/clients/s3';
 
 const s3Client = new S3();
 const bucketName = process.env.BUCKET_NAME;
@@ -10,21 +13,29 @@ export const main = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxy
   const decodedToken = decodeToken(event);
   const { personalNumber } = decodedToken as { personalNumber: string };
 
-  const listFilesResponse = await s3Client
-    .listObjectsV2({
-      Bucket: bucketName,
-      Prefix: `${personalNumber}/`,
-    })
-    .promise();
+  const [error, fileList] = await to<S3.ObjectList, AWSError>(getFileList(personalNumber));
 
-  const files = listFilesResponse.Contents.filter(file => file.Key !== `${personalNumber}/`).map(
-    file => ({
+  if (error) {
+    return buildResponse(error.code, {
+      type: 'userAttachment',
+      attributes: {
+        type: 'fileListError',
+        bucket: bucketName,
+        personalNumber,
+        message: error.message,
+        stackTrace: error.stack,
+      },
+    });
+  }
+
+  const files = fileList
+    .filter(file => file.Key !== `${personalNumber}/`) // The 'folder' with the name of the personal number is returned as a pseudofile, so we filter that out.
+    .map(file => ({
       key: file.Key,
       filename: file.Key.substring(`${personalNumber}/`.length),
       size: file.Size,
       lastModified: file.LastModified,
-    })
-  );
+    }));
 
   return buildResponse(200, {
     type: 'userAttachment',
@@ -35,4 +46,17 @@ export const main = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxy
       files,
     },
   });
+};
+
+const getFileList = async (Prefix: string) => {
+  const listFilesResponse = await s3Client
+    .listObjectsV2({
+      Bucket: bucketName,
+      Prefix,
+    })
+    .promise();
+  if (listFilesResponse.$response.error) {
+    throw listFilesResponse.$response.error;
+  }
+  return listFilesResponse.Contents;
 };
