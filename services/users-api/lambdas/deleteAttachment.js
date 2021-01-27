ceaa52b2b0effa55135cdf8c1c18d87671a94bda
Dan Nilsson <mail@dannilsson.se>
2021-01-27 20:02:27 +0100

    refactor: 💡 add lambda - delete s3 file
    revert back to non-ts to keep the code consistent with other
    non-typescript lambdas. typescript is good but has to be implemented for
    all lambdas in the future

---
+++ services/users-api/lambdas/deleteAttachment.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-console */
+import to from 'await-to-js';
+import { throwError, BadRequestError } from '@helsingborg-stad/npm-api-error-handling';
+import S3 from '../../../libs/S3';
+import * as response from '../../../libs/response';
+import { decodeToken } from '../../../libs/token';
+
+const BUCKET_NAME = process.env.BUCKET_NAME;
+
+export async function main(event) {
+  const { filename } = event.pathParameters;
+  if (!filename) {
+    return response.failure(new BadRequestError('Missing filename in path query string'));
+  }
+
+  const decodedToken = decodeToken(event);
+  const { personalNumber } = decodedToken;
+
+  const [getFilesError, userS3Files] = await to(getFilesFromUserS3Bucket(personalNumber));
+  if (getFilesError) {
+    return response.failure(getFilesError);
+  }
+
+  const fileToDelete = `${personalNumber}/${filename}`;
+
+  const [findFileError] = await to(findFile(userS3Files, fileToDelete));
+  if (findFileError) {
+    return response.failure(findFileError);
+  }
+
+  const [deleteFileError] = await to(deleteFile(fileToDelete));
+  if (deleteFileError) {
+    return response.failure(deleteFileError);
+  }
+
+  return response.success(200, {
+    type: 'userAttachment',
+  });
+}
+
+async function getFilesFromUserS3Bucket(personalNumber) {
+  const [getFilesError, s3Files] = await to(S3.getFiles(BUCKET_NAME, `${personalNumber}/`));
+  if (getFilesError) {
+    throwError(getFilesError.statusCode, getFilesError.message);
+  }
+
+  return s3Files;
+}
+
+async function deleteFile(key) {
+  const [deleteFileError] = await to(S3.deleteFile(BUCKET_NAME, key));
+  if (deleteFileError) {
+    throwError(deleteFileError.statusCode, deleteFileError.message);
+  }
+
+  return true;
+}
+
+async function findFile(s3Files, searchFileKey) {
+  const fileKeyExists = s3Files.Contents.find(file => file?.Key === searchFileKey);
+  if (!fileKeyExists) {
+    throwError(404, `No file with key '${searchFileKey}' found`);
+  }
+
+  return true;
+}
