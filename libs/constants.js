3f6667daac705cf0361d7d97e86d71754b559a51
Teddy Andersson <teddyandersson95@gmail.com>
2021-01-29 12:18:25 +0100

    refactor(viva-ms): import constans from libs instead of defining them twice

--- libs/constants.js
+++ libs/constants.js
@@ -1,5 +1,6 @@
 export const CASE_ITEM_TYPE = 'CASE';
 export const CASE_STATUS_SUBMITTED = 'submitted';
 export const CASE_STATUS_ONGOING = 'ongoing';
+export const CASE_STATUS_NOT_STARTED = 'notStarted';
 export const CASE_PROVIDER_VIVA = 'VIVA';
 export const CASE_EXPIRATION_HOURS = 72;
