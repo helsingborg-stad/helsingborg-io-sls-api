e8be5df5f8e11d4c2d42995bae06fb1ccebc06ed
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-02-04 14:54:47 +0100

    fix: changed status type separator to colons

--- libs/constants.js
+++ libs/constants.js
@@ -1,6 +1,6 @@
 import { getStatusByType } from '../libs/caseStatuses';
 
 export const CASE_ITEM_TYPE = 'CASE';
-export const CASE_STATUS_ONGOING = getStatusByType('active.ongoing');
+export const CASE_STATUS_ONGOING = getStatusByType('active:ongoing');
 export const CASE_PROVIDER_VIVA = 'VIVA';
 export const CASE_EXPIRATION_HOURS = 72;
