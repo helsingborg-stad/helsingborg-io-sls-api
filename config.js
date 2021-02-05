6a2c7e631aac35738dca83eb89f238f2e90ab8f5
Joel Bernerman <joel.bernerman@helsingborg.se>
2021-02-05 11:24:26 +0100

    feat(deploy-pipelines) CFN templates for pipeline and deploy scripts and config updates

--- config.js
+++ config.js
@@ -36,16 +36,80 @@ const stageConfigs = {
     vada: {
       envsKeyName: '/vadaEnvs/dev',
     },
+    navet: {
+      envsKeyName: '/navetEnvs/dev',
+    },
+  },
+  stage: {
+    auth: {
+      secrets: {
+        accessToken: {
+          keyName: 'AccessTokenSecret',
+          name: 'stage/token/access/secret',
+        },
+        refreshToken: {
+          keyName: 'RefreshTokenSecret',
+          name: 'stage/token/refresh/secret',
+        },
+        authorizationCode: {
+          keyName: 'AuthorizationCodeSecret',
+          name: 'stage/token/authorization/code/secret',
+        },
+      },
+    },
+    bankId: {
+      envsKeyName: '/bankidEnvs/stage',
+    },
+    users: {
+      tableName: 'users',
+    },
+    forms: {
+      tableName: 'forms',
+    },
+    cases: {
+      tableName: 'cases',
+    },
+    vada: {
+      envsKeyName: '/vadaEnvs/stage',
+    },
+    navet: {
+      envsKeyName: '/navetEnvs/stage',
+    },
   },
   prod: {
+    auth: {
+      secrets: {
+        accessToken: {
+          keyName: 'AccessTokenSecret',
+          name: 'prod/token/access/secret',
+        },
+        refreshToken: {
+          keyName: 'RefreshTokenSecret',
+          name: 'prod/token/refresh/secret',
+        },
+        authorizationCode: {
+          keyName: 'AuthorizationCodeSecret',
+          name: 'prod/token/authorization/code/secret',
+        },
+      },
+    },
     bankId: {
       envsKeyName: '/bankidEnvs/prod',
     },
     users: {
       tableName: 'users',
     },
+    forms: {
+      tableName: 'forms',
+    },
     cases: {
       tableName: 'cases',
+    },
+    vada: {
+      envsKeyName: '/vadaEnvs/prod',
+    },
+    navet: {
+      envsKeyName: '/navetEnvs/prod',
     },
   },
 };
