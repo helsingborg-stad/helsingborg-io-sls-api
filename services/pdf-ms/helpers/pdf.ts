f24136b66a0efe27a03572c57f007f2d61230d54
Dan Nilsson <mail@dannilsson.se>
2021-02-16 07:53:46 +0100

    refactor: 💡 naming, structure, readability

--- services/pdf-ms/helpers/pdf.ts
+++ services/pdf-ms/helpers/pdf.ts
@@ -1,40 +1,47 @@
-import { getPropertyFromDottedString } from './objects';
 import { PDFDocument, StandardFonts, rgb, PDFFont, PDFPage } from 'pdf-lib';
-import { TextNode, Font, Template, AnswerObject } from './types';
 
-const defaultTextColor = rgb(0.05, 0.05, 0.05);
-const changedTextColor = rgb(174 / 255, 11 / 255, 5 / 255);
+import { getPropertyFromDottedString } from './objects';
+import { TextNode, Font, Template, Answer } from './types';
 
-const loadFonts = async (document: PDFDocument): Promise<Record<Font, PDFFont>> => {
-  const courierFont = await document.embedFont(StandardFonts.Courier);
-  const timesRomanFont = await document.embedFont(StandardFonts.TimesRoman);
-  const helveticaFont = await document.embedFont(StandardFonts.Helvetica);
+const DEFAULT_TEXT_COLOR = rgb(0.05, 0.05, 0.05);
+const CHANGED_CASE_VALUE_TEXT_COLOR = rgb(174 / 255, 11 / 255, 5 / 255);
 
-  const fonts = {
-    courier: courierFont,
-    timesRoman: timesRomanFont,
-    helvetica: helveticaFont,
-  };
-  return fonts;
-};
+async function loadPdfDocumentFonts(document: PDFDocument): Promise<Record<Font, PDFFont>> {
+  try {
+    const courierFont = await document.embedFont(StandardFonts.Courier);
+    const timesRomanFont = await document.embedFont(StandardFonts.TimesRoman);
+    const helveticaFont = await document.embedFont(StandardFonts.Helvetica);
 
-/** Draws the text as specified in the textNode on the pdf page */
-const renderTextOnPage = (
+    const fonts = {
+      courier: courierFont,
+      timesRoman: timesRomanFont,
+      helvetica: helveticaFont,
+    };
+
+    return fonts;
+  } catch (PDFDocumentError) {
+    throw new Error(PDFDocumentError.message);
+  }
+}
+
+function drawTextNodeOnPdfPage(
   textNode: TextNode,
   pdfPage: PDFPage,
   fonts: Record<Font, PDFFont>,
   defaultFontSize: number
-) => {
+) {
   const { x, y, text, fontSize, font, color, maxWidthInChars } = textNode;
+
   if (maxWidthInChars && text.length > maxWidthInChars) {
     const numLines = Math.ceil(text.length / maxWidthInChars);
+
     for (let line = 0; line < numLines; line++) {
       pdfPage.drawText(text.substring(maxWidthInChars * line, maxWidthInChars * (line + 1)), {
         x,
         y: y - line * (fontSize * 1.5),
         size: fontSize || defaultFontSize,
         font: fonts[font || 'helvetica'],
-        color: color || defaultTextColor,
+        color: color || DEFAULT_TEXT_COLOR,
       });
     }
   } else {
@@ -43,79 +50,89 @@ const renderTextOnPage = (
       y,
       size: fontSize || defaultFontSize,
       font: fonts[font || 'helvetica'],
-      color: color || defaultTextColor,
+      color: color || DEFAULT_TEXT_COLOR,
     });
   }
-};
+}
 
-const replaceTextInTextNode = (
-  textNode: TextNode,
-  json: Record<string, any>,
-  answerArray: AnswerObject[]
-) => {
-  // This regex matches any string inside double braces, i.e. things like "{{hello}}" or "{{_x_  }}".
-  // Everything inside the braces becomes a capturing group.
-  const regex = /{{(.*?)}}/g;
-  const templateStrings = [...textNode.text.matchAll(regex)];
+function replaceTextInTextNode(textNode: TextNode, json: Record<string, any>, answers: Answer[]) {
+  const captureStringInDoubleBrachesRegexPattern = /{{(.*?)}}/g;
+  const templateStrings = [...textNode.text.matchAll(captureStringInDoubleBrachesRegexPattern)];
   const newTextNode: TextNode = { ...textNode };
 
   const newText = templateStrings.reduce((previous, currentRegexResult) => {
     const [fullMatch, capturingGroup] = currentRegexResult;
     const fieldId = capturingGroup.trim();
     const replacement = getPropertyFromDottedString(json, fieldId);
+
     if (replacement === undefined || replacement === 'undefined') {
       return previous.replace(fullMatch, '');
     }
+
     // Special logic for handling dates, based on the tags for the field including the keyword 'date'
-    const answerObject = answerArray.find(answer => answer.field.id === fieldId);
+    const answerObject = answers.find(answer => answer.field.id === fieldId);
+
     if (answerObject && answerObject.field.tags.includes('date')) {
-      const dateFormatOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
       const date = new Date(answerObject.value);
-      const dateString = date.toLocaleDateString('sv-SE', dateFormatOptions);
-      return previous.replace(fullMatch, dateString);
+      const dateYearMonthDay = date.toLocaleDateString('sv-SE', {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+      });
+
+      return previous.replace(fullMatch, dateYearMonthDay);
     }
 
     return previous.replace(fullMatch, replacement);
   }, textNode.text);
+
   newTextNode.text = newText;
+
   return newTextNode;
-};
+}
 
 /** Takes a base pdf, a templateData object that tells us where to put the texts,
  * and a json from which we import the values. The changedValues and newValues
  * tells us if some values have changed since the last case, and if so lets us
  * mark those values in red.
  */
-export const modifyPdf = async (
+export async function modifyPdf(
   pdfBuffer: Buffer,
   template: Template,
   json: Record<string, any>,
-  answerArray: AnswerObject[],
+  answers: Answer[],
   changedValues?: string[],
   newValues?: string[]
-) => {
-  const document = await PDFDocument.load(pdfBuffer);
-  const pages = document.getPages();
-  const fonts = await loadFonts(document);
+) {
+  try {
+    const document = await PDFDocument.load(pdfBuffer);
+    const pages = document.getPages();
+    const fonts = await loadPdfDocumentFonts(document);
 
-  const replacedTextNodes = template.texts.map(textNode => {
-    const replacedText = replaceTextInTextNode(textNode, json, answerArray);
-    // use the valueId property and the newValues/changedValues to see if the property has changed,
-    // and if so, change its color to mark the change.
-    if (
-      replacedText.valueId &&
-      (newValues.includes(replacedText.valueId) || changedValues.includes(replacedText.valueId))
-    ) {
-      replacedText.color = changedTextColor;
-    }
-    return replacedText;
-  });
+    const replacedTextNodes = template.texts.map(textNode => {
+      const replacedText = replaceTextInTextNode(textNode, json, answers);
 
-  replacedTextNodes.forEach(textNode => {
-    const page = pages[textNode.pageIndex || 0];
-    renderTextOnPage(textNode, page, fonts, template.defaultFontSize);
-  });
+      // use the valueId property and the newValues/changedValues to see if the property has changed,
+      // and if so, change its color to mark the change.
+      if (
+        replacedText.valueId &&
+        (newValues.includes(replacedText.valueId) || changedValues.includes(replacedText.valueId))
+      ) {
+        replacedText.color = CHANGED_CASE_VALUE_TEXT_COLOR;
+      }
 
-  const pdfBytes = await document.save();
-  return Buffer.from(pdfBytes);
-};
+      return replacedText;
+    });
+
+    replacedTextNodes.forEach(textNode => {
+      const page = pages[textNode.pageIndex || 0];
+      drawTextNodeOnPdfPage(textNode, page, fonts, template.defaultFontSize);
+    });
+
+    const pdfDocumentInBytes = await document.save();
+
+    return Buffer.from(pdfDocumentInBytes);
+  } catch (PDFDocumentError) {
+    throw new Error(PDFDocumentError.message);
+  }
+}
