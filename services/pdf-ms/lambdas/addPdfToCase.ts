8fdcad82f530f1fa07291ba28fadc751473bf9bd
Dan Nilsson <mail@dannilsson.se>
2021-02-16 08:02:26 +0100

    feat: 🎸 (wip) adapt new case data struct
    Work in progress...

--- services/pdf-ms/lambdas/addPdfToCase.ts
+++ services/pdf-ms/lambdas/addPdfToCase.ts
@@ -1,45 +1,73 @@
+/* eslint-disable no-console */
 import AWS from 'aws-sdk';
 import to from 'await-to-js';
 
-import { arrayToObject } from '../helpers/caseDataConverter';
 import config from '../../../config.js';
 import * as dynamoDb from '../../../libs/dynamoDb.js';
-import { Template, FormType, Case, formTypes, AnswerObject } from '../helpers/types';
+
+import { Template, FormType, formTypes, Answer } from '../helpers/types';
+import { answersToObject } from '../helpers/caseDataConverter';
 import { loadFileFromBucket, writeFileToBucket } from '../helpers/s3';
 import { modifyPdf } from '../helpers/pdf';
-import { getUser } from '../helpers/user';
-import { getNewAndChangedValues, getApplicantCasesByFormId } from '../helpers/case';
+// import { getUser } from '../helpers/user';
+import { getNewAndChangedValues, getUserCases } from '../helpers/case';
 
 const dynamoDbConverter = AWS.DynamoDB.Converter;
 
-export const main = async (event: Record<string, any>) => {
-  const caseData: Case & { user?: Record<string, any>; answersArray: AnswerObject[] } = parseCase(
+interface UserForm {
+  personalNumber: string;
+  currentFormAnswers: Answer[];
+  currentFormAnswerAsObject: Record<string, any>;
+}
+
+// Get current case answers (event)
+// Get previous (the one before current) case answers
+
+// Get diffs between previous case answers and latest case answers
+//
+
+export async function main(event: Record<string, any>) {
+  // Convert DynamoDB case data to plain object
+  const unMarshalledCase: Record<string, any> = dynamoDbConverter.unmarshall(
     event.detail.dynamodb.NewImage
   );
-  if (caseData.pdfGenerated) {
-    return;
-  }
-  const { formId, PK, SK } = caseData;
-  const personalNumber = PK.substring(5);
-  const user = await getUser(personalNumber);
-  if (user) {
-    caseData.answers.user = user;
+
+  if (unMarshalledCase.pdfGenerated) {
+    return null;
   }
 
+  const currentAndPreviousAnswers: {
+    currentAnswers: Answer[];
+    previousAnswers: Answer[];
+  } = createUserForm(unMarshalledCase);
+
+  // const { personalNumber, currentFormId } = userFrom;
+
+  // const user = await getUser(personalNumber);
+
+  // if (user) {
+  //   userFrom.forms[currentFormId].answers.user = user;
+  // }
+
   // find changed and newly added values, compared to the last case with the same formId.
-  const relevantCases = await getApplicantCasesByFormId(personalNumber, formId);
+  const userCases = await getUserCases(personalNumber);
+
   let changedValues: string[] = [];
   let newValues: string[] = [];
-  if (relevantCases.length > 1) {
-    const sortedCases = relevantCases
+
+  if (userCases) {
+    const sortedCases = userCases
       .filter(c => c.status.type.includes('submitted'))
       .filter(c => c.SK !== SK)
       .sort((c1, c2) => c2.updatedAt - c1.updatedAt);
+
     const newestRelevantCase = sortedCases[0];
-    const newAndChangedValues = getNewAndChangedValues(caseData, newestRelevantCase);
+    const newAndChangedValues = getNewAndChangedValues(userCase, newestRelevantCase);
+
     changedValues = newAndChangedValues.changedValues;
     newValues = newAndChangedValues.newValues;
-    caseData.answers.valuesChanged =
+
+    userCase.forms[currentFormId].answers.valuesChanged =
       changedValues.length > 0 || newValues.length > 0 ? 'Ja' : 'Nej';
   }
 
@@ -56,25 +84,27 @@ export const main = async (event: Record<string, any>) => {
   )) as Buffer;
 
   // add the data to the pdf according to the template
-  const newPdfBuffer = await modifyPdf(
-    pdfBaseFileBuffer,
-    jsonTemplate,
-    caseData.answers,
-    caseData.answersArray,
-    changedValues,
-    newValues
-  );
+  // const newPdfBuffer = await modifyPdf(
+  //   pdfBaseFileBuffer,
+  //   jsonTemplate,
+  //   userCase.answers,
+  //   userCase.answersArray,
+  //   changedValues,
+  //   newValues
+  // );
+
   // save a copy of the generated pdf to the bucket.
   // This is (probably) temporary, to make it easier for us to check the generated pdfs.
-  writeFileToBucket(`cases/${caseData.id}.pdf`, newPdfBuffer);
+  // writeFileToBucket(`cases/${userCase.id}.pdf`, newPdfBuffer);
 
-  const successfullyAddedData = await addPdfToCase(PK, SK, newPdfBuffer);
-  if (!successfullyAddedData) {
-    console.error(`Something went wrong with adding pdf data to case, PK: ${PK}, SK: ${SK}`);
-  }
-};
+  // const successfullyAddedData = await addPdfToCase(PK, SK, newPdfBuffer);
+  // if (!successfullyAddedData) {
+  //   console.error(`Something went wrong with adding pdf data to case, PK: ${PK}, SK: ${SK}`);
+  // }
+}
 
-const getFormType = async (formId: string): Promise<FormType> => {
+// Check if form exists not neede. Cases can't be created without an existing formid
+async function getFormType(formId: string): Promise<FormType> {
   const formPartitionKey = `FORM#${formId}`;
   const params = {
     TableName: config.forms.tableName,
@@ -86,53 +116,42 @@ const getFormType = async (formId: string): Promise<FormType> => {
 
   const [error, queryResponse] = await to(dynamoDb.call('query', params));
   if (error) {
-    console.error('Error in accessing dynamoDb: ', error);
+    console.error('Error in accessing dynamoDb:', error);
     return null;
   }
+
   const dbResponse = queryResponse as { Items: { formType?: string }[]; Count: number };
   if (dbResponse.Count === 0) {
     console.error(`Form with that id (${formId}) not found in the database.`);
     return null;
   }
+
   if (formTypes.includes(dbResponse.Items[0].formType)) {
     return dbResponse.Items[0].formType as FormType;
   }
+
   return null;
-};
+}
 
-const addPdfToCase = async (PK: string, SK: string, pdf?: string | Buffer) => {
-  const UpdateExpression = 'SET #pdf = :pdf, #pdfGenerated = :pdfGenerated';
-  const ExpressionAttributeNames = { '#pdf': 'pdf', '#pdfGenerated': 'pdfGenerated' };
-  const ExpressionAttributeValues = {
-    ':pdf': pdf || 'No pdf to add',
-    ':pdfGenerated': pdf !== undefined ? 'yes' : 'no',
-  };
+function createUserForm(unMarshalledCase: Record<string, any>): UserForm {
+  const {
+    PK,
+    currentFormId,
+    forms: {
+      [currentFormId]: { answers },
+    },
+  } = unMarshalledCase;
 
-  const params = {
-    TableName: config.cases.tableName,
-    Key: { PK, SK },
-    UpdateExpression,
-    ExpressionAttributeNames,
-    ExpressionAttributeValues,
-    ReturnValue: 'ALL_NEW',
-  };
-  const [error] = await to(dynamoDb.call('update', params));
-  if (error) {
-    console.error(error);
-    return false;
-  }
-  return true;
-};
+  let answer: Record<string, any> = {};
 
-const parseCase = (dynamoDbImage: any) => {
-  // Convert DynamoDB case data to plain object
-  const unMarshalledCase = dynamoDbConverter.unmarshall(dynamoDbImage);
-  const { answers } = unMarshalledCase;
-  let answerObject: Record<string, any> = {};
   if (Array.isArray(answers)) {
-    answerObject = arrayToObject(answers);
+    answer = answersToObject(answers);
   }
-  return { ...unMarshalledCase, answers: answerObject, answersArray: answers } as Case & {
-    answersArray: AnswerObject[];
+
+  return {
+    personalNumber: PK.substring(5),
+    currentFormId,
+    currentFormAnswers: answers,
+    currentFormAnswerAsObject: answer,
   };
-};
+}
