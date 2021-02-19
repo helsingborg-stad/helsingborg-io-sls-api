c111821abbf2b7eddf76aed7cbc1193b783bd594
Dan Nilsson <dan.nilsson@helsingborg.se>
2021-02-19 13:26:50 +0100

    refactor: 💡 replace arrow func to vanlia func
    naming, readability

--- services/pdf-ms/helpers/s3.ts
+++ services/pdf-ms/helpers/s3.ts
@@ -1,24 +1,33 @@
-import S3 from 'aws-sdk/clients/s3';
+import { s3Client } from '../../../libs/S3';
 
-const s3Client = new S3();
 const bucketName = process.env.BUCKET_NAME;
 
-export const loadFileFromBucket = async (filename: string) => {
+export async function loadFileFromBucket(Key: string) {
   try {
-    const file = await s3Client
+    const s3File = await s3Client
       .getObject({
         Bucket: bucketName,
-        Key: filename,
+        Key,
       })
       .promise();
-    return file.Body;
-  } catch (error) {
-    throw `S3 bucket: ${bucketName} (filename: ${filename}) - ${error.code}:${error.message}`;
-  }
-};
 
-export const writeFileToBucket = async (Key: string, data: Buffer) => {
-  s3Client.putObject({ Bucket: bucketName, Key, Body: data }, err => {
-    console.log(err);
-  });
-};
+    return s3File.Body;
+  } catch (s3ClientGetError) {
+    throw new Error(`S3 - ${s3ClientGetError.code}: ${s3ClientGetError.message}`);
+  }
+}
+
+export async function writeFileToBucket(Key: string, Body: Buffer) {
+  try {
+    const s3PutFileResult = await s3Client
+      .putObject({
+        Bucket: bucketName,
+        Key,
+        Body,
+      })
+      .promise();
+    return s3PutFileResult;
+  } catch (s3ClientPutError) {
+    throw new Error(`S3 - ${s3ClientPutError.code}: ${s3ClientPutError.message}`);
+  }
+}
