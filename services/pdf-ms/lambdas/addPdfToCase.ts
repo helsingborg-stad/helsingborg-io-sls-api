00250a21819c987eb3568dd0eb2c8fe98a893dfa
Dan Nilsson <dan.nilsson@helsingborg.se>
2021-02-23 09:55:26 +0100

    fix: 🐛 (wip) adapt new case data struct

--- services/pdf-ms/lambdas/addPdfToCase.ts
+++ services/pdf-ms/lambdas/addPdfToCase.ts
@@ -3,28 +3,19 @@ import AWS from 'aws-sdk';
 import to from 'await-to-js';
 
 import config from '../../../config.js';
+import params from '../../../libs/params';
 import * as dynamoDb from '../../../libs/dynamoDb.js';
 
-import { Template, FormType, formTypes, Answer } from '../helpers/types';
-import { answersToObject } from '../helpers/caseDataConverter';
+import { Template, Case } from '../helpers/types';
+import { arrayToObject } from '../helpers/caseDataConverter';
 import { loadFileFromBucket, writeFileToBucket } from '../helpers/s3';
 import { modifyPdf } from '../helpers/pdf';
-// import { getUser } from '../helpers/user';
+import { getUser } from '../helpers/user';
 import { getNewAndChangedValues, getUserCases } from '../helpers/case';
 
+const PDF_SSM_PARAMS = params.read(config.pdf.envsKeyName);
+
 const dynamoDbConverter = AWS.DynamoDB.Converter;
-
-interface UserForm {
-  personalNumber: string;
-  currentFormAnswers: Answer[];
-  currentFormAnswerAsObject: Record<string, any>;
-}
-
-// Get current case answers (event)
-// Get previous (the one before current) case answers
-
-// Get diffs between previous case answers and latest case answers
-//
 
 export async function main(event: Record<string, any>) {
   // Convert DynamoDB case data to plain object
@@ -32,126 +23,110 @@ export async function main(event: Record<string, any>) {
     event.detail.dynamodb.NewImage
   );
 
-  if (unMarshalledCase.pdfGenerated) {
-    return null;
+  if (unMarshalledCase.pdfGenerated === 'yes') {
+    throw 'PDF allready generated';
   }
 
-  const currentAndPreviousAnswers: {
-    currentAnswers: Answer[];
-    previousAnswers: Answer[];
-  } = createUserForm(unMarshalledCase);
+  const { personalNumber } = unMarshalledCase;
+  const ssmParams = await PDF_SSM_PARAMS;
 
-  // const { personalNumber, currentFormId } = userFrom;
-
-  // const user = await getUser(personalNumber);
-
-  // if (user) {
-  //   userFrom.forms[currentFormId].answers.user = user;
-  // }
-
-  // find changed and newly added values, compared to the last case with the same formId.
-  const userCases = await getUserCases(personalNumber);
-
-  let changedValues: string[] = [];
-  let newValues: string[] = [];
-
-  if (userCases) {
-    const sortedCases = userCases
-      .filter(c => c.status.type.includes('submitted'))
-      .filter(c => c.SK !== SK)
-      .sort((c1, c2) => c2.updatedAt - c1.updatedAt);
-
-    const newestRelevantCase = sortedCases[0];
-    const newAndChangedValues = getNewAndChangedValues(userCase, newestRelevantCase);
-
-    changedValues = newAndChangedValues.changedValues;
-    newValues = newAndChangedValues.newValues;
-
-    userCase.forms[currentFormId].answers.valuesChanged =
-      changedValues.length > 0 || newValues.length > 0 ? 'Ja' : 'Nej';
+  const [getUserCasesError, userCases] = await to(getUserCases(personalNumber));
+  if (getUserCasesError) {
+    console.error(getUserCasesError);
+    throw getUserCasesError;
   }
 
-  // get the correct template
-  const formType = await getFormType(formId);
-  const templates = JSON.parse((await loadFileFromBucket('templateFiles.json')).toString());
-  const templateFiles = templates[Object.keys(templates).includes(formType) ? formType : 'default'];
+  if (userCases.length === 0) {
+    throw 'User cases not found';
+  }
+
+  const [currentCase, previousCase] = sortCasesByDate(userCases, 2);
+
+  const currentCaseAnswers = currentCase.forms[ssmParams.recurringFormId].answers;
+  const previousCaseAnswers = previousCase.forms[ssmParams.recurringFormId].answers;
+
+  const [getUserError, user] = await to(getUser(personalNumber));
+  if (getUserError) {
+    console.error(getUserError);
+    throw getUserError;
+  }
+
+  const { changedValues, newValues } = getNewAndChangedValues(
+    currentCaseAnswers,
+    previousCaseAnswers
+  );
+
+  const pdfJsonValues = {
+    answers: {
+      ...arrayToObject(currentCaseAnswers),
+      user,
+      valuesChanged: changedValues.length > 0 || newValues.length > 0 ? 'Ja' : 'Nej',
+    },
+  };
+
+  const templates = ssmParams.templatesFilenames;
+  const ekbTemplateFiles = templates['EKB-recurring'];
 
   // load the template files from the s3 bucket
-  const templateFile = await loadFileFromBucket(templateFiles.JSONTemplateFilename);
+  const templateFile = await loadFileFromBucket(ekbTemplateFiles.JSONTemplateFilename);
   const jsonTemplate: Template = JSON.parse(templateFile.toString()) as Template;
   const pdfBaseFileBuffer: Buffer = (await loadFileFromBucket(
-    templateFiles.pdfBaseFilename
+    ekbTemplateFiles.pdfBaseFilename
   )) as Buffer;
 
-  // add the data to the pdf according to the template
-  // const newPdfBuffer = await modifyPdf(
-  //   pdfBaseFileBuffer,
-  //   jsonTemplate,
-  //   userCase.answers,
-  //   userCase.answersArray,
-  //   changedValues,
-  //   newValues
-  // );
+  //add the data to the pdf according to the template
+  const newPdfBuffer = await modifyPdf(
+    pdfBaseFileBuffer,
+    jsonTemplate,
+    pdfJsonValues,
+    currentCaseAnswers,
+    changedValues,
+    newValues
+  );
 
   // save a copy of the generated pdf to the bucket.
   // This is (probably) temporary, to make it easier for us to check the generated pdfs.
-  // writeFileToBucket(`cases/${userCase.id}.pdf`, newPdfBuffer);
+  writeFileToBucket(`cases/${currentCase.id}.pdf`, newPdfBuffer);
 
-  // const successfullyAddedData = await addPdfToCase(PK, SK, newPdfBuffer);
-  // if (!successfullyAddedData) {
-  //   console.error(`Something went wrong with adding pdf data to case, PK: ${PK}, SK: ${SK}`);
-  // }
+  const successfullyAddedData = await addPdfToCase(currentCase, newPdfBuffer);
+  if (!successfullyAddedData) {
+    console.error(
+      `Something went wrong with adding pdf data to case, PK: ${currentCase.PK}, SK: ${currentCase.SK}`
+    );
+  }
 }
 
-// Check if form exists not neede. Cases can't be created without an existing formid
-async function getFormType(formId: string): Promise<FormType> {
-  const formPartitionKey = `FORM#${formId}`;
+async function addPdfToCase(currentCase: Case, pdf?: string | Buffer) {
+  const UpdateExpression = 'SET #pdf = :pdf, #pdfGenerated = :pdfGenerated';
+  const ExpressionAttributeNames = { '#pdf': 'pdf', '#pdfGenerated': 'pdfGenerated' };
+  const ExpressionAttributeValues = {
+    ':pdf': pdf || undefined,
+    ':pdfGenerated': pdf !== undefined ? 'yes' : 'no',
+  };
+
   const params = {
-    TableName: config.forms.tableName,
-    KeyConditionExpression: 'PK = :pk',
-    ExpressionAttributeValues: {
-      ':pk': formPartitionKey,
+    TableName: config.cases.tableName,
+    Key: {
+      PK: currentCase.PK,
+      SK: currentCase.SK,
     },
+    UpdateExpression,
+    ExpressionAttributeNames,
+    ExpressionAttributeValues,
+    ReturnValue: 'ALL_NEW',
   };
 
-  const [error, queryResponse] = await to(dynamoDb.call('query', params));
+  const [error] = await to(dynamoDb.call('update', params));
   if (error) {
-    console.error('Error in accessing dynamoDb:', error);
-    return null;
+    console.error(error);
+    return false;
   }
 
-  const dbResponse = queryResponse as { Items: { formType?: string }[]; Count: number };
-  if (dbResponse.Count === 0) {
-    console.error(`Form with that id (${formId}) not found in the database.`);
-    return null;
-  }
-
-  if (formTypes.includes(dbResponse.Items[0].formType)) {
-    return dbResponse.Items[0].formType as FormType;
-  }
-
-  return null;
+  return true;
 }
 
-function createUserForm(unMarshalledCase: Record<string, any>): UserForm {
-  const {
-    PK,
-    currentFormId,
-    forms: {
-      [currentFormId]: { answers },
-    },
-  } = unMarshalledCase;
-
-  let answer: Record<string, any> = {};
-
-  if (Array.isArray(answers)) {
-    answer = answersToObject(answers);
-  }
-
-  return {
-    personalNumber: PK.substring(5),
-    currentFormId,
-    currentFormAnswers: answers,
-    currentFormAnswerAsObject: answer,
-  };
+function sortCasesByDate(cases: Case[], limit: number): Case[] {
+  return cases
+    .sort((caseA: Case, caseB: Case) => caseB.updatedAt - caseA.updatedAt)
+    .slice(0, limit);
 }
