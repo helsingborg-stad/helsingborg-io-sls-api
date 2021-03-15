8eb1d0c4ba83c8dd8e81e7d66b6199382a4f92fc
Teddy Andersson <teddyandersson95@gmail.com>
2021-03-15 17:48:33 +0100

    refactor(viva-ms): moving get viva application request to helper function
    The viva application request towards the viva adapter have been moved to an helper function. This
    have been done to reduce redundancy and to avoid timeout bugs when sending requests to the viva
    adapter

--- services/viva-ms/lambdas/createVivaCase.js
+++ services/viva-ms/lambdas/createVivaCase.js
@@ -1,17 +1,17 @@
 /* eslint-disable no-console */
 import to from 'await-to-js';
-import { throwError } from '@helsingborg-stad/npm-api-error-handling';
 import uuid from 'uuid';
 
 import config from '../../../config';
 import params from '../../../libs/params';
 import hash from '../../../libs/helperHashEncode';
-import * as request from '../../../libs/request';
 import { putItem } from '../../../libs/queries';
 import * as dynamoDB from '../../../libs/dynamoDb';
 import { CASE_PROVIDER_VIVA } from '../../../libs/constants';
 import { getStatusByType } from '../../../libs/caseStatuses';
 import { getApplicationStatus, isApplicationStatusCorrect } from '../helpers/applicationStatus';
+
+import vivaAdapter from '../helpers/vivaAdapterRequestClient';
 
 const VADA_SSM_PARAMS = params.read(config.vada.envsKeyName);
 const VIVA_CASE_SSM_PARAMS = params.read(config.cases.providers.viva.envsKeyName);
@@ -49,26 +49,26 @@ export async function main(event) {
     );
   }
 
-  const [myPagesError, myPagesResponse] = await to(sendMyPagesReguest(user.personalNumber));
-  if (myPagesError) {
-    return console.error('(Viva-ms) Viva My Pages Request', applicationStatusError);
+  const [getApplicationError, application] = await to(
+    vivaAdapter.application.get(user.personalNumber)
+  );
+  if (getApplicationError) {
+    return console.error('(Viva-ms) Viva Get Application Request', getApplicationError);
   }
 
-  const vivaApplication = myPagesResponse.person.application.vivaapplication;
-
-  if (!vivaApplication || !vivaApplication.period) {
+  if (!application || !application.period) {
     return console.error('(Viva-ms) Viva Application Period not present in response, aborting');
   }
 
-  if (!vivaApplication || !vivaApplication.workflowid) {
+  if (!application || !application.workflowid) {
     return console.error(
-      `(Viva-ms) Viva Application WorkflowId ${vivaApplication.workflowid} not present in response, aborting`
+      `(Viva-ms) Viva Application WorkflowId ${application.workflowid} not present in response, aborting`
     );
   }
 
   const casePartitionKey = `USER#${user.personalNumber}`;
   const [queryCasesError, queryCaseItems] = await to(
-    queryCasesWithWorkflowId(casePartitionKey, vivaApplication.workflowid)
+    queryCasesWithWorkflowId(casePartitionKey, application.workflowid)
   );
   if (queryCasesError) {
     return console.error('(Viva-ms) DynamoDb query on cases tabel failed', queryCasesError);
@@ -79,12 +79,12 @@ export async function main(event) {
   }
 
   const period = {
-    startDate: Date.parse(vivaApplication.period.start),
-    endDate: Date.parse(vivaApplication.period.end),
+    startDate: Date.parse(application.period.start),
+    endDate: Date.parse(application.period.end),
   };
 
   const [putItemError] = await to(
-    putRecurringVivaCase(casePartitionKey, vivaApplication.workflowid, period)
+    putRecurringVivaCase(casePartitionKey, application.workflowid, period)
   );
   if (putItemError) {
     return console.error('(viva-ms) syncApplicationStatus', putItemError);
@@ -159,37 +159,4 @@ async function putRecurringVivaCase(PK, workflowId, period) {
   }
 
   return caseItem;
-}
-
-async function sendMyPagesReguest(personalNumber) {
-  const ssmParams = await VADA_SSM_PARAMS;
-
-  const { hashSalt, hashSaltLength } = ssmParams;
-  const hashedPersonalNumber = hash.encode(personalNumber, hashSalt, hashSaltLength);
-
-  const { vadaUrl, xApiKeyToken } = ssmParams;
-  const authorizedRequestClient = request.requestClient({}, { 'x-api-key': xApiKeyToken });
-
-  const vadaMypagesApplicationUrl = `${vadaUrl}/mypages/${hashedPersonalNumber}`;
-
-  const [requestError, vadaResponse] = await to(
-    request.call(authorizedRequestClient, 'get', vadaMypagesApplicationUrl)
-  );
-
-  if (requestError) {
-    if (requestError.response) {
-      // The request was made and the server responded with a
-      // status code that falls out of the range of 2xx
-      throwError(requestError.response.status, requestError.response.data.message);
-    } else if (requestError.request) {
-      // The request was made but no response was received
-      // `error.request` is an instance of http.ClientRequest in node.js
-      throwError(500, requestError.request.message);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      throwError(500, requestError.message);
-    }
-  }
-
-  return vadaResponse.data;
 }
