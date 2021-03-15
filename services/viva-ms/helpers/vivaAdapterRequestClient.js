c099c409c2de67c0db5b3716e4231767cf429a32
Dan Nilsson <mail@dannilsson.se>
2021-03-15 16:44:32 +0100

    fix: 🐛 timeout issue
    improved handling viva completion post request. add helper function to
    get DRY

---
+++ services/viva-ms/helpers/vivaAdapterRequestClient.js
@@ -0,0 +1,69 @@
+import * as request from '../../../libs/request';
+import { to } from 'await-to-js';
+import params from '../../../libs/params';
+import config from '../../../config';
+import hash from '../../../libs/helperHashEncode';
+
+const VADA_SSM_PARAMS = params.read(config.vada.envsKeyName);
+
+const REQUEST_TIMEOUT_IN_MS = 15000;
+
+async function sendVivaAdapterRequest(requestParams) {
+  const { endpoint, method, body } = requestParams;
+  const { vadaUrl, xApiKeyToken } = await VADA_SSM_PARAMS;
+
+  const requestClient = request.requestClient(
+    {},
+    { 'x-api-key': xApiKeyToken },
+    REQUEST_TIMEOUT_IN_MS
+  );
+
+  const [requestError, response] = await to(
+    request.call(requestClient, method, `${vadaUrl}/${endpoint}`, body)
+  );
+
+  if (requestError) {
+    if (requestError.response) {
+      // The request was made and the server responded with a
+      // status code that falls out of the range of 2xx
+      throw requestError.response;
+    } else if (requestError.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of http.ClientRequest in node.js
+      throw requestError.request;
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      throw requestError.message;
+    }
+  }
+
+  return response.data;
+}
+
+async function postCompletion(payload) {
+  const { personalNumber, workflowId, attachments } = payload;
+
+  const { hashSalt, hashSaltLength } = await VADA_SSM_PARAMS;
+
+  const hashedPersonalNumber = hash.encode(personalNumber, hashSalt, hashSaltLength);
+
+  const requestParams = {
+    endpoint: `applications/${hashedPersonalNumber}/completions`,
+    method: 'post',
+    body: {
+      workflowId,
+      attachments,
+    },
+  };
+
+  const [sendVivaAdapterRequestError, response] = await to(sendVivaAdapterRequest(requestParams));
+  if (sendVivaAdapterRequestError) {
+    throw sendVivaAdapterRequestError;
+  }
+
+  return response;
+}
+
+export default {
+  completion: { post: postCompletion },
+};
