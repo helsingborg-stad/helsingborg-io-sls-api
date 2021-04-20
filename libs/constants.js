8240b396d4ae6382a9e0d3c578d6627916fcb437
Sven65 <Sven65@users.noreply.github.com>
2021-04-20 10:22:21 +0200

    fix(api): 🐛 Make case expiry 45 days (1080 hours) instead of 72 hours

--- libs/constants.js
+++ libs/constants.js
@@ -3,4 +3,4 @@ import { getStatusByType } from '../libs/caseStatuses';
 export const CASE_ITEM_TYPE = 'CASE';
 export const CASE_STATUS_ONGOING = getStatusByType('active:ongoing');
 export const CASE_PROVIDER_VIVA = 'VIVA';
-export const CASE_EXPIRATION_HOURS = 72;
+export const CASE_EXPIRATION_HOURS = 1080; // 45 days * 24 hours = 1080 hours
