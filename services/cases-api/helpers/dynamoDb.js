d7472ac2084c93398c2bc67acd4ac6e41d55453e
Dan Nilsson <mail@dannilsson.se>
2021-06-15 22:45:18 +0200

    feat: 🎸 add getUserCase helpers
    make it possible to get correct user case regardless of who logs in

--- services/cases-api/helpers/dynamoDb.js
+++ services/cases-api/helpers/dynamoDb.js
@@ -1,23 +1,64 @@
+/* eslint-disable no-console */
 import to from 'await-to-js';
 import * as dynamoDb from '../../../libs/dynamoDb';
 import config from '../../../config';
 
-export async function getUserApplicantCase(keys) {
-  const { PK, SK } = keys;
-  const queryParams = {
+export async function getUserCase(personalNumber, id) {
+  const [getUserApplicantCaseError, userApplicantCaseResult] = await to(
+    getUserApplicantCase(personalNumber, id)
+  );
+  if (getUserApplicantCaseError) {
+    throw getUserApplicantCaseError;
+  }
+
+  const [getCoApplicantCaseError, userCoApplicantCaseResult] = await to(
+    getUserCoApplicantCase(personalNumber, id)
+  );
+  if (getCoApplicantCaseError) {
+    throw getCoApplicantCaseError;
+  }
+
+  const concatAndDeDuplicateCase = (...userCaseList) => [...new Set([].concat(...userCaseList))];
+  const userCaseList = concatAndDeDuplicateCase(
+    userApplicantCaseResult.Items,
+    userCoApplicantCaseResult.Items
+  );
+
+  const [userCase] = userCaseList.filter(Boolean);
+  return userCase;
+}
+
+async function getUserApplicantCase(personalNumber, id) {
+  const PK = `USER#${personalNumber}`;
+  const SK = `CASE#${id}`;
+
+  const params = {
     TableName: config.cases.tableName,
+    KeyConditionExpression: 'PK = :pk AND SK = :sk',
     ExpressionAttributeValues: {
       ':pk': PK,
       ':sk': SK,
     },
-    KeyConditionExpression: 'PK = :pk AND SK = :sk',
     Limit: 1,
   };
 
-  const [queryError, queryResult] = await to(dynamoDb.call('query', queryParams));
-  if (queryError) {
-    throw queryError;
-  }
+  return dynamoDb.call('query', params);
+}
 
-  return queryResult.Items[0] || null;
+async function getUserCoApplicantCase(personalNumber, id) {
+  const GSI1 = `USER#${personalNumber}`;
+  const SK = `CASE#${id}`;
+
+  const params = {
+    TableName: config.cases.tableName,
+    IndexName: 'GSI1-SK-index',
+    KeyConditionExpression: 'GSI1 = :gsi1 AND SK = :sk',
+    ExpressionAttributeValues: {
+      ':gsi1': GSI1,
+      ':sk': SK,
+    },
+    Limit: 1,
+  };
+
+  return dynamoDb.call('query', params);
 }
