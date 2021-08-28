c234a0e124ba2f846492bd2dd3016224aa155865
Dan Nilsson <mail@dannilsson.se>
2021-08-28 16:50:21 +0200

    fix: 🐛 Throw 404 if posts is less than 2
    Add clearer exception error if navet response lack posts

--- services/navet-ms/helpers/parser.js
+++ services/navet-ms/helpers/parser.js
@@ -1,4 +1,5 @@
 import parser from 'xml2js';
+import { ResourceNotFoundError } from '@helsingborg-stad/npm-api-error-handling';
 
 export const parseXml =
   params => `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v1="${params.personpostXmlEnvUrl}">
@@ -18,20 +19,26 @@ export const parseJSON = input =>
   new Promise((resolve, reject) => {
     try {
       const posts = input.split('Folkbokforingsposter>');
-      // If result has any posts the length will be bigger then 2.
-      if (posts.length >= 2) {
-        const parsedTwice = posts[1].split('</ns0:');
 
-        const options = {
-          trim: true,
-          explicitArray: false,
-        };
-        parser.parseString(parsedTwice[0], options, (error, result) => {
-          if (error) throw error;
-          resolve(result);
-        });
+      // If result has any posts the length of posts will more than 2
+      if (posts.length < 2) {
+        throw new ResourceNotFoundError();
       }
-      resolve(input);
+
+      const parsedTwice = posts[1].split('</ns0:');
+
+      const options = {
+        trim: true,
+        explicitArray: false,
+      };
+
+      parser.parseString(parsedTwice[0], options, (error, result) => {
+        if (error) {
+          throw error;
+        }
+
+        resolve(result);
+      });
     } catch (error) {
       reject(error);
     }
