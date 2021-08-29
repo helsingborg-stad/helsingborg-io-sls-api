f14f8749c52fdef31a3ea53eef78159d426cb63e
Dan Nilsson <mail@dannilsson.se>
2021-08-29 23:46:15 +0200

    refactor: 💡 general code clean up
    Replace rest error handler package with console.log. Update naming for
    readability reasons.

--- services/users-ms/lambdas/createUser.js
+++ services/users-ms/lambdas/createUser.js
@@ -1,14 +1,23 @@
-import { throwError } from '@helsingborg-stad/npm-api-error-handling';
+/* eslint-disable no-console */
 import to from 'await-to-js';
+
 import uuid from 'uuid';
 import config from '../../../config';
 import * as dynamoDb from '../../../libs/dynamoDb';
-import * as response from '../../../libs/response';
 
-// create user
-export const main = async event => {
-  const data = event.detail;
+export async function main(event) {
+  const userDetail = event.detail;
 
+  const [putUserRequestError, putUserResponse] = await to(putUserRequest(userDetail));
+  if (putUserRequestError) {
+    return console.error('(users-ms) putUserRequestError', putUserRequestError);
+  }
+
+  console.info('(users-ms) putUserResponse', putUserResponse);
+  return true;
+}
+
+async function putUserRequest(userDetail) {
   const params = {
     TableName: config.users.tableName,
     Item: {
@@ -16,24 +25,9 @@ export const main = async event => {
       createdAt: Date.now(),
       email: null,
       mobilePhone: null,
-      ...data,
+      ...userDetail,
     },
   };
 
-  const [error, createUserResponse] = await to(sendCreateUserRequest(params));
-  if (!createUserResponse) return response.failure(error);
-
-  return response.success(201, {
-    type: 'createUser',
-    attributes: {
-      ...createUserResponse.rawParams.Item,
-    },
-  });
-};
-
-async function sendCreateUserRequest(params) {
-  const [error, createUserResponse] = await to(dynamoDb.call('put', params));
-  if (!createUserResponse) throwError(error);
-
-  return createUserResponse;
+  return dynamoDb.call('put', params);
 }
