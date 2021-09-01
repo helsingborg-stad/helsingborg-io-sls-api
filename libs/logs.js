55b911d316a6731b59f906082e664e8e2994bd8c
Joel Bernerman <joel.bernerman@helsingborg.se>
2021-09-01 13:54:28 +0200

    fix: prettier fixes

--- libs/logs.js
+++ libs/logs.js
@@ -8,25 +8,29 @@ const logger = winston.createLogger({
 });
 
 export const log = (level, message, requestId, errorCode, customData = {}) => {
-  logger.log(level, message, { errorCode: errorCode, requestId: requestId, customData: customData });
+  logger.log(level, message, {
+    errorCode: errorCode,
+    requestId: requestId,
+    customData: customData,
+  });
 };
 
 export const logError = (message, requestId, errorCode, customData = {}) => {
-  log('error', message, requestId, errorCode, customData)
-}
+  log('error', message, requestId, errorCode, customData);
+};
 
 export const logWarn = (message, requestId, errorCode, customData = {}) => {
-  log('warn', message, requestId, errorCode, customData)
-}
+  log('warn', message, requestId, errorCode, customData);
+};
 
 export const logInfo = (message, requestId, errorCode, customData = {}) => {
-  log('info', message, requestId, errorCode, customData)
-}
+  log('info', message, requestId, errorCode, customData);
+};
 
 export const logVerbose = (message, requestId, errorCode, customData = {}) => {
-  log('verbose', message, requestId, errorCode, customData)
-}
+  log('verbose', message, requestId, errorCode, customData);
+};
 
 export const logDebug = (message, requestId, errorCode, customData = {}) => {
-  log('debug', message, requestId, errorCode, customData)
-}
+  log('debug', message, requestId, errorCode, customData);
+};
