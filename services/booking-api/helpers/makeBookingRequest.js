8e575e6564f61978257d6f63978b4b6abe80ced1
Karl <karl.mosenbacher@knowit.se>
2021-09-20 12:29:33 +0200

    fix: fix reviewcomments for makebookingrequest

--- services/booking-api/helpers/makeBookingRequest.js
+++ services/booking-api/helpers/makeBookingRequest.js
@@ -1,15 +1,15 @@
+import to from 'await-to-js';
+
 import * as request from '../../../libs/request';
 
-export default async (url, apikey, body) => {
-  try {
-    const requestClient = request.requestClient(
-      { rejectUnauthorized: false },
-      { 'X-ApiKey': apikey }
-    );
+export async function makeBookingRequest(url, apikey, body) {
+  const requestClient = request.requestClient(
+    { rejectUnauthorized: false },
+    { 'X-ApiKey': apikey }
+  );
 
-    const response = await request.call(requestClient, 'post', url, body);
-    return response;
-  } catch (error) {
-    throw new Error(error);
-  }
-};
+  const [error, response] = await to(request.call(requestClient, 'post', url, body));
+  if (error) throw error;
+
+  return response;
+}
