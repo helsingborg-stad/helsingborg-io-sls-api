65deabb2083f3d986e383d13c09b4863e092505c
Karl <karl.mosenbacher@knowit.se>
2021-09-20 15:49:57 +0200

    fix: use function keyword instead of arrow function when defining lambda function

--- services/booking-api/lambdas/update.js
+++ services/booking-api/lambdas/update.js
@@ -6,7 +6,7 @@ import * as response from '../../../libs/response';
 import { getSsmParameters } from '../helpers/getSsmParameters';
 import { sendBookingPostRequest } from '../helpers/sendBookingPostRequest';
 
-export const main = async event => {
+export async function main(event) {
   const bookingId = event.pathParameters.id;
 
   const [error, ssmParameters] = await to(getSsmParameters());
@@ -29,4 +29,4 @@ export const main = async event => {
 
   const { data } = createBookingResponse.data;
   return response.success(200, data);
-};
+}
