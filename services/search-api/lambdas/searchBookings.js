df6060c1d2a803849e96108e2c4db730d426f769
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-09-30 14:16:46 +0200

    fix: adhere to if statement styling

--- services/search-api/lambdas/searchBookings.js
+++ services/search-api/lambdas/searchBookings.js
@@ -9,7 +9,9 @@ export async function main(event) {
   const body = { ...JSON.parse(event.body) };
 
   const [requestError, getSearchResponse] = await to(searchBookings(body));
-  if (requestError) throwError(requestError.status, requestError.errorMessage);
+  if (requestError) {
+    throwError(requestError.status, requestError.errorMessage);
+  }
 
   const { data } = getSearchResponse.data;
   return response.success(200, data);
