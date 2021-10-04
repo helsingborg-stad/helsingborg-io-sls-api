549946aa4011bd58dc04fa22cede2c91b34c1ff8
Dan Nilsson <mail@dannilsson.se>
2021-10-04 20:45:58 +0200

    wip: personApplicationStatus - init commit
    add new lambda
    
    handle checking if viva person is allowed to apply or if completion is required

---
+++ services/viva-ms/lambdas/personApplicationStatus.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-console */
+import to from 'await-to-js';
+
+import log from '../../../libs/logs';
+
+import putVivaMsEvent from '../helpers/putVivaMsEvent';
+import vivaAdapter from '../helpers/vivaAdapterRequestClient';
+import validateApplicationStatus from '../helpers/validateApplicationStatus';
+
+export async function main(event, context) {
+  const clientUser = event.detail;
+
+  const [applicationStatusError, applicationStatusList] = await to(
+    vivaAdapter.application.status(clientUser.personalNumber)
+  );
+  if (applicationStatusError) {
+    log.error(
+      'Vada: applicationStatusError',
+      context.awsRequestId,
+      'service-viva-ms-personApplicationStatus-001',
+      applicationStatusError
+    );
+    return false;
+  }
+
+  /**
+   * The combination of status codes 1, 128, 256, 512
+   * determines if a Viva application workflow is open for applicant.
+   *
+   * 1 - Application(period) is open for applicant,
+   * 128 - Case exsits in Viva
+   * 256 - An active e-application is activated in Viva
+   * 512 - Application allows e-application
+   */
+  const requiredStatusCodes = [1, 128, 256, 512];
+  if (!validateApplicationStatus(applicationStatusList, requiredStatusCodes)) {
+    log.info(
+      'validateApplicationStatus. No application period open.',
+      context.awsRequestId,
+      'service-viva-ms-personApplicationStatus-003',
+      applicationStatusList
+    );
+    return false;
+  }
+
+  const [putEventError] = await to(putVivaMsEvent.statusApplySuccess(applicationStatusList));
+  if (putEventError) {
+    log.error(
+      'putEventError: statusApplySuccess',
+      context.awsRequestId,
+      'service-viva-ms-personApplicationStatus-002',
+      putEventError
+    );
+    return false;
+  }
+
+  return true;
+}
