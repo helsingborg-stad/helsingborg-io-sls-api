58844c93b31c77659a6c1b5c936006104ed6caef
Teddy Andersson <teddyandersson95@gmail.com>
2021-10-13 15:26:17 +0200

    feat(cases-ms): a new service for non api related case lambdas
    A new service have been created in for non api related case related lambdas. The first lambda in
    this service is used to update pdf attributes on a case when a pdf generate event have been put on
    the aws eventbridge. The lambda retrives an pdf from s3, gets the related case from dynamodb and
    updates the pdf attributes on the retrived case.

---
+++ services/case-ms/lambdas/updatePdf.js
@@ -0,0 +1,85 @@
+import { s3Client } from '../../../libs/S3';
+import * as dynamoDb from '../../../libs/dynamoDb';
+import config from '../../../config';
+import { to } from 'await-to-js';
+
+export async function main(event) {
+  const { resourceId, pdfStorageBucketKey } = event.details;
+
+  const [s3GetObjectError, pdfObject] = await to(
+    s3Client
+      .getObject({
+        Bucket: process.env.PDF_STORAGE_BUCKET_NAME,
+        Key: pdfStorageBucketKey,
+      })
+      .promise()
+  );
+  if (s3GetObjectError) {
+    console.error(s3GetObjectError);
+    return false;
+  }
+
+  const [scanCasesByIdError, scanCasesResult] = await to(scanCasesById(resourceId));
+  if (scanCasesByIdError) {
+    console.error(scanCasesByIdError);
+    return false;
+  }
+
+  const currentCase = scanCasesResult[1].Items;
+
+  const [updateCasePdfAttributesError] = await to(
+    updateCasePdfAttributes(currentCase, pdfObject.Body)
+  );
+  if (updateCasePdfAttributesError) {
+    console.error(updateCasePdfAttributesError);
+    return false;
+  }
+}
+
+async function scanCasesById(caseId) {
+  const scanParams = {
+    TableName: config.cases.tableName,
+    FilterExpression: '#id = :id',
+    ExpressionAttributeNames: {
+      '#id': 'id',
+    },
+    ExpressionAttributeValues: {
+      ':id': caseId,
+    },
+  };
+
+  const [scanError, scanResult] = await to(dynamoDb.call('scan', scanParams));
+  if (scanError) {
+    throw scanError;
+  }
+
+  return scanResult;
+}
+
+async function updateCasePdfAttributes(currentCase, pdf) {
+  const UpdateExpression = 'SET #pdf = :pdf, #pdfGenerated = :pdfGenerated';
+  const ExpressionAttributeNames = { '#pdf': 'pdf', '#pdfGenerated': 'pdfGenerated' };
+  const ExpressionAttributeValues = {
+    ':pdf': pdf || undefined,
+    ':pdfGenerated': pdf !== undefined ? 'yes' : 'no',
+  };
+
+  const params = {
+    TableName: config.cases.tableName,
+    Key: {
+      PK: currentCase.PK,
+      SK: currentCase.SK,
+    },
+    UpdateExpression,
+    ExpressionAttributeNames,
+    ExpressionAttributeValues,
+    ReturnValue: 'NONE',
+  };
+
+  const [dynamoDbUpdateCaseError] = await to(dynamoDb.call('update', params));
+  if (dynamoDbUpdateCaseError) {
+    throw dynamoDbUpdateCaseError;
+  }
+
+  return true;
+}
