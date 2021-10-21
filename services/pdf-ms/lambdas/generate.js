964bd18f8998f20eb3eda027889d4123c60edfe7
Teddy Andersson <teddyandersson95@gmail.com>
2021-10-21 11:23:57 +0200

    refactor(pdf-ms/generate): updated data attribute for bucket key in the put event action

--- services/pdf-ms/lambdas/generate.js
+++ services/pdf-ms/lambdas/generate.js
@@ -49,7 +49,11 @@ export async function main(event) {
 
   if (pdfOutputObject) {
     const [putEventError] = await to(
-      putEvent({ resourceId, pdfOutputObject }, 'PdfMsGenerateSuccess', 'pdfMs.generate')
+      putEvent(
+        { resourceId, pdfStorageBucketKey: pdfBucketKey },
+        'PdfMsGenerateSuccess',
+        'pdfMs.generate'
+      )
     );
     if (putEventError) {
       console.error(putEventError);
