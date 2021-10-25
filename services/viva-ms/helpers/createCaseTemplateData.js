89e32ae90729189d15d618a35009685818c1bb8c
Teddy Andersson <teddyandersson95@gmail.com>
2021-10-25 13:37:58 +0200

    feat(generaterecurringcasehtml): new lambda for generating html from case with handlebar template
    This new lambda generates html from case data with handlebars. The lambda triggers on a dynamodb
    events that fires on case modifications where the case provider is VIVA and the status is set to
    active:submitted. The lambda then retrives a handlebars template file from an s3 bucket, converts
    case data to a data-strucutre that we can work with in the handlebar template, passes the data into
    the template and generates html. Finally it saves the html to s3 and outputs an event on the
    eventbridge

---
+++ services/viva-ms/helpers/createCaseTemplateData.js
@@ -0,0 +1,149 @@
+import { VIVA_POST_TYPE_COLLECTION } from './constans';
+import formatPeriodDates from './formatPeriodDates';
+import formHelpers from './formHelpers';
+
+const personMapFunctions = {
+  applicant: (person, answers) => mapApplicant(person, answers),
+  coApplicant: (person, answers) => mapCoApplicant(person, answers),
+};
+
+function mapApplicant(person, answers) {
+  const personalInfoAnswers = formHelpers.filterFieldIdIncludes(answers, 'personalInfo');
+  const personalInfo = personalInfoAnswers.reduce((acc, curr) => {
+    const attribute = formHelpers.getAttributeNameFromAnswerFieldId(curr.field.id);
+    return { ...acc, [attribute]: curr.value };
+  }, {});
+
+  return {
+    role: person.role,
+    personalNumber: person.personalNumber,
+    firstName: person.firstName,
+    lastName: person.lastName,
+    phone: personalInfo.telephone,
+    email: personalInfo.email,
+    occupation: personalInfo.occupation,
+  };
+}
+
+function mapCoApplicant(person, answers) {
+  const partnerInfoAnswers = formHelpers.filterFieldIdIncludes(answers, 'personalInfo');
+  const partnerInfo = partnerInfoAnswers.reduce((acc, curr) => {
+    const attribute = formHelpers.getAttributeNameFromAnswerFieldId(curr.field.id);
+    return { ...acc, [attribute]: curr.value };
+  }, {});
+
+  return {
+    role: person.role,
+    personalNumber: person.personalNumber,
+    firstName: person.firstName,
+    lastName: person.lastName,
+    phone: partnerInfo.partnerPhone,
+    email: partnerInfo.partnerMail,
+    occupation: partnerInfo.partnerOccupation,
+  };
+}
+
+export function createPersonsObject(persons, answers) {
+  const applicantPersons = persons.map(person => {
+    const personMapFunction = personMapFunctions[person.role];
+    if (personMapFunction) {
+      return personMapFunction(person, answers);
+    }
+    return person;
+  });
+
+  return applicantPersons;
+}
+
+export function createHousingInfoObject(answers) {
+  const filteredAnswers = answers.filter(answer => answer.field.id.includes('housingInfo'));
+
+  const coApplicant = filteredAnswers.reduce((acc, curr) => {
+    const strings = curr.field.id.split('.');
+    return { ...acc, [strings[1]]: curr.value };
+  }, {});
+
+  return coApplicant;
+}
+
+export function createEconomicsObject(answers) {
+  const categories = ['expenses', 'incomes'];
+  const [expenses, incomes] = categories.map(category => {
+    const categoryAnswers = formHelpers.filterByTags(answers, category);
+    const categorySummaryList = categoryAnswers.reduce((summaryList, answer) => {
+      const groupTag = formHelpers.getTagIfIncludes(answer.field.tags, 'group');
+
+      let summaryItem = {
+        type: category,
+        id: groupTag,
+        belongsTo: 'APPLICANT',
+        title: '',
+        description: '',
+        date: '',
+        value: '',
+      };
+
+      const summaryItemIndex = summaryList.findIndex(summaryItem => summaryItem.id === groupTag);
+
+      if (summaryItemIndex !== -1) {
+        summaryItem = summaryList[summaryItemIndex];
+      }
+
+      if (answer.field.tags.includes('appliesto')) {
+        summaryItem.belongsTo = answer.value;
+      }
+
+      if (answer.field.tags.includes('amount')) {
+        summaryItem.value = answer.value;
+      }
+
+      if (answer.field.tags.includes('description')) {
+        summaryItem.description = answer.value;
+      }
+
+      if (answer.field.tags.includes('date')) {
+        summaryItem.date = answer.value;
+      }
+
+      const vivaPostType = answer.field.tags.reduce((type, tag) => {
+        if (VIVA_POST_TYPE_COLLECTION[tag] !== undefined) {
+          return tag;
+        }
+        return type;
+      }, '');
+
+      if (vivaPostType) {
+        summaryItem.type = vivaPostType;
+        summaryItem.title = VIVA_POST_TYPE_COLLECTION[vivaPostType];
+      }
+
+      if (summaryItemIndex !== -1) {
+        summaryList[summaryItemIndex] = summaryItem;
+      } else {
+        summaryList.push(summaryItem);
+      }
+
+      return summaryList;
+    }, []);
+    return categorySummaryList;
+  });
+  return {
+    expenses,
+    incomes,
+  };
+}
+
+export default function createRecurringCaseTemplateData(caseItem, recurringFormId) {
+  const recurringform = caseItem.forms[recurringFormId];
+  const period = formatPeriodDates(caseItem.details.period);
+  const persons = createPersonsObject(caseItem.persons, recurringform.answers);
+  const housing = createHousingInfoObject(recurringform.answers);
+  const economis = createEconomicsObject(recurringform.answers);
+
+  return {
+    period,
+    persons,
+    housing,
+    economis,
+  };
+}
---
+++ services/viva-ms/lambdas/generateRecurringCaseHtml.js
@@ -0,0 +1,69 @@
+import { AWS } from 'aws-sdk';
+import config from '../../../config';
+import params from '../../../libs/params';
+import { to } from 'await-to-js';
+import handlebars from 'handlebars';
+import { s3Client } from '../../../libs/S3';
+import { putEvent } from '../../../libs/awsEventBridge';
+import createRecurringCaseTemplateData from '../helpers/createCaseTemplateData';
+
+const VIVA_CASE_SSM_PARAMS = params.read(config.cases.providers.viva.envsKeyName);
+
+export async function main(event) {
+  const { dynamodb } = event.detail;
+  if (dynamodb.NewImage === undefined) {
+    return false;
+  }
+  const vivaCaseSsmParams = await VIVA_CASE_SSM_PARAMS;
+  const caseItem = AWS.DynamoDB.Converter.unmarshall(dynamodb.NewImage);
+
+  const [s3GetObjectError, s3Object] = await to(
+    s3Client
+      .getObject({
+        Bucket: process.env.PDF_STORAGE_BUCKET_NAME,
+        Key: 'templates/ekb-recurring.hbs',
+      })
+      .promise()
+  );
+  if (s3GetObjectError) {
+    console.error(s3GetObjectError);
+    return false;
+  }
+
+  if (!s3Object.Key.includes('.hbs')) {
+    console.error('The s3Object tempalate file extension is not of type .hbs');
+    return false;
+  }
+
+  const handlebarsTemplateFileBody = s3Object.Body.toString();
+  const template = handlebars.compile(handlebarsTemplateFileBody);
+  const caseTemplateData = createRecurringCaseTemplateData(
+    caseItem,
+    vivaCaseSsmParams.recurringFormId
+  );
+  const html = template(caseTemplateData);
+
+  const caseHtmlKey = `html/case-${caseItem.id}.html`;
+  const [s3PutObjectError] = await to(
+    s3Client.putObject({
+      Bucket: process.env.PDF_STORAGE_BUCKET_NAME,
+      key: caseHtmlKey,
+      Body: html,
+    })
+  );
+  if (s3PutObjectError) {
+    console.error(s3PutObjectError);
+    return false;
+  }
+
+  const [putEventError] = await to(
+    putEvent(
+      { pdfStorageBucketKey: caseHtmlKey, resourceId: caseItem.id },
+      'htmlGeneratedSuccess',
+      'vivaMs.generateCaseHtml'
+    )
+  );
+  if (putEventError) {
+    throw putEventError;
+  }
+}
