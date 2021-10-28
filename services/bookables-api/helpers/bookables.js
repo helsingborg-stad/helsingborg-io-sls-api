8f1515e69c34265ebcde1bf618f424e4dfb6b690
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-10-28 11:19:12 +0200

    feat: add quick and dirty parameter store implementation of bookables service

---
+++ services/bookables-api/helpers/bookables.js
@@ -0,0 +1,21 @@
+import { throwError } from '@helsingborg-stad/npm-api-error-handling';
+import to from 'await-to-js';
+
+import params from '../../../libs/params';
+import config from '../../../config';
+
+async function getBookables() {
+  const { bookables } = await getSsmParameters();
+  return bookables;
+}
+
+async function getSsmParameters() {
+  const [error, response] = await to(params.read(config.bookables.envsKeyName));
+  if (error) {
+    throwError(500);
+  }
+
+  return response;
+}
+
+export { getBookables };
---
+++ services/bookables-api/lambdas/getBookables.js
@@ -0,0 +1,14 @@
+import to from 'await-to-js';
+
+import * as response from '../../../libs/response';
+
+import { getBookables } from '../helpers/bookables';
+
+export async function main() {
+  const [ssmError, bookables] = await to(getBookables());
+  if (ssmError) {
+    return response.failure(ssmError);
+  }
+
+  return response.success(200, bookables);
+}
