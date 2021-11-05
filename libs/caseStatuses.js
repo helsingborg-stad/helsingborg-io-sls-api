598a21019d431363cb801ac64517cdeef3846120
Dan Nilsson <mail@dannilsson.se>
2021-11-05 23:47:08 +0100

    refactor: 💡 move case status constant

--- libs/caseStatuses.js
+++ libs/caseStatuses.js
@@ -1,54 +1,54 @@
-export const statusTypes = {
-  NOT_STARTED: 'notStarted',
-  ACTIVE_ONGOING: 'active:ongoing',
-  ACTIVE_SIGNATURE_COMPLETED: 'active:signature:completed',
-  ACTIVE_SIGNATURE_PENDING: 'active:signature:pending',
-  ACTIVE_SUBMITTED: 'active:submitted',
-  ACTIVE_PROCESSING: 'active:processing',
-  CLOSED: 'closed',
-  NOT_STARTED_VIVA: 'notStarted:viva',
-  ACTIVE_COMPLETION_REQUIRED_VIVA: 'active:completionRequired:viva',
-  CLOSED_APPROVED_VIVA: 'closed:approved:viva',
-  CLOSED_PARTIALLY_APPROVED_VIVA: 'closed:partiallyApproved:viva',
-  CLOSED_REJECTED_VIVA: 'closed:rejected:viva',
-  CLOSED_COMPLETION_REJECTED_VIVA: 'closed:completionRejected:viva',
-};
+import {
+  NOT_STARTED,
+  ACTIVE_ONGOING,
+  ACTIVE_SIGNATURE_COMPLETED,
+  ACTIVE_SIGNATURE_PENDING,
+  ACTIVE_SUBMITTED,
+  ACTIVE_PROCESSING,
+  CLOSED,
+  NOT_STARTED_VIVA,
+  ACTIVE_COMPLETION_REQUIRED_VIVA,
+  CLOSED_APPROVED_VIVA,
+  CLOSED_PARTIALLY_APPROVED_VIVA,
+  CLOSED_REJECTED_VIVA,
+  CLOSED_COMPLETION_REJECTED_VIVA,
+} from './constants';
 
 const statuses = [
   {
-    type: statusTypes.NOT_STARTED,
+    type: NOT_STARTED,
     name: 'Ej påbörjad',
     description: 'Ansökan är ej påbörjad.',
   },
   {
-    type: statusTypes.ACTIVE_ONGOING,
+    type: ACTIVE_ONGOING,
     name: 'Pågående',
     description:
       'Du har påbörjat en ansökan. Du kan öppna din ansökan och fortsätta där du slutade.',
   },
   {
-    type: statusTypes.ACTIVE_SIGNATURE_COMPLETED,
+    type: ACTIVE_SIGNATURE_COMPLETED,
     name: 'Signerad',
     description: 'Ansökan är signerad',
   },
   {
-    type: statusTypes.ACTIVE_SIGNATURE_PENDING,
+    type: ACTIVE_SIGNATURE_PENDING,
     name: 'Väntar på signering',
     description:
       'Väntar på signering. När båda har signerat med BankID skickas ansökan in för bedömning',
   },
   {
-    type: statusTypes.ACTIVE_SUBMITTED,
+    type: ACTIVE_SUBMITTED,
     name: 'Inskickad',
     description: 'Ansökan är inskickad.',
   },
   {
-    type: statusTypes.ACTIVE_PROCESSING,
+    type: ACTIVE_PROCESSING,
     name: 'Ansökan behandlas',
     description: 'Ditt ärende är mottaget och bearbetas.',
   },
   {
-    type: statusTypes.CLOSED,
+    type: CLOSED,
     name: 'Avslutat',
     description: 'Ditt ärende är avslutat.',
   },
@@ -56,35 +56,35 @@ const statuses = [
    * Service: Ekonomiskt bistånd
    */
   {
-    type: statusTypes.NOT_STARTED_VIVA,
+    type: NOT_STARTED_VIVA,
     name: 'Öppen',
     description: 'Ansökan är öppen. Du kan nu söka ekonomiskt bistånd för perioden.',
   },
   {
-    type: statusTypes.ACTIVE_COMPLETION_REQUIRED_VIVA,
+    type: ACTIVE_COMPLETION_REQUIRED_VIVA,
     name: 'Stickprovskontroll',
     description:
       'Du måste komplettera din ansökan med bilder som visar dina utgifter och inkomster. Vi behöver din komplettering inom 4 dagar för att kunna betala ut pengar för perioden.',
   },
   {
-    type: statusTypes.CLOSED_APPROVED_VIVA,
+    type: CLOSED_APPROVED_VIVA,
     name: 'Godkänd',
     description: 'Din ansökan är godkänd. Pengarna sätts in på ditt konto.',
   },
   {
-    type: statusTypes.CLOSED_PARTIALLY_APPROVED_VIVA,
+    type: CLOSED_PARTIALLY_APPROVED_VIVA,
     name: 'Delvis godkänd',
     description:
       'Delar av din ansökan är godkänd, men några av de utgifter du sökt för får du inte bistånd för. Pengarna för godkända utgifter sätts in på ditt konto.',
   },
   {
-    type: statusTypes.CLOSED_REJECTED_VIVA,
+    type: CLOSED_REJECTED_VIVA,
     name: 'Avslagen',
     description:
       'Din ansökan är inte godkänd och du kommer inte att få någon utbetalning. Vill du överklaga beslutet lämnar du en skriftlig motivering med e-post eller brev till din handläggare.',
   },
   {
-    type: statusTypes.CLOSED_COMPLETION_REJECTED_VIVA,
+    type: CLOSED_COMPLETION_REJECTED_VIVA,
     name: 'Avslagen',
     description:
       'Din ansökan är inte godkänd eftersom vi saknar stickprov för perioden. Därför kan vi inte gå vidare och godkänna din ansökan.',
