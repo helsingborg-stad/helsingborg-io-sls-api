0078a8e44d0d033990f77809732c86f5c8e78783
Dan Nilsson <mail@dannilsson.se>
2021-11-16 22:55:26 +0100

    Revert "feat: 🎸 Add isExpirationTimeUnchanged"
    This reverts commit 189eee1dd51917cf90b40f0e4ea73d6548177aa0.

--- services/stream-eventbridge-ms/lambdas/streamProcessor.js
+++ services/stream-eventbridge-ms/lambdas/streamProcessor.js
@@ -1,11 +1,14 @@
 import AWS from 'aws-sdk';
-import DynamoDB from 'aws-sdk/clients/dynamodb';
 
 import log from '../../../libs/logs';
 
 const eventBridge = new AWS.EventBridge();
 
-export async function main(event, context) {
+/**
+ * Lambda function takes DynamoDB stream events and
+ * publishes them to an EventBridge Event Bus
+ */
+export const main = async (event, context) => {
   const eventsToPut = [];
 
   for (const record of event.Records) {
@@ -15,26 +18,19 @@ export async function main(event, context) {
       'service-stream-event-bridge-ms-001'
     );
 
-    if (isExpirationTimeUnchanged(record)) {
-      const [tableArn] = record.eventSourceARN.split('/stream');
-      eventsToPut.push({
-        Time: record.dynamodb.ApproximateCreationDateTime,
-        Source: 'cases.database',
-        Resources: [tableArn],
-        DetailType: record.eventName,
-        Detail: JSON.stringify(record),
-        EventBusName: 'default',
-      });
-    }
+    const [tableArn] = record.eventSourceARN.split('/stream');
+
+    eventsToPut.push({
+      Time: record.dynamodb.ApproximateCreationDateTime,
+      Source: 'cases.database',
+      Resources: [tableArn],
+      DetailType: record.eventName,
+      Detail: JSON.stringify(record),
+      EventBusName: 'default',
+    });
   }
 
   await eventBridge.putEvents({ Entries: eventsToPut }).promise();
 
-  return true;
-}
-
-function isExpirationTimeUnchanged(record) {
-  const oldImage = DynamoDB.Converter.unmarshall(record.dynamodb.OldImage);
-  const newImage = DynamoDB.Converter.unmarshall(record.dynamodb.NewImage);
-  return oldImage.expirationTime === newImage.expirationTime;
-}
+  return 'OK';
+};
