74bbfb8a32990cda0e1f489a098a83d92552708e
Teddy Andersson <teddyandersson95@gmail.com>
2021-11-17 10:55:19 +0100

    fix(viva-ms): added empty income and expense post constants

--- services/viva-ms/helpers/constans.js
+++ services/viva-ms/helpers/constans.js
@@ -31,15 +31,31 @@ export const TAG_NAME = {
   description: 'description',
   date: 'date',
   group: 'group',
-  school: 'school',
-  housing: 'housing',
-  firstName: 'firstName',
-  lastName: 'lastName',
-  personalNumber: 'personalNumber',
 };
 
 export const PERSON_ROLE = {
   applicant: 'applicant',
   coApplicant: 'coApplicant',
-  children: 'children',
+};
+
+export const EMPTY_INCOME_POST = {
+  type: 'income',
+  group: '',
+  belongsTo: 'APPLICANT',
+  title: '',
+  description: '',
+  date: '',
+  value: '',
+  currency: 'kr',
+};
+
+export const EMPTY_EXPENSE_POST = {
+  type: 'expense',
+  group: '',
+  belongsTo: 'APPLICANT',
+  title: '',
+  description: '',
+  date: '',
+  value: '',
+  currency: 'kr',
 };
