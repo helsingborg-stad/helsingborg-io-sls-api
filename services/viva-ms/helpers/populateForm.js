0e6719af3f047a5f4dbfef156ac2200c5252a269
Dan Nilsson <mail@dannilsson.se>
2021-12-08 23:54:51 +0100

    feat: 🎸 add logic to prepopulate form with children
    Helper function used to prepopulate recurring form answers with children
    stored in Viva.

--- services/viva-ms/helpers/populateForm.js
+++ services/viva-ms/helpers/populateForm.js
@@ -1,45 +1,36 @@
 /* eslint-disable no-console */
-import { generateDataMap, formatAnswer } from '../../../libs/formAnswers';
+import { generateDataMap, formatAnswer, mergeAnswers } from '../../../libs/formAnswers';
 
-function populateChildren(fieldList, childrenList) {
-  const answers = childrenList.map(child => {
-    const childAnswers = fieldList.inputs.map(input => {
-      let value = '';
-      if (input.tags.includes('firstName')) {
-        value = child.firstName;
-      }
+export function populateChildrenAnswers(repeaterInputList, childrenList) {
+  const answers = [];
 
-      if (input.tags.includes('lastName')) {
-        value = child.lastName;
-      }
+  childrenList.forEach((child, childrenListIndex) => {
+    for (const [childKey, childValue] of Object.entries(child)) {
+      const childAnswer = repeaterInputList
+        .map(input => {
+          if (input.tags.includes(childKey)) {
+            const inputId = input.id.replace('[*]', childrenListIndex);
+            return formatAnswer(inputId, input.tags, childValue);
+          }
+        })
+        .filter(Boolean);
 
-      if (input.tags.includes('personalNumber')) {
-        value = child.personalNumber;
-      }
-
-      const answer = formatAnswer(input.id, input.tags, value);
-      return answer;
-    });
-
-    return childAnswers;
+      answers.push(...childAnswer);
+    }
   });
 
   return answers;
 }
 
-export default function populateFormWithVivaChildren(forms, vivaChildrenList, formTemplates) {
-  Object.keys(forms).forEach(formId => {
-    const caseFormTemplate = formTemplates?.[formId] || {};
+export default function populateFormWithVivaChildren(form, formTemplate, vivaChildrenList) {
+  const dataMap = generateDataMap(formTemplate);
 
-    const dataMap = generateDataMap(caseFormTemplate);
-    console.log('dataMap', dataMap);
+  const childrenRepeaterInputList = dataMap.filter(field => field.tags?.includes('children'));
 
-    const childrenRepeaterFieldList = dataMap.filter((field, index) =>
-      field[index].tags.includes('children')
-    );
-    console.log('childrenRepeaterFieldList', childrenRepeaterFieldList);
+  const childrenAnswers = populateChildrenAnswers(childrenRepeaterInputList, vivaChildrenList);
 
-    const answers = populateChildren(childrenRepeaterFieldList, vivaChildrenList);
-    console.log('answers', answers);
-  });
+  const mergedChildrenAnswers = mergeAnswers(childrenAnswers, form.answers);
+  console.log('mergedChildrenAnswers', mergedChildrenAnswers);
+
+  return { ...form, answers: mergedChildrenAnswers };
 }
