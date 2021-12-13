0cf94db06c134c2a1fb664a7db2cf7114abeceaf
Karl <karl.mosenbacher@knowit.se>
2021-12-13 15:01:07 +0100

    fix: check if timeslot is available before creating booking

--- services/booking-api/lambdas/create.js
+++ services/booking-api/lambdas/create.js
@@ -17,6 +17,17 @@ export async function main(event) {
     });
   }
 
+  const searchBookingBody = { startTime, endTime };
+  const [searchBookingError, searchResponse] = await to(booking.search(searchBookingBody));
+  if (searchBookingError) {
+    return response.failure(searchBookingError);
+  }
+
+  const timeslotTaken = searchResponse?.data?.data?.attributes.length > 0;
+  if (timeslotTaken) {
+    return response.failure({ message: 'Timeslot not available for booking', status: 500 });
+  }
+
   const createBookingBody = getCreateBookingBody(body);
   const [error, createBookingResponse] = await to(booking.create(createBookingBody));
   if (error) {
