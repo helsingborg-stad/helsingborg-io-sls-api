dc531f52abee65aa4a020775400a6ad4fd096c4a
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2022-01-24 14:38:34 +0100

    test: update tests for getHistoricalAttendees

--- services/booking-api/test/lambdas/getHistoricalAttendees.test.ts
+++ services/booking-api/test/lambdas/getHistoricalAttendees.test.ts
@@ -3,23 +3,74 @@ import booking from '../../src/helpers/booking';
 
 jest.mock('../../src/helpers/booking');
 
-const { getHistoricalAttendees } = jest.mocked(booking);
+const { getHistoricalAttendees, getAdministratorDetails } = jest.mocked(booking);
 const mockHeaders = {
   'Access-Control-Allow-Credentials': true,
   'Access-Control-Allow-Origin': '*',
 };
 
-const mockDatatorgetResponse = {
-  type: 'bookings',
-  id: '123456789',
-  attributes: ['outlook_1@helsingborg.se', 'outlook_2@helsingborg.se'],
+const mockContext = {
+  awsRequestId: 'xxxxx',
 };
 
-const getHistoricalAttendeesMockData = {
+const mockLookupResponse1 = {
+  data: {
+    data: {
+      type: 'userdetails',
+      id: 'id',
+      attributes: {
+        Email: 'outlook_1@helsingborg.se',
+        DisplayName: 'Display Name 1',
+        Department: 'Department 1',
+        JobTitle: 'Job Title 1',
+      },
+    },
+  },
+};
+
+const mockLookupResponse2 = {
+  data: {
+    data: {
+      type: 'userdetails',
+      id: 'id_2',
+      attributes: {
+        Email: 'outlook_2@helsingborg.se',
+        DisplayName: 'Display Name 2',
+        Department: 'Department 2',
+        JobTitle: 'Job Title 2',
+      },
+    },
+  },
+};
+
+const mockHistoricalResponse = {
+  type: 'bookings',
+  id: '123456789',
+  attributes: [
+    mockLookupResponse1.data.data.attributes.Email,
+    mockLookupResponse2.data.data.attributes.Email,
+  ],
+};
+
+const expectedData = {
   jsonapi: { version: '1.0' },
   data: {
-    ...mockDatatorgetResponse,
-    attributes: [{ Email: 'outlook_1@helsingborg.se' }, { Email: 'outlook_2@helsingborg.se' }],
+    ...mockHistoricalResponse,
+    attributes: [
+      mockLookupResponse1.data.data.attributes,
+      mockLookupResponse2.data.data.attributes,
+    ],
+  },
+};
+
+const expectedDataBadLookup = {
+  jsonapi: { version: '1.0' },
+  data: {
+    ...mockHistoricalResponse,
+    attributes: [
+      { Email: mockLookupResponse1.data.data.attributes.Email },
+      { Email: mockLookupResponse2.data.data.attributes.Email },
+    ],
   },
 };
 
@@ -54,18 +105,21 @@ it('gets historical attendees successfully', async () => {
   expect.assertions(3);
 
   const expectedResult = {
-    body: JSON.stringify(getHistoricalAttendeesMockData),
+    body: JSON.stringify(expectedData),
     headers: mockHeaders,
     statusCode: 200,
   };
 
   getHistoricalAttendees.mockResolvedValueOnce({
     data: {
-      data: mockDatatorgetResponse,
+      data: mockHistoricalResponse,
     },
   });
+  getAdministratorDetails
+    .mockResolvedValueOnce(mockLookupResponse1)
+    .mockResolvedValueOnce(mockLookupResponse2);
 
-  const result = await main(mockEvent);
+  const result = await main(mockEvent, mockContext);
 
   expect(result).toEqual(expectedResult);
   expect(booking.getHistoricalAttendees).toHaveBeenCalledTimes(1);
@@ -96,7 +150,7 @@ it('returns failure if "referenceCode" is not provided as path parameter', async
     statusCode,
   };
 
-  const result = await main(mockEvent);
+  const result = await main(mockEvent, mockContext);
 
   expect(result).toEqual(expectedResult);
   expect(booking.getHistoricalAttendees).toHaveBeenCalledTimes(0);
@@ -123,7 +177,7 @@ it('returns failure if one of required query strings parameters does not exist',
     statusCode,
   };
 
-  const result = await main(mockEvent);
+  const result = await main(mockEvent, mockContext);
 
   expect(result).toEqual(expectedResult);
   expect(booking.getHistoricalAttendees).toHaveBeenCalledTimes(0);
@@ -153,8 +207,68 @@ it('returns failure if datatorget request fails', async () => {
     message,
   });
 
-  const result = await main(mockEvent);
+  const result = await main(mockEvent, mockContext);
 
   expect(result).toEqual(expectedResult);
   expect(booking.getHistoricalAttendees).toHaveBeenCalledTimes(1);
+});
+
+it('returns fallback if datatorget lookup fails', async () => {
+  expect.assertions(3);
+
+  const expectedResult = {
+    body: JSON.stringify(expectedDataBadLookup),
+    headers: mockHeaders,
+    statusCode: 200,
+  };
+
+  getHistoricalAttendees.mockResolvedValueOnce({
+    data: {
+      data: mockHistoricalResponse,
+    },
+  });
+  getAdministratorDetails.mockRejectedValue(undefined);
+
+  const result = await main(mockEvent, mockContext);
+
+  expect(result).toEqual(expectedResult);
+  expect(getHistoricalAttendees).toHaveBeenCalledTimes(1);
+  expect(getHistoricalAttendees).toHaveBeenCalledWith({
+    referenceCode: mockReferenceCode,
+    startTime: mockStartTime,
+    endTime: mockEndTime,
+  });
+});
+
+it('caches datatorget lookup between invocations', async () => {
+  expect.assertions(5);
+
+  const expectedResult = {
+    body: JSON.stringify(expectedData),
+    headers: mockHeaders,
+    statusCode: 200,
+  };
+
+  getHistoricalAttendees.mockResolvedValue({
+    data: {
+      data: mockHistoricalResponse,
+    },
+  });
+  getAdministratorDetails
+    .mockResolvedValueOnce(mockLookupResponse1)
+    .mockResolvedValueOnce(mockLookupResponse2)
+    .mockRejectedValue(undefined);
+
+  const result1 = await main(mockEvent, mockContext);
+  const result2 = await main(mockEvent, mockContext);
+
+  expect(result1).toEqual(expectedResult);
+  expect(result2).toEqual(expectedResult);
+  expect(booking.getHistoricalAttendees).toHaveBeenCalledTimes(2);
+  expect(booking.getHistoricalAttendees).toHaveBeenCalledWith({
+    referenceCode: mockReferenceCode,
+    startTime: mockStartTime,
+    endTime: mockEndTime,
+  });
+  expect(booking.getHistoricalAttendees).toHaveBeenCalledTimes(2);
 });
