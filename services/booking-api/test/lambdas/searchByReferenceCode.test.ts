8cb19314607a9c7316156e27a49e092c8d8bbbcd
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2022-01-27 11:16:48 +0100

    test: add tests for AD lookup and cache to search

--- services/booking-api/test/lambdas/searchByReferenceCode.test.ts
+++ services/booking-api/test/lambdas/searchByReferenceCode.test.ts
@@ -6,7 +6,7 @@ import booking from '../../src/helpers/booking';
 
 jest.mock('../../src/helpers/booking');
 
-const { search } = jest.mocked(booking);
+const { search, getAdministratorDetails } = jest.mocked(booking);
 
 const mockBody = {
   startTime: '2021-11-01T00:00:00+01:00',
@@ -46,13 +46,49 @@ const mockCalendarSearch = [
   },
 ];
 
+const mockLookupResponse = {
+  data: {
+    data: {
+      type: 'userdetails',
+      id: 'id',
+      attributes: {
+        Email: 'test@mail.com',
+        DisplayName: 'Display Name',
+        Department: 'Department',
+        JobTitle: 'Job Title',
+      },
+    },
+  },
+};
+
+const mockReturnBody = [
+  {
+    BookingId: '123456789',
+    Attendees: [
+      {
+        Email: 'test@mail.com',
+        Type: 'Required',
+        Status: 'Accepted',
+        DisplayName: 'Display Name',
+        Department: 'Department',
+        JobTitle: 'Job Title',
+      },
+    ],
+    Subject: ' ##attempt_1',
+    Body: null,
+    Location: 'plats',
+    ReferenceCode: 'attempt_1',
+    StartTime: '2021-11-10T10:00:00+01:00',
+    EndTime: '2021-11-10T11:00:00+01:00',
+  },
+];
+
 beforeEach(() => {
   jest.resetAllMocks();
-  booking.getAdministratorDetails.mockRejectedValue();
 });
 
-it('gets booking from a successful search', async () => {
-  expect.assertions(2);
+it('gets booking from a successful search without AD lookup', async () => {
+  expect.assertions(3);
 
   const expectedResult = {
     body: JSON.stringify({ jsonapi: { version: '1.0' }, data: { attributes: mockCalendarSearch } }),
@@ -67,15 +103,69 @@ it('gets booking from a successful search', async () => {
       },
     },
   });
+  getAdministratorDetails.mockRejectedValueOnce(undefined);
 
   const result = await main(mockEvent, mockContext);
 
   expect(result).toEqual(expectedResult);
-  expect(booking.search).toHaveBeenCalledWith(mockBody);
+  expect(search).toHaveBeenCalledWith(mockBody);
+  expect(getAdministratorDetails).toHaveBeenCalledTimes(1);
+});
+
+it('gets booking from a successful search with successful AD lookup', async () => {
+  expect.assertions(3);
+
+  const expectedResult = {
+    body: JSON.stringify({ jsonapi: { version: '1.0' }, data: { attributes: mockReturnBody } }),
+    headers: mockHeaders,
+    statusCode: 200,
+  };
+
+  search.mockResolvedValueOnce({
+    data: {
+      data: {
+        attributes: mockCalendarSearch,
+      },
+    },
+  });
+  getAdministratorDetails.mockResolvedValueOnce(mockLookupResponse);
+
+  const result = await main(mockEvent, mockContext);
+
+  expect(result).toEqual(expectedResult);
+  expect(search).toHaveBeenCalledWith(mockBody);
+  expect(getAdministratorDetails).toHaveBeenCalledTimes(1);
+});
+
+it('caches AD lookup between calls', async () => {
+  expect.assertions(4);
+
+  const expectedResult = {
+    body: JSON.stringify({ jsonapi: { version: '1.0' }, data: { attributes: mockReturnBody } }),
+    headers: mockHeaders,
+    statusCode: 200,
+  };
+
+  search.mockResolvedValue({
+    data: {
+      data: {
+        attributes: mockCalendarSearch,
+      },
+    },
+  });
+  getAdministratorDetails.mockResolvedValueOnce(mockLookupResponse);
+
+  const result1 = await main(mockEvent, mockContext);
+  const result2 = await main(mockEvent, mockContext);
+
+  expect(result1).toEqual(expectedResult);
+  expect(result2).toEqual(expectedResult);
+  expect(search).toHaveBeenCalledWith(mockBody);
+  expect(getAdministratorDetails).toHaveBeenCalledTimes(1);
 });
 
 it('throws when searching for a booking fails', async () => {
-  expect.assertions(1);
+  expect.assertions(2);
 
   const statusCode = 500;
   const message = messages[statusCode];
@@ -97,10 +187,11 @@ it('throws when searching for a booking fails', async () => {
   const result = await main(mockEvent, mockContext);
 
   expect(result).toEqual(expectedResult);
+  expect(getAdministratorDetails).not.toHaveBeenCalled();
 });
 
 it('returns failure if required paramerer is missing', async () => {
-  expect.assertions(2);
+  expect.assertions(3);
 
   const event = {
     body: JSON.stringify({ startTime: '2021-11-01T00:00:00+01:00' }),
@@ -122,5 +213,6 @@ it('returns failure if required paramerer is missing', async () => {
   const result = await main(event, mockContext);
 
   expect(result).toEqual(expectedResult);
-  expect(booking.search).toHaveBeenCalledTimes(0);
+  expect(search).not.toHaveBeenCalled();
+  expect(getAdministratorDetails).not.toHaveBeenCalled();
 });
