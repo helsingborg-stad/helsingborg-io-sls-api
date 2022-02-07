89d601f8c1e81fbe1dd61ef543acd93a36a22c68
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2022-02-07 09:33:48 +0100

    test: lint tests

--- services/booking-api/test/helpers/isTimeslotTaken.test.js
+++ services/booking-api/test/helpers/isTimeslotTaken.test.js
@@ -23,20 +23,16 @@ const mockAttendeeOptionalDeclined = {
 describe('isTimeSlotTaken', () => {
   it('returns true if every required attendee has accepted', () => {
     const bookings = [{ Attendees: [mockAttendeeRequiredAccepted, mockAttendeeRequiredAccepted] }];
-    const truth = true;
-
     const result = isTimeslotTaken(bookings);
-
-    expect(result).toEqual(truth);
+    expect(result).toBe(true);
   });
+
   it('returns false if at least one required attendee has declined', () => {
     const bookings = [{ Attendees: [mockAttendeeRequiredAccepted, mockAttendeeRequiredDeclined] }];
-    const truth = false;
-
     const result = isTimeslotTaken(bookings);
-
-    expect(result).toEqual(truth);
+    expect(result).toBe(false);
   });
+
   it('returns true if every required attendee has accepted even if optional attendees have declined', () => {
     const bookings = [
       {
@@ -48,12 +44,10 @@ describe('isTimeSlotTaken', () => {
         ],
       },
     ];
-    const truth = true;
-
     const result = isTimeslotTaken(bookings);
-
-    expect(result).toEqual(truth);
+    expect(result).toBe(true);
   });
+
   it('returns false if at least one required attendee has declined even if all other required or optional attendees have accepted', () => {
     const bookings = [
       {
@@ -65,22 +59,17 @@ describe('isTimeSlotTaken', () => {
         ],
       },
     ];
-    const truth = false;
-
     const result = isTimeslotTaken(bookings);
-
-    expect(result).toEqual(truth);
+    expect(result).toBe(false);
   });
+
   it('returns true if at least one of several bookings is busy', () => {
     const bookings = [
       { Attendees: [mockAttendeeRequiredAccepted, mockAttendeeRequiredDeclined] },
       { Attendees: [mockAttendeeRequiredAccepted, mockAttendeeRequiredAccepted] },
       { Attendees: [mockAttendeeRequiredDeclined, mockAttendeeRequiredDeclined] },
     ];
-    const truth = true;
-
     const result = isTimeslotTaken(bookings);
-
-    expect(result).toEqual(truth);
+    expect(result).toBe(true);
   });
 });
