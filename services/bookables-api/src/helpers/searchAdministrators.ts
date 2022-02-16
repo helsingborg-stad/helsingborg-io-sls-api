478280812577f86325006f718915f445ffff88b1
Petter Andersson <petter.andersson@helsingborg.se>
2022-02-16 09:16:47 +0100

    feat: convert bookables-api to typescript
    Fourth service to be migrated to typescript

--- services/bookables-api/src/helpers/searchAdministrators.ts
+++ services/bookables-api/src/helpers/searchAdministrators.ts
@@ -1,16 +1,24 @@
 import to from 'await-to-js';
 
-import * as request from '../../../libs/request';
-import params from '../../../libs/params';
-import config from '../../../config';
+import * as request from '../libs/request';
+import params from '../libs/params';
+import config from '../libs/config';
 
 const URI_SOURCE = 'misc/getGroupMembers';
 
-async function searchAdministrators(body) {
+export interface SearchAdministratorsResult {
+  data?: Record<string, unknown>;
+}
+
+async function searchAdministrators(body: {
+  email?: string;
+  mailbox?: string;
+}): Promise<SearchAdministratorsResult | undefined> {
   const [readError, { datatorgetEndpoint, apiKey }] = await to(
     params.read(config.datatorget.envsKeyName)
   );
   if (readError) {
+    // eslint-disable-next-line no-console
     console.error('Read parameter error: ', readError);
     throw readError;
   }
@@ -20,7 +28,7 @@ async function searchAdministrators(body) {
     { 'X-ApiKey': apiKey }
   );
 
-  const [datatorgetError, datatorgetResponse] = await to(
+  const [datatorgetError, datatorgetResponse] = await to<SearchAdministratorsResult>(
     request.call(requestClient, 'post', `${datatorgetEndpoint}/${URI_SOURCE}`, body)
   );
   if (datatorgetError) {
