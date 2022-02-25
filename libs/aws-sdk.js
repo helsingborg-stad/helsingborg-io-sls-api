d3d1b13247f0c94923758b2d93937d4c4600503f
Dan Nilsson <dan.nilsson@helsingborg.se>
2022-02-25 10:57:11 +0100

    ci: 🎡 Make code pipeline build all

--- libs/aws-sdk.js
+++ libs/aws-sdk.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import aws from 'aws-sdk';
 
-const awsWrapped = process.env.IS_LOCAL ? aws : aws; // Can be something other productions stuff wrapped around aws
+const awsWrapped = process.env.IS_LOCAL ? aws : aws; // Can be something other productions stuff wrapped around aws.
 
 export default awsWrapped;
