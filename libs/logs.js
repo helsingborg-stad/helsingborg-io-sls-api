5356de9a5d5bb6376390f48ee0ced88369aa6a99
Petter Andersson <petter.andersson@helsingborg.se>
2022-03-17 14:44:35 +0100

    feat: add extended auto logging
    fix: add simplified log functions
    
    fix: add lambda types
    
    fix: add user-agent to logging
    
    fix: review comments
    
    fix: review comments
    
    fix: change how error is logged
    
    fix: code review comments
    
    fix: code review comments

--- libs/logs.js
+++ libs/logs.js
@@ -8,32 +8,94 @@ const logger = winston.createLogger({
 });
 
 const log = {
+  requestId: 'N/A',
+  writeLog: (level, message, customData = {}) => {
+    logger.log(level, message, {
+      requestId: log.requestId,
+      customData,
+    });
+  },
+
   log: (level, message, requestId, errorCode, customData = {}) => {
     logger.log(level, message, {
       errorCode,
-      requestId,
+      requestId: log.requestId,
       customData,
     });
+  },
+
+  writeError: (message, customData = {}) => {
+    log.writeLog('error', message, customData);
   },
 
   error: (message, requestId, errorCode, customData = {}) => {
     log.log('error', message, requestId, errorCode, customData);
   },
 
+  writeWarn: (message, customData = {}) => {
+    log.writeLog('warn', message, customData);
+  },
+
   warn: (message, requestId, errorCode, customData = {}) => {
     log.log('warn', message, requestId, errorCode, customData);
+  },
+
+  writeInfo: (message, customData = {}) => {
+    log.writeLog('info', message, customData);
   },
 
   info: (message, requestId, errorCode, customData = {}) => {
     log.log('info', message, requestId, errorCode, customData);
   },
 
+  writeVerbose: (message, customData = {}) => {
+    log.writeLog('verbose', message, customData);
+  },
+
   verbose: (message, requestId, errorCode, customData = {}) => {
     log.log('verbose', message, requestId, errorCode, customData);
   },
 
+  writeDebug: (message, customData = {}) => {
+    log.writeLog('debug', message, customData);
+  },
+
   debug: (message, requestId, errorCode, customData = {}) => {
     log.log('debug', message, requestId, errorCode, customData);
+  },
+
+  initialize: (event, context) => {
+    log.requestId = context.awsRequestId;
+    log.writeInfo('Lambda initialize', {
+      source: event.source,
+      detailType: event['detail-type'],
+      path: event.path,
+      method: event.httpMethod,
+      pathParameters: event.pathParameters,
+      queryStringParameters: event.queryStringParameters,
+      userAgent: event.headers?.['User-Agent'],
+    });
+  },
+
+  finalize: (response, error) => {
+    log.writeInfo('Lambda finalize', {
+      statusCode: response?.statusCode ?? 0,
+      error: error?.toString(),
+    });
+  },
+
+  wrap: lambda => {
+    return async (event, context) => {
+      try {
+        log.initialize(event, context);
+        const response = await lambda(event, context);
+        log.finalize(response);
+        return response;
+      } catch (error) {
+        log.finalize(null, error);
+        throw error;
+      }
+    };
   },
 };
 
