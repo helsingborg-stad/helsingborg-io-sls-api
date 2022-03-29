076ec7679cfacdf015784e697b17300ba41336b2
Dan Nilsson <mail@dannilsson.se>
2022-03-29 10:09:42 +0200

    test: 💍 (completions) refactor tests to fit expected result

--- services/viva-ms/test/helpers/completions.test.js
+++ services/viva-ms/test/helpers/completions.test.js
@@ -1,219 +1,270 @@
+/* eslint-disable jest/no-commented-out-tests */
 import { getStatusByType } from '../../src/libs/caseStatuses';
 import {
-  ACTIVE_COMPLETION_REQUIRED_VIVA,
+  // status type
   ACTIVE_RANDOM_CHECK_REQUIRED_VIVA,
+  ACTIVE_RANDOM_CHECK_SUBMITTED,
+  ACTIVE_COMPLETION_REQUIRED_VIVA,
+  ACTIVE_COMPLETION_SUBMITTED,
+
+  // state
   COMPLETIONS_RANDOM_SELECT,
   COMPLETIONS_REQUIRED,
   COMPLETIONS_PENDING,
 } from '../../src/libs/constants';
+import {
+  getCompletionFormId,
+  getCompletionStatus,
+  getCompletionState,
+} from '../../src/helpers/completions';
 
-function getCompletionFormId(completionForms, completions) {
-  const { randomCheckFormId, completionFormId } = completionForms;
-  return isRandomSelect(completions) ? randomCheckFormId : completionFormId;
-}
-
-function getCompletionStatus(completions) {
-  return isRandomSelect(completions)
-    ? getStatusByType(ACTIVE_RANDOM_CHECK_REQUIRED_VIVA)
-    : getStatusByType(ACTIVE_COMPLETION_REQUIRED_VIVA);
-}
-
-function getCompletionState(completions) {
-  if (isRandomSelect(completions)) {
-    return COMPLETIONS_RANDOM_SELECT;
-  }
-
-  if (completions.isAttachmentPending) {
-    return COMPLETIONS_PENDING;
-  }
-
-  return COMPLETIONS_REQUIRED;
-}
-
-function isRandomSelect(completions) {
-  const { isRandomCheck, isAttachmentPending, requested } = completions;
-  return isRandomCheck && !isAttachmentPending && !isAnyRequestedCompletionsReceived(requested);
-}
-
-function isAnyRequestedCompletionsReceived(requestedList) {
-  return requestedList.reduce((received, current) => {
-    if (current.received) {
-      return true;
-    }
-    return received;
-  }, undefined);
-}
-
-const expectedRandomCheckStatus = {
-  type: ACTIVE_RANDOM_CHECK_REQUIRED_VIVA,
-  name: 'Stickprovskontroll',
-  description:
-    'Du måste komplettera din ansökan med bilder som visar dina utgifter och inkomster. Vi behöver din komplettering inom 4 dagar för att kunna betala ut pengar för perioden.',
+const recurringCompletionForms = {
+  randomCheckFormId: '123ABC',
+  completionFormId: '456CDF',
 };
 
-const expectCompletionStatus = {
-  type: ACTIVE_COMPLETION_REQUIRED_VIVA,
-  name: 'Ansökan behöver kompletteras',
-  description:
-    'Du har skickat in en ansökan för #MONTH_NAME. För att vi ska kunna behandla din ansökan finns det uppgifter som du behöver komplettera.\n\nKomplettering ska ha skickats in till oss senast #COMPLETION_DUEDATE.',
-};
+const requestedAllFalseList = [
+  {
+    description: 'Underlag på alla sökta utgifter',
+    received: false,
+  },
+  {
+    description: 'Underlag på alla inkomster/tillgångar',
+    received: false,
+  },
+  {
+    description: 'Alla kontoutdrag för hela förra månaden och fram till idag',
+    received: false,
+  },
+];
 
-const recurringCompletionsForms = {
-  randomCheckFormId: '123',
-  completionFormId: '456',
-};
+const requestedSomeTrueList = [
+  {
+    description: 'Underlag på alla sökta utgifter',
+    received: false,
+  },
+  {
+    description: 'Underlag på alla inkomster/tillgångar',
+    received: true,
+  },
+  {
+    description: 'Alla kontoutdrag för hela förra månaden och fram till idag',
+    received: false,
+  },
+];
 
-const completionsInitialRandomCheck = {
-  requested: [
+describe('Completions form (getCompletionFormId)', () => {
+  test.each([
     {
-      description: 'Underlag på alla sökta utgifter',
-      received: false,
+      conditionOption: {
+        requested: [...requestedAllFalseList],
+        isRandomCheck: true,
+        isAttachmentPending: false,
+      },
+      expectedResult: recurringCompletionForms.randomCheckFormId,
+      description: `isAttachmentPending is false, isRandomCheck is true, expect randomCheckFormId: ${recurringCompletionForms.randomCheckFormId}`,
     },
     {
-      description: 'Underlag på alla inkomster/tillgångar',
-      received: false,
+      conditionOption: {
+        requested: [...requestedAllFalseList],
+        isRandomCheck: true,
+        isAttachmentPending: true,
+      },
+      expectedResult: recurringCompletionForms.randomCheckFormId,
+      description: `isAttachmentPending is true, isRandomCheck is true expect randomCheckFormId: ${recurringCompletionForms.randomCheckFormId}`,
     },
     {
-      description: 'Alla kontoutdrag för hela förra månaden och fram till idag',
-      received: false,
-    },
-  ],
-  dueDate: 1648249200000,
-  isRandomCheck: true,
-  isAttachmentPending: false,
-  receivedDate: null,
-  isCompleted: false,
-  isDueDateExpired: false,
-};
-
-const completionsAttachmentPending = {
-  requested: [
-    {
-      description: 'Underlag på alla sökta utgifter',
-      received: false,
+      conditionOption: {
+        requested: [...requestedAllFalseList],
+        isRandomCheck: false,
+        isAttachmentPending: false,
+      },
+      expectedResult: recurringCompletionForms.completionFormId,
+      description: `isAttachmentPending is false, isRandomCheck is false, expect completionFormId: ${recurringCompletionForms.completionFormId}`,
     },
     {
-      description: 'Underlag på alla inkomster/tillgångar',
-      received: false,
+      conditionOption: {
+        requested: [...requestedAllFalseList],
+        isRandomCheck: false,
+        isAttachmentPending: true,
+      },
+      expectedResult: recurringCompletionForms.completionFormId,
+      description: `isAttachmentPending is true, isRandomCheck is false, expect completionFormId: ${recurringCompletionForms.completionFormId}`,
     },
     {
-      description: 'Alla kontoutdrag för hela förra månaden och fram till idag',
-      received: false,
+      conditionOption: {
+        requested: [...requestedSomeTrueList],
+      },
+      expectedResult: recurringCompletionForms.completionFormId,
+      description: `some requested is true, expect completionFormId: ${recurringCompletionForms.completionFormId}`,
     },
-  ],
-  dueDate: 1648249200000,
-  isRandomCheck: true,
-  isAttachmentPending: true,
-  receivedDate: null,
-  isCompleted: false,
-  isDueDateExpired: false,
-};
-
-const completionsRequestedPartialyReceived = {
-  requested: [
-    {
-      description: 'Underlag på alla sökta utgifter',
-      received: true,
-    },
-    {
-      description: 'Underlag på alla inkomster/tillgångar',
-      received: false,
-    },
-    {
-      description: 'Alla kontoutdrag för hela förra månaden och fram till idag',
-      received: false,
-    },
-  ],
-  dueDate: 1648249200000,
-  isRandomCheck: true,
-  isAttachmentPending: false,
-  receivedDate: null,
-  isCompleted: false,
-  isDueDateExpired: false,
-};
-
-const completionsDefault = {
-  requested: [
-    {
-      description: 'Underlag på alla sökta utgifter',
-      received: false,
-    },
-    {
-      description: 'Underlag på alla inkomster/tillgångar',
-      received: false,
-    },
-    {
-      description: 'Alla kontoutdrag för hela förra månaden och fram till idag',
-      received: false,
-    },
-  ],
-  dueDate: 1648249200000,
-  isRandomCheck: false,
-  isAttachmentPending: false,
-  receivedDate: null,
-  isCompleted: false,
-  isDueDateExpired: false,
-};
-
-const completionsAttachmentPendingNotRandomCheck = {
-  ...completionsAttachmentPending,
-  isRandomCheck: false,
-};
-
-const completionsAttachmentPendingAndRequestedPartialyReceivedNotRandomCheck = {
-  ...completionsRequestedPartialyReceived,
-  isRandomCheck: false,
-  isAttachmentPending: true,
-};
-
-it('Returns form id of form type > recurring random check', () => {
-  const results = getCompletionFormId(recurringCompletionsForms, completionsInitialRandomCheck);
-  expect(results).toBe('123');
+  ])('$description', ({ conditionOption, expectedResult }) => {
+    const result = getCompletionFormId(recurringCompletionForms, conditionOption);
+    expect(result).toBe(expectedResult);
+  });
 });
 
-it('Returns form id of form type > recurring completion', () => {
-  const results = getCompletionFormId(recurringCompletionsForms, completionsAttachmentPending);
-  expect(results).toBe('456');
+describe('Completions random select (getCompletionStatus)', () => {
+  test.each([
+    {
+      conditionOption: {
+        requested: [...requestedAllFalseList],
+        isRandomCheck: true,
+        isAttachmentPending: false,
+      },
+      expectedResult: getStatusByType(ACTIVE_RANDOM_CHECK_REQUIRED_VIVA),
+      description: `yet to be submitted, expect ${ACTIVE_RANDOM_CHECK_REQUIRED_VIVA}`,
+    },
+    {
+      conditionOption: {
+        requested: [...requestedAllFalseList],
+        isRandomCheck: true,
+        isAttachmentPending: true,
+      },
+      expectedResult: getStatusByType(ACTIVE_RANDOM_CHECK_SUBMITTED),
+      description: `submitted with attachments, expect ${ACTIVE_RANDOM_CHECK_SUBMITTED}`,
+    },
+    {
+      conditionOption: {
+        requested: [...requestedSomeTrueList],
+        isRandomCheck: true,
+        isAttachmentPending: false,
+      },
+      expectedResult: getStatusByType(ACTIVE_COMPLETION_REQUIRED_VIVA),
+      description: `one or more requested received, expect ${ACTIVE_COMPLETION_REQUIRED_VIVA}`,
+    },
+  ])('$description', ({ conditionOption, expectedResult }) => {
+    const results = getCompletionStatus(conditionOption);
+    expect(results).toEqual(expectedResult);
+  });
 });
 
-it('Returns status for recurring random check', () => {
-  const results = getCompletionStatus(completionsInitialRandomCheck);
-  expect(results).toEqual(expectedRandomCheckStatus);
+describe('Completions requested received (getCompletionStatus)', () => {
+  test.each([
+    {
+      conditionOption: {
+        requested: [...requestedSomeTrueList],
+        isRandomCheck: false,
+        isAttachmentPending: true,
+      },
+      expectedResult: getStatusByType(ACTIVE_COMPLETION_SUBMITTED),
+      description: `submitted with attachments, expect ${ACTIVE_COMPLETION_SUBMITTED}`,
+    },
+    {
+      conditionOption: {
+        requested: [...requestedSomeTrueList],
+        isRandomCheck: false,
+        isAttachmentPending: false,
+      },
+      expectedResult: getStatusByType(ACTIVE_COMPLETION_REQUIRED_VIVA),
+      description: `completion requested, expect ${ACTIVE_COMPLETION_REQUIRED_VIVA}`,
+    },
+  ])('$description', ({ conditionOption, expectedResult }) => {
+    const results = getCompletionStatus(conditionOption);
+    expect(results).toEqual(expectedResult);
+  });
 });
 
-it('Returns status for recurring completion when attachment peinding', () => {
-  const results = getCompletionStatus(completionsAttachmentPending);
-  expect(results).toEqual(expectCompletionStatus);
+describe('Completions random select (getCompletionState)', () => {
+  test.each([
+    {
+      conditionOption: {
+        requested: [...requestedAllFalseList],
+        isRandomCheck: true,
+        isAttachmentPending: false,
+      },
+      expectedResult: COMPLETIONS_RANDOM_SELECT,
+      description: `set state to ${COMPLETIONS_RANDOM_SELECT} the first time`,
+    },
+    {
+      conditionOption: {
+        requested: [...requestedAllFalseList],
+        isRandomCheck: true,
+        isAttachmentPending: true,
+      },
+      expectedResult: COMPLETIONS_PENDING,
+      description: `set state to ${COMPLETIONS_PENDING} when uploaded attachments`,
+    },
+  ])('$description', ({ conditionOption, expectedResult }) => {
+    const results = getCompletionState(conditionOption);
+    expect(results).toEqual(expectedResult);
+  });
 });
 
-it('Returns status for recurring completion when request completions is partialy received', () => {
-  const results = getCompletionStatus(completionsRequestedPartialyReceived);
-  expect(results).toEqual(expectCompletionStatus);
+describe('Completions random select, requested received (getCompletionState)', () => {
+  test.each([
+    {
+      conditionOption: {
+        requested: [...requestedSomeTrueList],
+        isRandomCheck: true,
+        isAttachmentPending: false,
+      },
+      expectedResult: COMPLETIONS_REQUIRED,
+      description: `set state to ${COMPLETIONS_REQUIRED} the first time`,
+    },
+    {
+      conditionOption: {
+        requested: [...requestedSomeTrueList],
+        isRandomCheck: true,
+        isAttachmentPending: true,
+      },
+      expectedResult: COMPLETIONS_PENDING,
+      description: `set state to ${COMPLETIONS_PENDING} when uploaded attachments`,
+    },
+  ])('$description', ({ conditionOption, expectedResult }) => {
+    const results = getCompletionState(conditionOption);
+    expect(results).toEqual(expectedResult);
+  });
 });
 
-it('Returns status for recurring completion when requesting completions AND NO attachemnt is uploaded', () => {
-  const results = getCompletionStatus(completionsDefault);
-  expect(results).toEqual(expectCompletionStatus);
+describe('Completions requesting (getCompletionState)', () => {
+  test.each([
+    {
+      conditionOption: {
+        requested: [...requestedAllFalseList],
+        isRandomCheck: false,
+        isAttachmentPending: false,
+      },
+      expectedResult: COMPLETIONS_REQUIRED,
+      description: `set state to ${COMPLETIONS_REQUIRED} the first time`,
+    },
+    {
+      conditionOption: {
+        requested: [...requestedAllFalseList],
+        isRandomCheck: false,
+        isAttachmentPending: true,
+      },
+      expectedResult: COMPLETIONS_PENDING,
+      description: `set state to ${COMPLETIONS_PENDING} when uploaded attachments`,
+    },
+  ])('$description', ({ conditionOption, expectedResult }) => {
+    const results = getCompletionState(conditionOption);
+    expect(results).toEqual(expectedResult);
+  });
 });
 
-it('Returns status for recurring completion when requesting completions AND is pending AND is NOT random check', () => {
-  const results = getCompletionStatus(completionsAttachmentPendingNotRandomCheck);
-  expect(results).toEqual(expectCompletionStatus);
-});
-
-it('Returns state for recurring random select', () => {
-  const results = getCompletionState(completionsInitialRandomCheck);
-  expect(results).toEqual(COMPLETIONS_RANDOM_SELECT);
-});
-
-it('Returns state for recurring completion pending', () => {
-  const results = getCompletionState(
-    completionsAttachmentPendingAndRequestedPartialyReceivedNotRandomCheck
-  );
-  expect(results).toEqual(COMPLETIONS_PENDING);
-});
-
-it('Returns state for recurring completion required', () => {
-  const results = getCompletionState(completionsDefault);
-  expect(results).toEqual(COMPLETIONS_REQUIRED);
+describe('Completions requested received (getCompletionState)', () => {
+  test.each([
+    {
+      conditionOption: {
+        requested: [...requestedSomeTrueList],
+        isRandomCheck: false,
+        isAttachmentPending: false,
+      },
+      expectedResult: COMPLETIONS_REQUIRED,
+      description: `set state to ${COMPLETIONS_REQUIRED} the first time`,
+    },
+    {
+      conditionOption: {
+        requested: [...requestedSomeTrueList],
+        isRandomCheck: false,
+        isAttachmentPending: true,
+      },
+      expectedResult: COMPLETIONS_PENDING,
+      description: `set state to ${COMPLETIONS_PENDING} when uploaded attachments`,
+    },
+  ])('$description', ({ conditionOption, expectedResult }) => {
+    const results = getCompletionState(conditionOption);
+    expect(results).toEqual(expectedResult);
+  });
 });
