18523d45104f72d960a4348075284b14aece8c1a
Dan Nilsson <mail@dannilsson.se>
2022-04-04 16:15:11 +0200

    refactor: 💡 (viva-ms) undo destructing

--- services/viva-ms/src/helpers/validateApplicationStatus.js
+++ services/viva-ms/src/helpers/validateApplicationStatus.js
@@ -2,8 +2,8 @@ export default function validateApplicationStatus(statusList, requiredCodeList) 
   if (!Array.isArray(statusList)) {
     return false;
   }
-  const uniqueStatusCodes = new Set([...statusList.map(status => status.code)]);
-  const uniqueRequiredCodes = new Set([...requiredCodeList]);
+  const uniqueStatusCodes = new Set(statusList.map(({ code }) => code));
+  const uniqueRequiredCodes = new Set(requiredCodeList);
 
   const isEqualSize = uniqueStatusCodes.size === uniqueRequiredCodes.size;
   const hasSameCodes = [...uniqueStatusCodes].every(code =>
