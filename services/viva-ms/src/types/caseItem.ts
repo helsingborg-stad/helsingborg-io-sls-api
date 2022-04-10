56b2f76c158b1fcfe9f243cc5532ef3e4a2c40eb
Dan Nilsson <mail@dannilsson.se>
2022-04-10 03:28:16 +0200

    feat: (viva-ms) add interfaces

--- services/viva-ms/src/types/caseItem.ts
+++ services/viva-ms/src/types/caseItem.ts
@@ -1,4 +1,36 @@
-export interface CasePeriod {
-  startDate: number;
-  endDate: number;
+export interface CaseUser {
+  readonly personalNumber: string;
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly civilStatus: string;
+  readonly mobilePhone: string;
+  readonly email: string;
+  readonly address: {
+    readonly city: string;
+    readonly street: string;
+    readonly postalCode: string;
+  };
+  readonly uuid: string;
+  readonly createdAt: number;
 }
+
+export interface CasePeriod {
+  readonly startDate: number;
+  readonly endDate: number;
+}
+
+export interface CasePerson {
+  readonly personalNumber: number;
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly role: 'applicant' | 'coApplicant' | 'children' | 'unknown';
+  hasSigned?: boolean;
+}
+
+export interface CasePersonRoleType {
+  readonly client: 'applicant';
+  readonly partner: 'coApplicant';
+  readonly child: 'children';
+}
+
+export interface CaseItem {}
--- services/viva-ms/src/types/vivaMyPages.ts
+++ services/viva-ms/src/types/vivaMyPages.ts
@@ -1,23 +1,44 @@
-export interface VivaMyPagesPersonCase {
-  persons: {
-    person: Record<string, unknown> | Record<string, unknown>[] | null;
-  };
-  client: VivaCaseClient;
+export interface VivaMyPages {
+  readonly case: VivaMyPagesPersonCase;
+  readonly application: VivaMyPagesPersonApplication;
 }
 
-export interface VivaCaseClient {
-  type: string;
+export interface VivaMyPagesPersonCase {
+  readonly client: VivaClient;
+  readonly officers: VivaOfficer | VivaOfficer[];
+  readonly persons: VivaPerson | VivaPerson[] | null;
 }
 
 export interface VivaMyPagesPersonApplication {
-  workflowid?: string;
-  period: {
-    start: string;
-    end: string;
+  readonly workflowid?: string;
+  readonly period: {
+    readonly start: string;
+    readonly end: string;
   };
 }
 
-export interface VivaMyPages {
-  cases: VivaMyPagesPersonCase;
-  application: VivaMyPagesPersonApplication;
+export interface VivaClient {
+  readonly pnumber: string;
+  readonly fname: string;
+  readonly lname: string;
+  type: 'client';
+}
+
+export interface VivaPerson {
+  readonly pnumber: string;
+  readonly fname: string;
+  readonly lname: string;
+  readonly type: 'client' | 'partner' | 'child';
+  readonly startdate?: string | null;
+  readonly startend?: string | null;
+  readonly days?: string | null;
+}
+
+export interface VivaOfficer {
+  readonly name: string;
+  readonly type: string;
+  readonly typeenclair: string;
+  readonly phone: string | null;
+  readonly mail: string;
+  readonly title: string;
 }
