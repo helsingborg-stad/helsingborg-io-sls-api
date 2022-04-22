c7b7d98379bffb9d176611c591f0d2852f0bbe4d
Dan Nilsson <dan.nilsson@helsingborg.se>
2022-04-22 13:52:14 +0200

    Feat/create new application (#379)
    * wip: (viva-ms) new application
    
    wip: (new application)
    
    * feat: (viva-ms) add interfaces
    
    * refactor: (viva-ms) extract functions to helpers
    
    * feat: (viva-ms) add case related types
    
    * feat: (viva-ms) add role type
    
    * fix: (viva-ms) return personal number as string type
    
    * feat: (viva-ms) add global index to case item
    
    * refactor: (viva-ms) instant return
    
    * refactor: (viva-ms) make type updated at mandatory
    
    * refactor: (viva-ms) use type Record
    
    * refactor: (viva-ms) direct return
    
    * refactor: (viva-ms) use ?? syntax instead
    
    * refactor: (viva-ms) move helper functions
    
    * refactor: (viva-ms) convert createCase to typescript
    
    * feat: (viva-ms) add event if new application
    
    * refactor: (viva-ms) move create new app. to its own lambda
    
    * refactor: 💡 (viva-ms) implement typescript enums
    
    * fix: (viva-ms) remove falsy return type
    
    * refactor: (viva-ms) make type params optionals
    
    * feat: (viva-ms) add Viva status type
    
    * feat: (viva-ms) add lambda create new Viva case
    
    Used for 'Grundansökan'
    
    * test: (event) add user for new application
    
    * fix: (git) add tmp to ignore
    
    * refactor: 💡 (viva-ms) rename function
    
    * fix: 🐛 (viva-ms) for initial setup we need null
    
    * refactor: 💡 (viva-ms) use ? syntax in condition
    
    * refactor: 💡 (viva-ms) extract role type to constant
    
    * refactor: 💡 (viva-ms) extract 'currentPosition' to new interfac
    
    * refactor: 💡 (viva-ms) add interface for answer field
    
    * refactor: 💡 (viva-ms) add interface for user case address
    
    * refactor: 💡 (viva-ms) add interface for Viva period
    
    * refactor: 💡 (viva-ms) make encryptoin functions dumber
    
    * feat: 🎸 (viva-ms) add initial case attributes
    
    * style: 💄 (viva-ms) remove unused imports
    
    * refactor: 💡 (viva-ms) extract create case person list
    
    * test: 💍 (new application) add test user to event mocj
    
    * style: 💄 (viva-ms) linting
    
    * refactor: 💡 (viva-ms) move initializing outside object
    
    * refactor: 💡 (viva-ms) getCaseListByPeriod
    
    * refactor: 💡 (viva-ms) rename variable
    
    * refactor: 💡 (viva-ms) getFormTemplates helper
    
    * refactor: 💡 (viva-ms) add enum for encryption type
    
    * fix: 🐛 (viva-ms) prevent from creating duplicated case
    
    * fix: 🐛 (viva-ms) get init form encryption
    
    * feat: 🎸 (viva-ms) add enum for Viva person type
    
    feat: 🎸 (viva-ms) add enum for Viva person type
    
    feat: 🎸 (viva-ms) use enum Viva person type
    
    * fix: 🐛 (viva-ms) wrong data structure
    
    * test: 💍 (viva-ms) add tests covering case create helpers
    
    * fix: (viva-ms) hotfix decision
    
    Before this fix the case will not be processed if decision is an list. This fix will always take the latest by date and make the calculation pass

---
+++ services/viva-ms/src/helpers/createCase.ts
@@ -0,0 +1,134 @@
+import uuid from 'uuid';
+
+import {
+  VivaMyPagesPersonApplication,
+  VivaMyPagesPersonCase,
+  VivaPerson,
+  VivaPersonType,
+} from '../types/vivaMyPages';
+import {
+  CaseForm,
+  CasePeriod,
+  CasePerson,
+  CasePersonRole,
+  CaseUser,
+  CaseFormEncryption,
+  EncryptionType,
+} from '../types/caseItem';
+
+export interface CasePersonRoleType {
+  readonly client: CasePersonRole.Applicant;
+  readonly partner: CasePersonRole.CoApplicant;
+  readonly child: CasePersonRole.Children;
+}
+
+const ROLE_TYPE: CasePersonRoleType = {
+  client: CasePersonRole.Applicant,
+  partner: CasePersonRole.CoApplicant,
+  child: CasePersonRole.Children,
+};
+
+function stripNonNumericalCharacters(valueIncludingChars: string) {
+  const matchNonNumericalCharactersRegex = /\D/g;
+  return valueIncludingChars.replace(matchNonNumericalCharactersRegex, '');
+}
+
+function getPeriodInMilliseconds(vivaApplication: VivaMyPagesPersonApplication): CasePeriod {
+  return {
+    startDate: Date.parse(vivaApplication.period.start),
+    endDate: Date.parse(vivaApplication.period.end),
+  };
+}
+
+function getCasePersonList(vivaCase: VivaMyPagesPersonCase): CasePerson[] {
+  const vivaPerson = vivaCase.persons?.person;
+  const vivaClient = vivaCase.client;
+  vivaClient.type = VivaPersonType.Client;
+
+  const personList: VivaPerson[] = [vivaClient as VivaPerson];
+
+  if (Array.isArray(vivaPerson)) {
+    personList.push(...vivaPerson);
+  } else if (vivaPerson != undefined) {
+    personList.push(vivaPerson);
+  }
+
+  return personList.map(mapperCasePersonFromVivaPerson);
+}
+
+function mapperCasePersonFromVivaPerson(vivaPerson: VivaPerson): CasePerson {
+  const { pnumber, fname, lname, type } = vivaPerson;
+  const role = ROLE_TYPE[type] ?? CasePersonRole.Unknown;
+  const isApplicant = [CasePersonRole.Applicant, CasePersonRole.CoApplicant].includes(role);
+
+  const person: CasePerson = {
+    personalNumber: stripNonNumericalCharacters(pnumber),
+    firstName: fname,
+    lastName: lname,
+    role,
+  };
+
+  if (isApplicant) {
+    person.hasSigned = false;
+  }
+
+  return person;
+}
+
+function createCaseApplicantPerson(user: CaseUser): CasePerson {
+  const { personalNumber, firstName, lastName } = user;
+  return {
+    personalNumber,
+    firstName,
+    lastName,
+    role: CasePersonRole.Applicant,
+    hasSigned: false,
+  };
+}
+
+function getUserByRole(personList: CasePerson[], role: CasePersonRole): CasePerson | undefined {
+  return personList.find(user => user.role === role);
+}
+
+function getVivaChildren(casePersonList: CasePerson[]): CasePerson[] {
+  return casePersonList.filter(person => person.role === 'children');
+}
+
+function getInitialFormAttributes(
+  formIdList: string[],
+  encryption: CaseFormEncryption
+): Record<string, CaseForm> {
+  const initialFormAttributes: CaseForm = {
+    answers: [],
+    encryption,
+    currentPosition: {
+      currentMainStep: 1,
+      currentMainStepIndex: 0,
+      index: 0,
+      level: 0,
+    },
+  };
+
+  return formIdList.reduce(
+    (allFormIds, currentFormId) => ({ ...allFormIds, [currentFormId]: initialFormAttributes }),
+    {}
+  );
+}
+
+function getFormEncryptionAttributes(isCoApplicant: boolean): CaseFormEncryption {
+  return {
+    type: EncryptionType.Decrypted,
+    ...(isCoApplicant && { symmetricKeyName: uuid.v4() }),
+  };
+}
+
+export default {
+  stripNonNumericalCharacters,
+  getPeriodInMilliseconds,
+  getCasePersonList,
+  getUserByRole,
+  getVivaChildren,
+  getInitialFormAttributes,
+  getFormEncryptionAttributes,
+  createCaseApplicantPerson,
+};
