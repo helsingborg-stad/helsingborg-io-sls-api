c3731897e59fba1b874822dd475772c98ac05e76
Petter Andersson <petter.andersson@helsingborg.se>
2022-04-25 15:01:47 +0200

    feat: new request handler for typescript
    fix: adding options
    
    fix: change name

---
+++ libs/https.ts
@@ -0,0 +1,53 @@
+import axios, { AxiosError, Method } from 'axios';
+import https from 'https';
+interface RequestConfig<T = unknown> {
+  method?: Method;
+  data?: T;
+  headers?: Record<string, unknown>;
+  timeout?: number;
+}
+
+interface HttpError {
+  status: number;
+  message: string;
+}
+
+type ErrorTransform = (error: HttpError) => HttpError;
+
+const defaultErrorTransform: ErrorTransform = (error: HttpError) => {
+  return error;
+};
+
+function errorExtractor(e: AxiosError): HttpError {
+  return {
+    status: e.response?.status ?? 500,
+    message: e.response?.statusText ?? e.message,
+  };
+}
+
+async function request<Response = unknown, Request = unknown>(
+  url: string,
+  config: RequestConfig<Request> = {},
+  errorTransform: ErrorTransform = defaultErrorTransform,
+  options: https.AgentOptions = {}
+): Promise<Response> {
+  try {
+    const aggregatedConfig = {
+      ...config,
+      httpsAgent: new https.Agent({ ...options }),
+      headers: {
+        'Content-Type': 'application/json',
+        ...config.headers,
+      },
+      maxContentLength: Infinity,
+      maxBodyLength: Infinity,
+    };
+    return (await axios(url, aggregatedConfig)).data;
+  } catch (e) {
+    throw errorTransform(errorExtractor(e as AxiosError));
+  }
+}
+
+export default {
+  request,
+};
