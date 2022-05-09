fc3846a97eb497e26cee75512c650ac7f01bb364
MasterKenth <MasterKenth@users.noreply.github.com>
2022-05-09 15:15:12 +0200

    refactor/generate-recurring-html-solid-ts (#396)
    * refactor(generate-html): convert generateRecurringCaseHtml to SOLID and add typings
    
    * refactor(handlebars): move handlebars helpers into separate file
    
    enables use of a vscode preview extension
    
    * refactor(generaterecurringcasehtml): organize imports
    
    * refactor(viva-ms): convert handlebars helpers to ts

--- services/viva-ms/src/lambdas/generateRecurringCaseHtml.ts
+++ services/viva-ms/src/lambdas/generateRecurringCaseHtml.ts
@@ -1,16 +1,26 @@
 import to from 'await-to-js';
-import handlebars from '../helpers/htmlTemplate';
+import {
+  GetObjectOutput as S3GetObjectOutput,
+  PutObjectOutput as S3PutObjectOutput,
+} from 'aws-sdk/clients/s3';
+import { AWSError } from 'aws-sdk/lib/error';
+import { PromiseResult } from 'aws-sdk/lib/request';
+
 import config from '../libs/config';
 import log from '../libs/logs';
-import { getItem as getStoredUserCase } from '../libs/queries';
 import params from '../libs/params';
 import S3 from '../libs/S3';
+import { getItem } from '../libs/queries';
 
+import handlebars from '../helpers/htmlTemplate';
 import createRecurringCaseTemplate from '../helpers/createRecurringCaseTemplate';
 import putVivaMsEvent from '../helpers/putVivaMsEvent';
 import { getClosedUserCases, updateVivaCaseState } from '../helpers/dynamoDb';
 
 interface CaseItem {
+  id: string;
+  PK: string;
+  SK: string;
   updatedAt: number;
   forms: Record<
     string,
@@ -18,131 +28,49 @@ interface CaseItem {
       answers: unknown;
     }
   >;
+  currentFormId: string;
 }
 
-export async function main(event, context) {
-  const { caseKeys } = event.detail;
-
-  const [getCaseItemError, storedUserCase] = await getStoredUserCase(
-    config.cases.tableName,
-    caseKeys.PK,
-    caseKeys.SK
-  );
-  const { Item: caseItem } = storedUserCase;
-
-  if (getCaseItemError) {
-    log.error(
-      'Error getting stored case from the cases table',
-      context.awsRequestId,
-      'service-viva-ms-generateRecurringCaseHtml-000',
-      getCaseItemError
-    );
-    return false;
-  }
-  const [paramsReadError, vivaCaseSSMParams] = await to(
-    params.read(config.cases.providers.viva.envsKeyName)
-  );
-  if (paramsReadError) {
-    log.error(
-      'Read ssm params ´config.cases.providers.viva.envsKeyName´ failed',
-      context.awsRequestId,
-      'service-viva-ms-generateRecurringCaseHtml-002',
-      paramsReadError
-    );
-    return false;
-  }
-
-  const [getClosedCasesError, closedUserCases] = await to(getClosedUserCases(caseKeys.PK));
-  if (getClosedCasesError) {
-    log.error(
-      'Error getting previous items from the cases table',
-      context.awsRequestId,
-      'service-viva-ms-generateRecurringCaseHtml-006',
-      getClosedCasesError
-    );
-    return false;
-  }
-
-  const { Items: closedCaseList } = closedUserCases as { Items: CaseItem[] };
-
-  const { recurringFormId } = vivaCaseSSMParams;
-  let changedAnswerValues = caseItem.forms[caseItem.currentFormId]?.answers ?? [];
-  if (closedCaseList.length > 0) {
-    const [closedCase] = closedCaseList.sort((caseA, caseB) => caseB.updatedAt - caseA.updatedAt);
-
-    changedAnswerValues = getChangedCaseAnswerValues(
-      caseItem.forms[recurringFormId].answers,
-      closedCase.forms[recurringFormId].answers
-    );
-  }
-
-  const [s3GetObjectError, hbsTemplateS3Object] = await to(
-    S3.getFile(process.env.PDF_STORAGE_BUCKET_NAME, 'templates/ekb-recurring-v2.hbs')
-  );
-  if (s3GetObjectError) {
-    log.error(
-      'Failed to get object from S3 bucket',
-      context.awsRequestId,
-      'service-viva-ms-generateRecurringCaseHtml-001',
-      s3GetObjectError
-    );
-    return false;
-  }
-
-  const handlebarsTemplateFileBody = hbsTemplateS3Object.Body.toString();
-  const template = handlebars.compile(handlebarsTemplateFileBody);
-
-  const caseTemplateData = createRecurringCaseTemplate(caseItem, changedAnswerValues);
-  const html = template(caseTemplateData);
-
-  const caseHtmlKey = `html/case-${caseItem.id}.html`;
-  const [s3StoreFileError] = await to(
-    S3.storeFile(process.env.PDF_STORAGE_BUCKET_NAME, caseHtmlKey, html)
-  );
-  if (s3StoreFileError) {
-    log.error(
-      'Failed to put object to S3 bucket',
-      context.awsRequestId,
-      'service-viva-ms-generateRecurringCaseHtml-003',
-      s3StoreFileError
-    );
-    return false;
-  }
-
-  const [updateCaseError] = await to(updateVivaCaseState(caseItem));
-  if (updateCaseError) {
-    log.error(
-      'Failed to update case state attribute',
-      context.awsRequestId,
-      'service-viva-ms-generateRecurringCaseHtml-004',
-      updateCaseError
-    );
-    return false;
-  }
-
-  const [putEventError] = await to(
-    putVivaMsEvent.htmlGeneratedSuccess({
-      pdfStorageBucketKey: caseHtmlKey,
-      keys: {
-        PK: caseItem.PK,
-        SK: caseItem.SK,
-      },
-    })
-  );
-  if (putEventError) {
-    log.error(
-      'Put event ´htmlGeneratedSuccess´ failed',
-      context.awsRequestId,
-      'service-viva-ms-generateRecurringCaseHtml-005',
-      putEventError
-    );
-    return false;
-  }
-
-  return true;
+interface FormAnswer {
+  field: {
+    id: string;
+    tags: string[];
+  };
+  value: unknown;
+}
+export interface LambdaRequest {
+  detail: {
+    caseKeys: {
+      PK: string;
+      SK: string;
+    };
+  };
 }
 
-function getChangedCaseAnswerValues(currentAnswerList, previousAnswerList) {
+export type LambdaResponse = boolean;
+
+export interface Dependencies {
+  getStoredUserCase: (
+    TableName: string,
+    PK: string,
+    SK: string
+  ) => Promise<[Error, { Item: CaseItem }]>;
+  readParams: (id: string) => Promise<{ recurringFormId: string }>;
+  getClosedUserCases: (PK: string) => Promise<{ Items: CaseItem[] }>;
+  getFile: (bucket: string, key: string) => Promise<PromiseResult<S3GetObjectOutput, AWSError>>;
+  storeFile: (
+    bucket: string,
+    key: string,
+    content: string
+  ) => Promise<PromiseResult<S3PutObjectOutput, AWSError>>;
+  updateVivaCaseState: (caseItem: CaseItem) => Promise<void>;
+  putHtmlGeneratedSuccessEvent: (detail: unknown) => Promise<void>;
+}
+
+function getChangedCaseAnswerValues(
+  currentAnswerList: FormAnswer[],
+  previousAnswerList: FormAnswer[]
+): FormAnswer[] {
   return currentAnswerList.map(currentAnswer => {
     const tags = [...currentAnswer.field.tags];
 
@@ -153,6 +81,7 @@ function getChangedCaseAnswerValues(currentAnswerList, previousAnswerList) {
     if (previousAnswer?.value !== currentAnswer.value) {
       tags.push('changed');
     }
+
     return {
       ...currentAnswer,
       field: {
@@ -162,3 +91,123 @@ function getChangedCaseAnswerValues(currentAnswerList, previousAnswerList) {
     };
   });
 }
+
+function validateRequest(data: LambdaRequest): boolean {
+  const isValid = !!data?.detail?.caseKeys?.PK && !!data?.detail?.caseKeys?.SK;
+  return isValid;
+}
+
+function printErrorAndReturn(msg: string, customData?: unknown): false {
+  log.writeError(msg, customData);
+  return false;
+}
+
+export async function generateRecurringCaseHtml(
+  input: LambdaRequest,
+  dependencies: Dependencies
+): Promise<LambdaResponse> {
+  if (!validateRequest(input)) {
+    return printErrorAndReturn(`invalid input data`, input);
+  }
+
+  const { caseKeys } = input.detail;
+
+  const [getCaseItemError, storedUserCase] = await dependencies.getStoredUserCase(
+    config.cases.tableName,
+    caseKeys.PK,
+    caseKeys.SK
+  );
+  if (!storedUserCase || getCaseItemError) {
+    return printErrorAndReturn('Error getting stored case from the cases table', getCaseItemError);
+  }
+
+  const { Item: caseItem } = storedUserCase;
+
+  const [paramsReadError, vivaCaseSSMParams] = await to(
+    dependencies.readParams(config.cases.providers.viva.envsKeyName)
+  );
+
+  if (!vivaCaseSSMParams || paramsReadError) {
+    return printErrorAndReturn('Read SSM params failed', paramsReadError);
+  }
+
+  const [getClosedCasesError, closedUserCases] = await to(
+    dependencies.getClosedUserCases(caseKeys.PK)
+  );
+
+  if (!closedUserCases || getClosedCasesError) {
+    return printErrorAndReturn(
+      'Error getting previous items from the cases table',
+      getClosedCasesError
+    );
+  }
+
+  const { Items: closedCaseList } = closedUserCases;
+
+  const { recurringFormId } = vivaCaseSSMParams;
+  let changedAnswerValues = caseItem.forms[caseItem.currentFormId]?.answers ?? [];
+  if (closedCaseList.length > 0) {
+    const [closedCase] = closedCaseList.sort((caseA, caseB) => caseB.updatedAt - caseA.updatedAt);
+
+    changedAnswerValues = getChangedCaseAnswerValues(
+      caseItem.forms[recurringFormId].answers as FormAnswer[],
+      closedCase.forms[recurringFormId].answers as FormAnswer[]
+    );
+  }
+
+  const [s3GetObjectError, hbsTemplateS3Object] = await to(
+    dependencies.getFile(
+      process.env.PDF_STORAGE_BUCKET_NAME ?? '',
+      'templates/ekb-recurring-v2.hbs'
+    )
+  );
+  if (!hbsTemplateS3Object || s3GetObjectError) {
+    return printErrorAndReturn('Failed to get handlebar template', s3GetObjectError);
+  }
+
+  const handlebarsTemplateFileBody = hbsTemplateS3Object.Body?.toString();
+  const template = handlebars.compile(handlebarsTemplateFileBody);
+
+  const caseTemplateData = createRecurringCaseTemplate(caseItem, changedAnswerValues);
+  const html = template(caseTemplateData);
+
+  const caseHtmlKey = `html/case-${caseItem.id}.html`;
+  const [s3StoreFileError] = await to(
+    dependencies.storeFile(process.env.PDF_STORAGE_BUCKET_NAME ?? '', caseHtmlKey, html)
+  );
+  if (s3StoreFileError) {
+    return printErrorAndReturn('Failed to store generated html', s3StoreFileError);
+  }
+
+  const [updateCaseError] = await to(dependencies.updateVivaCaseState(caseItem));
+  if (updateCaseError) {
+    return printErrorAndReturn('Failed to update case state attribute', updateCaseError);
+  }
+
+  const [putEventError] = await to(
+    dependencies.putHtmlGeneratedSuccessEvent({
+      pdfStorageBucketKey: caseHtmlKey,
+      keys: {
+        PK: caseItem.PK,
+        SK: caseItem.SK,
+      },
+    })
+  );
+  if (putEventError) {
+    return printErrorAndReturn('Failed to put htmlGeneratedSuccess event', putEventError);
+  }
+
+  return true;
+}
+
+export const main = log.wrap(async event => {
+  return generateRecurringCaseHtml(event, {
+    getStoredUserCase: getItem,
+    readParams: params.read,
+    getClosedUserCases,
+    getFile: S3.getFile,
+    storeFile: S3.storeFile,
+    updateVivaCaseState,
+    putHtmlGeneratedSuccessEvent: putVivaMsEvent.htmlGeneratedSuccess,
+  });
+});
