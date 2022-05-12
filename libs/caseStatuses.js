ca7354f60d6adf9dbac149b82ed308c6ea50bcf9
Dan Nilsson <mail@dannilsson.se>
2022-05-12 15:25:09 +0200

    style: 💄 (status-text) remove dot

--- libs/caseStatuses.js
+++ libs/caseStatuses.js
@@ -112,17 +112,17 @@ const statuses = [
   {
     type: ACTIVE_COMPLETION_REQUIRED_VIVA,
     name: 'Ansökan behöver kompletteras',
-    description: 'Skicka in senast: #COMPLETION_DUEDATE.',
+    description: 'Skicka in senast: #COMPLETION_DUEDATE',
     detailedDescription:
-      'Du har skickat in en ansökan för #MONTH_NAME. För att vi ska kunna behandla din ansökan finns det uppgifter som du behöver komplettera.\n\nSkicka in senast: #COMPLETION_DUEDATE.',
+      'Du har skickat in en ansökan för #MONTH_NAME. För att vi ska kunna behandla din ansökan finns det uppgifter som du behöver komplettera.\n\nSkicka in senast: #COMPLETION_DUEDATE',
   },
   {
     type: ACTIVE_COMPLETION_SUBMITTED_VIVA,
     name: 'Komplettering inskickad',
     description:
-      'Du har skickat in #ATTACHMENT_UPLOADED_COUNT bilder/filer för komplettering.\n\nDu kan skicka in fler fram till: #COMPLETION_DUEDATE.',
+      'Du har skickat in #ATTACHMENT_UPLOADED_COUNT bilder/filer för komplettering.\n\nDu kan skicka in fler fram till: #COMPLETION_DUEDATE',
     detailedDescription:
-      'Du har kompletterat med #ATTACHMENT_UPLOADED_COUNT bilder/filer. Har du fler bilder att skicka in startar du formuläret och laddar upp kompletterande bilder och signerar med BankID.\n\nDu kan fortsätta skicka in bilder fram till att senaste datumet för inskickning har passerat.\n\nDu kan skicka in fler fram till: #COMPLETION_DUEDATE.',
+      'Du har kompletterat med #ATTACHMENT_UPLOADED_COUNT bilder/filer. Har du fler bilder att skicka in startar du formuläret och laddar upp kompletterande bilder och signerar med BankID.\n\nDu kan fortsätta skicka in bilder fram till att senaste datumet för inskickning har passerat.\n\nDu kan skicka in fler fram till: #COMPLETION_DUEDATE',
   },
   {
     type: ACTIVE_RANDOM_CHECK_REQUIRED_VIVA,
@@ -136,7 +136,7 @@ const statuses = [
     description:
       'Du har skickat in #ATTACHMENT_UPLOADED_COUNT bilder/filer för stickprovskontrollen.\n\nDu kan skicka in fler fram till: #COMPLETION_DUEDATE',
     detailedDescription:
-      'Du har skickat in #ATTACHMENT_UPLOADED_COUNT för stickprovskontrollen. Har du fler bilder att skicka in startar du formuläret och laddar upp kompletterande bilder på rätt sida och signerar med BankID.\n\nDu kan fortsätta skicka in bilder fram till att senaste datumet för inskickning har passerat.\n\nDu kan skicka in fler fram till: #COMPLETION_DUEDATE.',
+      'Du har skickat in #ATTACHMENT_UPLOADED_COUNT för stickprovskontrollen. Har du fler bilder att skicka in startar du formuläret och laddar upp kompletterande bilder på rätt sida och signerar med BankID.\n\nDu kan fortsätta skicka in bilder fram till att senaste datumet för inskickning har passerat.\n\nDu kan skicka in fler fram till: #COMPLETION_DUEDATE',
   },
   {
     type: CLOSED_APPROVED_VIVA,
