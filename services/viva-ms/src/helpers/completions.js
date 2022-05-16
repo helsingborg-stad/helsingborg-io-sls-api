c9d2dc1a48d390ad8ac7c25eecfbf2760f6cb774
Dan Nilsson <mail@dannilsson.se>
2022-05-16 13:32:25 +0200

    Fix/completions (#400)
    * fix: 🐛 (viva-ms) status description
    
    * refactor: 💡 (status) change attribute name
    
    * refactor: 💡 (completions) select viva specifc status
    
    * refactor: 💡 (completions) texts
    
    * fix: 🐛 (status-text) #COMPLETION_DUEDATE
    
    * fix: 🐛 (status) add due date to description
    
    * style: 💄 (status-text) remove dot
    
    * feat: 🎸 (viva-ms) add custom status when due date passed
    
    * fix: 🐛 (status) description text
    
    * style: 💄 (status) remove dot

--- services/viva-ms/src/helpers/completions.js
+++ services/viva-ms/src/helpers/completions.js
@@ -9,7 +9,7 @@ import {
   ACTIVE_RANDOM_CHECK_REQUIRED_VIVA,
   ACTIVE_RANDOM_CHECK_SUBMITTED_VIVA,
   ACTIVE_COMPLETION_REQUIRED_VIVA,
-  ACTIVE_COMPLETION_SUBMITTED,
+  ACTIVE_COMPLETION_SUBMITTED_VIVA,
   ACTIVE_SUBMITTED,
 
   // state
@@ -40,7 +40,7 @@ export function getCompletionStatus(completions) {
   }
 
   if (isAttachmentPending) {
-    return getStatusByType(ACTIVE_COMPLETION_SUBMITTED);
+    return getStatusByType(ACTIVE_COMPLETION_SUBMITTED_VIVA);
   }
 
   return getStatusByType(ACTIVE_COMPLETION_REQUIRED_VIVA);
