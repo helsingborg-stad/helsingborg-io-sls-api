b7d24394941927c56a397cc3cdd48bd0a0d15aea
Dan Nilsson <mail@dannilsson.se>
2022-05-20 16:46:23 +0200

    fix: (hotfix case decision)
    When case status is active:ongoing and due date expired it could not be synced with Viva due to event rule pattern. This fix will ensure that the case always is up to date with Viva regardless if the user has not yet submitted the case
    
    refactor: (viva-ms) check if case list is empty
    
    fix: (viva-ms) filter case if workflow id exists
    
    missing details in json path

--- services/viva-ms/src/lambdas/syncWorkflow.js
+++ services/viva-ms/src/lambdas/syncWorkflow.js
@@ -1,6 +1,4 @@
-/* eslint-disable no-console */
 import to from 'await-to-js';
-import deepEqual from 'deep-equal';
 
 import config from '../libs/config';
 
@@ -13,10 +11,12 @@ import vivaAdapter from '../helpers/vivaAdapterRequestClient';
 export async function main(event, context) {
   const { personalNumber } = event.detail.user;
 
-  const [getCasesError, userCases] = await to(getCasesSumbittedOrProcessing(personalNumber));
+  const [getCasesError, userCases] = await to(
+    getSubmittedOrProcessingOrOngoingCases(personalNumber)
+  );
   if (getCasesError) {
     log.error(
-      'Could not get closed or submitted cases',
+      'Get closed or submitted or ongoing cases failed',
       context.awsRequestId,
       'service-viva-ms-syncWorkflow-001',
       getCasesError
@@ -25,12 +25,14 @@ export async function main(event, context) {
   }
 
   const caseList = userCases.Items;
-  if (caseList === undefined || caseList.length === 0) {
-    log.info('No active:submitted or active:processing case(s) found', context.awsRequestId, null);
+  const isEmptyCaseList = Array.isArray(caseList) && !caseList.length;
+  if (isEmptyCaseList) {
     return true;
   }
 
-  for (const caseItem of caseList) {
+  const caseListHasWorkflowId = caseList.filter(caseItem => !!caseItem.details.workflowId);
+
+  for (const caseItem of caseListHasWorkflowId) {
     const caseKeys = {
       PK: caseItem.PK,
       SK: caseItem.SK,
@@ -43,7 +45,7 @@ export async function main(event, context) {
     );
     if (adapterWorkflowGetError) {
       log.error(
-        'Could not get workflow information from Vada request',
+        'Get workflow from Vada request failed',
         context.awsRequestId,
         'service-viva-ms-syncWorkflow-002',
         adapterWorkflowGetError
@@ -51,45 +53,21 @@ export async function main(event, context) {
       continue;
     }
 
-    if (deepEqual(workflow.attributes, caseItem.details?.workflow)) {
-      log.info('Case workflow is in sync with Viva', context.awsRequestId, null);
-      continue;
-    }
-
-    const [updateDbWorkflowError] = await to(updateCaseWorkflow(caseKeys, workflow.attributes));
-    if (updateDbWorkflowError) {
-      log.error(
-        'Could not update case workflow details',
-        context.awsRequestId,
-        'service-viva-ms-syncWorkflow-003',
-        updateDbWorkflowError
-      );
-      return false;
-    }
-
-    const [putEventError] = await to(putVivaMsEvent.syncWorkflowSuccess({ caseKeys, workflow }));
-    if (putEventError) {
-      log.error(
-        'Could not put sync workflow success event',
-        context.awsRequestId,
-        'service-viva-ms-syncWorkflow-004',
-        putEventError
-      );
-      return false;
-    }
+    await updateCaseDetailsWorkflow(caseKeys, workflow.attributes);
+    await putVivaMsEvent.syncWorkflowSuccess({ caseKeys, workflow });
   }
 
   return true;
 }
 
-function getCasesSumbittedOrProcessing(personalNumber) {
+function getSubmittedOrProcessingOrOngoingCases(personalNumber) {
   const PK = `USER#${personalNumber}`;
 
   const queryParams = {
     TableName: config.cases.tableName,
     KeyConditionExpression: 'PK = :pk',
     FilterExpression:
-      '(begins_with(#status.#type, :statusTypeSubmitted) or begins_with(#status.#type, :statusTypeProcessing)) and provider = :provider',
+      '(begins_with(#status.#type, :statusTypeSubmitted) or begins_with(#status.#type, :statusTypeProcessing) or begins_with(#status.#type, :statusTypeOngoing)) and provider = :provider',
     ExpressionAttributeNames: {
       '#status': 'status',
       '#type': 'type',
@@ -98,6 +76,7 @@ function getCasesSumbittedOrProcessing(personalNumber) {
       ':pk': PK,
       ':statusTypeSubmitted': 'active:submitted',
       ':statusTypeProcessing': 'active:processing',
+      ':statusTypeOngoing': 'active:ongoing',
       ':provider': 'VIVA',
     },
   };
@@ -105,7 +84,7 @@ function getCasesSumbittedOrProcessing(personalNumber) {
   return dynamoDb.call('query', queryParams);
 }
 
-function updateCaseWorkflow(caseKeys, workflow) {
+function updateCaseDetailsWorkflow(caseKeys, newWorkflow) {
   const updateParams = {
     TableName: config.cases.tableName,
     Key: {
@@ -113,7 +92,9 @@ function updateCaseWorkflow(caseKeys, workflow) {
       SK: caseKeys.SK,
     },
     UpdateExpression: 'SET details.workflow = :newWorkflow',
-    ExpressionAttributeValues: { ':newWorkflow': workflow },
+    ExpressionAttributeValues: {
+      ':newWorkflow': newWorkflow,
+    },
     ReturnValues: 'NONE',
   };
 
