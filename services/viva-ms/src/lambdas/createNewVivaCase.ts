da2f60dfbf3a4b7f1a769644dae3175bcb8089a8
Karl Mösenbacher <karlmosenbacher@hotmail.com>
2022-05-25 08:07:06 +0200

    Feat/create new viva case tests (#408)
    * fix(createnewcase): rewrite to agreed format
    
    * fix(createnewvivacase): add tests
    
    * fix(createnewvivacase): only fetch count of user cases items
    
    * fix(createnewcase test): align tests with update lambda code
    
    * fix(createnewvivacase test): refactor lambda input
    
    Co-authored-by: Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>

--- services/viva-ms/src/lambdas/createNewVivaCase.ts
+++ services/viva-ms/src/lambdas/createNewVivaCase.ts
@@ -1,6 +1,4 @@
-import to from 'await-to-js';
 import uuid from 'uuid';
-import { GetItemOutput, QueryOutput } from 'aws-sdk/clients/dynamodb';
 
 import config from '../libs/config';
 
@@ -28,51 +26,61 @@ import {
   CasePersonRole,
   CasePerson,
 } from '../types/caseItem';
-import { VivaApplicationStatus } from '../types/vivaMyPages';
+import { VivaParametersResponse } from '../types/ssmParameters';
 
-export interface AWSEvent {
-  detail: {
-    user: CaseUser;
-    status: VivaApplicationStatus[];
+interface GetUserCaseListResponse {
+  Count: number;
+}
+
+interface LambdaDetails {
+  user: CaseUser;
+}
+
+export interface LambdaRequest {
+  detail: LambdaDetails;
+}
+export interface Dependencies {
+  createCase: typeof putItem;
+  readParams: (envsKeyName: string) => Promise<VivaParametersResponse>;
+  getUserCasesCount: (personalNumber: string) => Promise<GetUserCaseListResponse>;
+  getTemplates: typeof getFormTemplates;
+}
+
+function getUserCasesCount(personalNumber: string): Promise<GetUserCaseListResponse> {
+  const queryParams = {
+    TableName: config.cases.tableName,
+    KeyConditionExpression: 'PK = :pk',
+    ExpressionAttributeValues: {
+      ':pk': `USER#${personalNumber}`,
+    },
+    Select: 'COUNT',
   };
+
+  return dynamoDb.call('query', queryParams);
 }
 
-interface AWSContext {
-  awsRequestId: string;
-}
-
-interface DynamoDbPutParam {
-  TableName: string;
-  Item: CaseItem;
-}
-
-export interface LambdaContext {
-  putItem({ TableName, Item }: DynamoDbPutParam): Promise<GetItemOutput>;
-}
-
-export function main(event: AWSEvent, context: AWSContext) {
-  return lambda(event, context, { putItem });
-}
-
-export async function lambda(
-  event: AWSEvent,
-  context: AWSContext,
-  lambdaContext: LambdaContext
+export async function createNewVivaCase(
+  input: LambdaRequest,
+  dependencies: Dependencies
 ): Promise<boolean> {
-  const { user } = event.detail;
-  const { newApplicationFormId, newApplicationCompletionFormId, newApplicationRandomCheckFormId } =
-    await params.read(config.cases.providers.viva.envsKeyName);
+  const { user } = input.detail;
+  const { readParams, getTemplates, getUserCasesCount, createCase } = dependencies;
 
-  const [, queryResponse] = await to(getUserCaseList(user.personalNumber));
-  if (queryResponse?.Count) {
+  const { Count } = await getUserCasesCount(user.personalNumber);
+
+  if (Count > 0) {
     return true;
   }
+
+  const { newApplicationFormId, newApplicationCompletionFormId, newApplicationRandomCheckFormId } =
+    await readParams(config.cases.providers.viva.envsKeyName);
 
   const formIdList = [
     newApplicationFormId,
     newApplicationCompletionFormId,
     newApplicationRandomCheckFormId,
   ];
+
   const isCoApplicant = false;
   const initialFormEncryption = createCaseHelper.getFormEncryptionAttributes(isCoApplicant);
   const initialFormList = createCaseHelper.getInitialFormAttributes(
@@ -88,6 +96,22 @@ export async function lambda(
   const initialStatus: CaseStatus = getStatusByType(NEW_APPLICATION_VIVA);
   const initialPersonList: CasePerson[] = [createCaseHelper.createCaseApplicantPerson(user)];
 
+  const extendedCasePersonList = initialPersonList.map(person => {
+    if (person.role === CasePersonRole.Applicant && person.personalNumber === user.personalNumber) {
+      return { ...user, ...person };
+    }
+    return person;
+  });
+
+  const formTemplates = await getTemplates(formIdList);
+
+  const prePopulatedForms: Record<string, CaseForm> = populateFormWithPreviousCaseAnswers(
+    initialFormList,
+    extendedCasePersonList,
+    formTemplates,
+    {}
+  );
+
   const newVivaCaseItem: CaseItem = {
     id,
     PK,
@@ -97,7 +121,7 @@ export async function lambda(
     createdAt: timestampNow,
     updatedAt: 0,
     status: initialStatus,
-    forms: null,
+    forms: prePopulatedForms,
     provider: CASE_PROVIDER_VIVA,
     persons: initialPersonList,
     details: {
@@ -110,58 +134,21 @@ export async function lambda(
     currentFormId: newApplicationFormId,
   };
 
-  const extendedCasePersonList = initialPersonList.map(person => {
-    if (person.role === CasePersonRole.Applicant && person.personalNumber === user.personalNumber) {
-      return { ...user, ...person };
-    }
-    return person;
-  });
+  const createdCaseItem = (await createCase({
+    TableName: config.cases.tableName,
+    Item: newVivaCaseItem,
+  })) as CaseItem;
 
-  const [, formTemplates] = await to(getFormTemplates(formIdList));
-
-  const prePopulatedForms: Record<string, CaseForm> = populateFormWithPreviousCaseAnswers(
-    initialFormList,
-    extendedCasePersonList,
-    formTemplates,
-    {}
-  );
-
-  newVivaCaseItem.forms = prePopulatedForms;
-
-  const [putItemError, createdCaseItem] = await to(
-    lambdaContext.putItem({
-      TableName: config.cases.tableName,
-      Item: newVivaCaseItem,
-    })
-  );
-  if (putItemError) {
-    log.error(
-      'Failed to create new Viva case',
-      context.awsRequestId,
-      'service-viva-ms-createNewVivaCase-015',
-      putItemError
-    );
-    return false;
-  }
-
-  log.info(
-    'New Viva case created successfully',
-    context.awsRequestId,
-    'service-viva-ms-createNewVivaCase-020',
-    createdCaseItem
-  );
+  log.writeInfo(`New case with id: ${createdCaseItem.id} successfully created`);
 
   return true;
 }
 
-function getUserCaseList(personalNumber: string): Promise<QueryOutput> {
-  const queryParams = {
-    TableName: config.cases.tableName,
-    KeyConditionExpression: 'PK = :pk',
-    ExpressionAttributeValues: {
-      ':pk': `USER#${personalNumber}`,
-    },
-  };
-
-  return dynamoDb.call('query', queryParams);
-}
+export const main = log.wrap(event => {
+  return createNewVivaCase(event, {
+    createCase: putItem,
+    readParams: params.read,
+    getUserCasesCount,
+    getTemplates: getFormTemplates,
+  });
+});
