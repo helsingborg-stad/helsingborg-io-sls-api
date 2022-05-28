b65ef048a46c689cb131ca89791ef93dcb8ddffb
Dan Nilsson <mail@dannilsson.se>
2022-05-28 08:08:34 +0200

    test: 💍 (viva-ms) add test if Viva response eq ERROR

--- services/viva-ms/test/lambdas/submitCompletion.test.ts
+++ services/viva-ms/test/lambdas/submitCompletion.test.ts
@@ -8,47 +8,48 @@ const PK = 'mockPK';
 const SK = 'mockSK';
 const postVivaResponseId = 'mockPostResponseId';
 
-// const form = {
-//   answers: [],
-// };
+const form = {
+  answers: [],
+  currentPosition: {
+    currentMainStep: 0,
+    currentMainStepIndex: 0,
+    index: 0,
+    level: 0,
+  },
+  encryption: {
+    type: EncryptionType.Decrypted,
+  },
+};
+
+const details = {
+  workflowId: 'workflowId',
+  period: {
+    startDate: 123,
+    endDate: 456,
+  },
+};
 
 const myCase: CaseItem = {
   id: '123',
-  PK: '1',
-  SK: '2',
+  PK,
+  SK,
   state: 'SOME STATE',
   expirationTime: 0,
   createdAt: 0,
   updatedAt: 0,
   status: {
     type: 'myStatusType',
-    name: 'myName',
-    description: 'myDescription',
+    name: 'myStatusName',
+    description: 'myStatusDescription',
   },
   forms: {
-    abc: {
-      answers: [],
-      currentPosition: {
-        currentMainStep: 0,
-        currentMainStepIndex: 0,
-        index: 0,
-        level: 0,
-      },
-      encryption: {
-        type: EncryptionType.Decrypted,
-      },
-    },
+    [randomCheckFormId]: form,
+    [completionFormId]: form,
   },
   provider: 'VIVA',
   persons: [],
-  details: {
-    period: {
-      startDate: 0,
-      endDate: 0,
-    },
-    workflowId: '321',
-  },
-  currentFormId: '1234',
+  details,
+  currentFormId: completionFormId,
 };
 
 let input: LambdaRequest;
@@ -56,7 +57,7 @@ let dependencies: Dependencies;
 
 beforeEach(() => {
   dependencies = {
-    getStoredUserCase: () => Promise.resolve([null, { Item: null }]),
+    getStoredUserCase: () => Promise.resolve([null, { Item: myCase }]),
     readParams: () => Promise.resolve({ randomCheckFormId, completionFormId }),
     postCompletion: () => Promise.resolve({ status: 'OK', id: postVivaResponseId }),
     updateCase: () => Promise.resolve(null),
@@ -77,3 +78,19 @@ it('successfully submits completions', async () => {
 
   expect(result).toBe(true);
 });
+
+it('returns true if `currentFormId` does not match `randomCheckFormId` or `completionFormId`', async () => {
+  myCase.currentFormId = 'No matching form id';
+  const result = await submitCompletion(input, dependencies);
+
+  expect(result).toBe(true);
+});
+
+it('returns false if `postCompletionResponse is successful but `status` is ERROR`', async () => {
+  myCase.currentFormId = completionFormId;
+  dependencies.postCompletion = () => Promise.resolve({ status: 'ERROR', id: postVivaResponseId });
+  const result = await submitCompletion(input, dependencies);
+
+  expect(result).toBe(false);
+});
+             
