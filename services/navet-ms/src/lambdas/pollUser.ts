9a01b943a10e81082e197e8ae659c7d2e42ac99e
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-07-19 11:17:52 +0200

    fix(polluser): make dependencies changes

--- services/navet-ms/src/lambdas/pollUser.ts
+++ services/navet-ms/src/lambdas/pollUser.ts
@@ -17,18 +17,19 @@ export interface Input {
 export interface Dependencies {
   requestNavetUserXml: (personalNumber: string) => Promise<string>;
   getParsedNavetPersonPost: (xml: string) => Promise<NavetUser>;
+  putSuccessEvent: (eventDetail: CaseUser, detailType: string, source: string) => Promise<void>;
 }
 
 export async function pollUser(input: Input, dependencies: Dependencies) {
   const { user } = input.detail;
 
-  const { requestNavetUserXml, getParsedNavetPersonPost } = dependencies;
+  const { requestNavetUserXml, getParsedNavetPersonPost, putSuccessEvent } = dependencies;
 
   const navetXmlResponse = await requestNavetUserXml(user.personalNumber);
   const navetUser = await getParsedNavetPersonPost(navetXmlResponse);
   const eventDetail = getNavetPollEventDetail(navetUser);
 
-  await putEvent(eventDetail, 'navetMsPollUserSuccess', 'navetMs.pollUser');
+  await putSuccessEvent(eventDetail, 'navetMsPollUserSuccess', 'navetMs.pollUser');
 
   return true;
 }
@@ -39,9 +40,9 @@ function getNavetPollEventDetail(navetUser: NavetUser): CaseUser {
     firstName: navetUser.Namn.Fornamn,
     lastName: navetUser.Namn.Efternamn,
     address: {
-      street: navetUser.Adresser.Folkbokforingsadress.Utdelningsadress2,
-      postalCode: navetUser.Adresser.Folkbokforingsadress.PostNr,
-      city: navetUser.Adresser.Folkbokforingsadress.Postort,
+      street: navetUser?.Adresser?.Folkbokforingsadress?.Utdelningsadress2,
+      postalCode: navetUser?.Adresser?.Folkbokforingsadress?.PostNr,
+      city: navetUser?.Adresser?.Folkbokforingsadress?.Postort,
     },
     civilStatus: navetUser.Civilstand.CivilstandKod,
   };
@@ -51,5 +52,6 @@ export const main = log.wrap(async event => {
   return pollUser(event, {
     requestNavetUserXml: requestNavetUser,
     getParsedNavetPersonPost: getNavetPersonPost,
+    putSuccessEvent: putEvent,
   });
 });
