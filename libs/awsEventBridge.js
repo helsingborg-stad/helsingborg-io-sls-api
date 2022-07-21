0679ed2268f3abb52648bbe69476003b69c28556
Karl Mösenbacher <karlmosenbacher@hotmail.com>
2022-07-21 16:27:08 +0200

    Feat/xray (#425)
    * fix(xray): add xray
    
    * fix(libs): add subsegment with metadata
    
    - Will be added in xray under source
    
    * fix(awseventbridge): fix imports
    
    Co-authored-by: Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>

--- libs/awsEventBridge.js
+++ libs/awsEventBridge.js
@@ -1,6 +1,33 @@
 import AWS from 'aws-sdk';
+import Xray from 'aws-xray-sdk';
 
-const eventBridge = new AWS.EventBridge();
+const XrayEnhancedAWS = Xray.captureAWS(AWS);
+const eventBridge = new XrayEnhancedAWS.EventBridge();
+
+function putEventRequest(Detail, DetailType, Source) {
+  const params = {
+    Entries: [
+      {
+        Detail: JSON.stringify(Detail),
+        DetailType,
+        Source,
+        Time: new Date(),
+      },
+    ],
+  };
+
+  return eventBridge.putEvents(params).promise();
+}
+
+function addXrayMetadata(DetailType, Source) {
+  const segment = Xray.getSegment();
+  const subSegment = segment.addNewSubsegment('PutEventsMetadata');
+
+  subSegment.addMetadata('DetailType', DetailType);
+  subSegment.addMetadata('Source', Source);
+
+  subSegment.close();
+}
 
 /**
  *
@@ -9,15 +36,7 @@ const eventBridge = new AWS.EventBridge();
  * @param {String} Source The source of the event.
  */
 export function putEvent(Detail, DetailType, Source) {
-  const params = {
-    Entries: [
-      {
-        Detail: JSON.stringify(Detail),
-        DetailType,
-        Source,
-      },
-    ],
-  };
+  addXrayMetadata(DetailType, Source);
 
-  return eventBridge.putEvents(params).promise();
+  return putEventRequest(Detail, DetailType, Source);
 }
