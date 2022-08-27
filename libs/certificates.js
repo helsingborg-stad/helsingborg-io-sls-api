f9c9411209002c58e24dbd5d9dcf2bf715bf3b8d
Dan Nilsson <mail@dannilsson.se>
2022-08-27 17:49:20 +0200

    refactor: 💡 (libs) import of S3 sdk

--- libs/certificates.js
+++ libs/certificates.js
@@ -1,15 +1,16 @@
-import AWS from 'aws-sdk';
+import S3 from 'aws-sdk/clients/s3';
 
-const S3 = new AWS.S3();
+const s3Client = new S3({ apiVersion: '2006-03-01' });
 
-export const read = async (Bucket, Key) => {
+export function read(Bucket, Key) {
   try {
-    const file = await S3.getObject({
-      Bucket,
-      Key,
-    }).promise();
-    return file;
+    return s3Client
+      .getObject({
+        Bucket,
+        Key,
+      })
+      .promise();
   } catch (error) {
     throw `S3 - ${error.code}:${error.message}`;
   }
-};
+}
