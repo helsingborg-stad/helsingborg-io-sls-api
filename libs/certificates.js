012e2e983cfc6aa0ca5bfcd689a00b414d3bef02
Dan Nilsson <mail@dannilsson.se>
2022-08-29 15:09:10 +0200

    refactor: 💡 (libs) remove try catch block

--- libs/certificates.js
+++ libs/certificates.js
@@ -2,15 +2,11 @@ import S3 from 'aws-sdk/clients/s3';
 
 const s3Client = new S3({ apiVersion: '2006-03-01' });
 
-export function read(Bucket, Key) {
-  try {
-    return s3Client
-      .getObject({
-        Bucket,
-        Key,
-      })
-      .promise();
-  } catch (error) {
-    throw `S3 - ${error.code}:${error.message}`;
-  }
+export function read(bucket, key) {
+  return s3Client
+    .getObject({
+      Bucket: bucket,
+      Key: key,
+    })
+    .promise();
 }
