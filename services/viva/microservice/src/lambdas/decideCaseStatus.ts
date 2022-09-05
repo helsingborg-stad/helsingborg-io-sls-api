314b488cd2adccb067c0000526adadfd833119e6
Dan Nilsson <mail@dannilsson.se>
2022-09-05 22:47:03 +0200

    refactor: 💡 (viva-ms) to typescript

---
+++ services/viva/microservice/src/lambdas/decideCaseStatus.ts
@@ -0,0 +1,75 @@
+import config from '../libs/config';
+
+import * as dynamoDb from '../libs/dynamoDb';
+import log from '../libs/logs';
+import { VIVA_APPLICATION_RECEIVED } from '../libs/constants';
+import { getStatusByType } from '../libs/caseStatuses';
+
+import putVivaMsEvent from '../helpers/putVivaMsEvent';
+import decideNewCaseStatus from '../helpers/caseDecision';
+
+import type { CaseStatus } from '../types/caseItem';
+import type { VivaWorkflow } from '../types/vivaWorkflow';
+
+interface CaseKeys {
+  PK: string;
+  SK: string;
+}
+
+interface LambdaDetails {
+  caseKeys: CaseKeys;
+  workflow: VivaWorkflow;
+}
+
+export interface LambdaRequest {
+  detail: LambdaDetails;
+}
+
+interface PutSuccessEvent {
+  caseKeys: CaseKeys;
+}
+
+export interface Dependencies {
+  putSuccessEvent: (params: PutSuccessEvent) => Promise<void>;
+  updateCase: (keys: CaseKeys, newStatus: CaseStatus) => Promise<void>;
+}
+
+function updateCaseStatusAndState(keys: CaseKeys, newStatus: CaseStatus): Promise<void> {
+  const updateParams = {
+    TableName: config.cases.tableName,
+    Key: {
+      PK: keys.PK,
+      SK: keys.SK,
+    },
+    UpdateExpression: 'SET #status = :newStatusType, #state = :newState',
+    ExpressionAttributeNames: { '#status': 'status', '#state': 'state' },
+    ExpressionAttributeValues: {
+      ':newStatusType': newStatus,
+      ':newState': VIVA_APPLICATION_RECEIVED,
+    },
+    ReturnValues: 'NONE',
+  };
+
+  return dynamoDb.call('update', updateParams);
+}
+
+async function decideCaseStatus(input: LambdaRequest, dependencies: Dependencies) {
+  const { caseKeys, workflow } = input.detail;
+
+  const newStatusType = decideNewCaseStatus(workflow);
+  if (newStatusType == undefined) {
+    return true;
+  }
+
+  await dependencies.updateCase(caseKeys, getStatusByType(newStatusType));
+  await dependencies.putSuccessEvent({ caseKeys });
+
+  return true;
+}
+
+export const main = log.wrap(event => {
+  return decideCaseStatus(event, {
+    putSuccessEvent: putVivaMsEvent.decideCaseStatusSuccess,
+    updateCase: updateCaseStatusAndState,
+  });
+});
