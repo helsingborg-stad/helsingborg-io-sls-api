eaf730408e4e65ac119116672eef3316a0102c91
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-09-08 13:25:41 +0200

    fix(personapplication.tsx): rewrite to agreed format

--- services/viva/microservice/src/lambdas/personApplication.ts
+++ services/viva/microservice/src/lambdas/personApplication.ts
@@ -1,31 +1,48 @@
-/* eslint-disable no-console */
-import to from 'await-to-js';
+import log from '../libs/logs';
 
 import putVivaMsEvent from '../helpers/putVivaMsEvent';
-
 import vivaAdapter from '../helpers/vivaAdapterRequestClient';
 
-export async function main(event) {
-  const clientUser = event.detail.user;
+import type { CaseUser } from '../types/caseItem';
+import type { VivaMyPages } from '../types/vivaMyPages';
 
-  const [getVivaPersonError, vivaPersonDetail] = await to(
-    vivaAdapter.person.get(clientUser.personalNumber)
-  );
-  if (getVivaPersonError) {
-    console.error('(Viva-ms: personApplication) getVivaPersonError', getVivaPersonError);
-    return false;
-  }
+export interface LambdaDetail {
+  user: CaseUser;
+}
 
-  const [putEventError] = await to(
-    putVivaMsEvent.personDetailSuccess({
-      clientUser,
-      vivaPersonDetail,
-    })
-  );
-  if (putEventError) {
-    console.error('(Viva-ms: personApplication) putEventError.', putEventError);
-    return false;
-  }
+export interface LambdaRequest {
+  detail: LambdaDetail;
+}
+
+export interface PutEventParameters {
+  clientUser: CaseUser;
+  vivaPersonDetail: VivaMyPages;
+}
+
+export interface Dependencies {
+  getVivaPerson: (personalNumber: string) => Promise<VivaMyPages>;
+  putSuccessEvent: (parameters: PutEventParameters) => Promise<void>;
+}
+
+export async function personApplication(
+  input: LambdaRequest,
+  dependencies: Dependencies
+): Promise<boolean> {
+  const clientUser = input.detail.user;
+
+  const vivaPersonDetail = await dependencies.getVivaPerson(clientUser.personalNumber);
+
+  await dependencies.putSuccessEvent({
+    clientUser,
+    vivaPersonDetail,
+  });
 
   return true;
 }
+
+export const main = log.wrap(event => {
+  return personApplication(event, {
+    getVivaPerson: vivaAdapter.person.get,
+    putSuccessEvent: putVivaMsEvent.personDetailSuccess,
+  });
+});
