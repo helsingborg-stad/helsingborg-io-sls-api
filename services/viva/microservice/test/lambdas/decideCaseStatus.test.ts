0fe104a57c7536c77d2af51785fb0861424a68b6
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-09-09 13:31:03 +0200

    fix(decidecasestatus.test.ts): update tests for lambda

--- services/viva/microservice/test/lambdas/decideCaseStatus.test.ts
+++ services/viva/microservice/test/lambdas/decideCaseStatus.test.ts
@@ -1,7 +1,14 @@
 import { decideCaseStatus } from '../../src/lambdas/decideCaseStatus';
+
+import { getStatusByType } from '../../src/libs/caseStatuses';
+import { ACTIVE_PROCESSING } from '../../src/libs/constants';
+
 import type { LambdaRequest } from '../../src/lambdas/decideCaseStatus';
-import type { CaseStatus } from '../../src/types/caseItem';
-import type { VivaWorkflow, VivaWorkflowApplication } from '../../src/types/vivaWorkflow';
+import type {
+  VivaWorkflow,
+  VivaWorkflowApplication,
+  VivaWorkflowDecisionRoot,
+} from '../../src/types/vivaWorkflow';
 
 const caseKeys = {
   PK: 'USER#198602102389',
@@ -18,45 +25,58 @@ function createLambdaInput(
       workflow: {
         workflowid: 'XYZ123',
         application: {
-          receiveddate: null,
-          periodstartdate: null,
-          periodenddate: null,
-          otherperiod: null,
-          requestingcompletion: null,
-          completiondate: null,
-          completionreceiveddate: null,
-          completionsreceived: null,
-          completionsuploaded: null,
-          completions: null,
-          completiondescription: null,
-          completionduedate: null,
-          islockedwithoutcompletionreceived: null,
           islocked: null,
           ...workflowApplicationParams,
         },
+        decision: {
+          decisions: {
+            decision: {
+              typecode: 1,
+            },
+          },
+        },
         ...workflowParams,
-      },
+      } as VivaWorkflow,
     },
   } as unknown as LambdaRequest;
 }
 
 describe('decideCaseStatus', () => {
-  it('successfully updates case with new status', async () => {
-    const newSatatus: CaseStatus = {
-      type: 'closed:approved:viva',
-      name: 'Godkänd',
-      description: 'Din ansökan är godkänd. Pengarna sätts in på ditt konto',
-    };
-
+  it('successfully updates a case with new status', async () => {
     const updateCaseMock = jest.fn();
-    const lambdaInput = createLambdaInput();
+    const putSuccessEventMock = jest.fn();
 
-    const result = await decideCaseStatus(lambdaInput, {
+    const result = await decideCaseStatus(createLambdaInput(), {
       updateCase: updateCaseMock,
-      putSuccessEvent: () => Promise.resolve(),
+      putSuccessEvent: putSuccessEventMock,
     });
 
     expect(result).toBe(true);
-    expect(updateCaseMock).toHaveBeenCalledWith(caseKeys, newSatatus);
+    expect(updateCaseMock).toHaveBeenCalledWith(caseKeys, getStatusByType(ACTIVE_PROCESSING));
+    expect(putSuccessEventMock).toHaveBeenCalledWith({ caseKeys: caseKeys });
+  });
+
+  it('fails updating case status for undefined new status type', async () => {
+    const updateCaseMock = jest.fn();
+    const putSuccessEventMock = jest.fn();
+
+    const lambdaInput = {
+      decision: {
+        decisions: {
+          decision: {
+            typecode: '0',
+          },
+        },
+      } as VivaWorkflowDecisionRoot,
+    };
+
+    const result = await decideCaseStatus(createLambdaInput(lambdaInput), {
+      updateCase: updateCaseMock,
+      putSuccessEvent: putSuccessEventMock,
+    });
+
+    expect(result).toBe(true);
+    expect(updateCaseMock).toHaveBeenCalledTimes(0);
+    expect(putSuccessEventMock).toHaveBeenCalledTimes(0);
   });
 });
