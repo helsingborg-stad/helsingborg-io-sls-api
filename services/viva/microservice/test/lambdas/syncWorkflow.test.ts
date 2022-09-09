20ab513d74487a61c6d397ae7f2bf6eac4bd63de
Sven65 <Sven65@users.noreply.github.com>
2022-09-09 11:05:21 +0200

    feat(workflow): add tests for syncWorkflow

---
+++ services/viva/microservice/test/lambdas/syncWorkflow.test.ts
@@ -0,0 +1,144 @@
+import { LambdaRequest, syncWorkflow } from '../../src/lambdas/syncWorkflow';
+import { EncryptionType } from '../../../types/caseItem';
+import { CaseItem } from '../../src/types/caseItem';
+import { VivaWorkflow } from '../../src/types/vivaWorkflow';
+
+const PK = 'USER#199001011234';
+const SK = 'CASE#11111111-2222-3333-4444-555555555555';
+const WORKFLOW_ID = 'flowId123ABC';
+
+const caseKeys = {
+  PK,
+  SK,
+};
+
+const input: LambdaRequest = {
+  detail: {
+    user: {
+      personalNumber: '199801011234',
+    },
+  },
+};
+
+function makeWorkflow(): VivaWorkflow {
+  return {
+    workflowid: WORKFLOW_ID,
+    application: {
+      completiondate: null,
+      completiondescription: '',
+      completionduedate: null,
+      completionreceiveddate: null,
+      completions: null,
+      completionsreceived: null,
+      completionsuploaded: null,
+      islocked: null,
+      islockedwithoutcompletionreceived: null,
+      otherperiod: null,
+      periodenddate: null,
+      periodstartdate: null,
+      receiveddate: null,
+      requestingcompletion: null,
+    },
+  };
+}
+
+function makeCase(workflowId: string = WORKFLOW_ID): CaseItem {
+  return {
+    PK: caseKeys.PK,
+    SK: caseKeys.SK,
+    currentFormId: '123ABC',
+    id: 'caseId',
+    persons: [],
+    state: 'SOME_STATE',
+    expirationTime: 123,
+    createdAt: 123,
+    updatedAt: 456,
+    status: {
+      type: 'active:submitted',
+      name: '',
+      description: '',
+    },
+    forms: {
+      abc123: {
+        answers: [],
+        encryption: {
+          type: EncryptionType.Decrypted,
+        },
+        currentPosition: {
+          currentMainStep: 0,
+          currentMainStepIndex: 0,
+          index: 0,
+          level: 0,
+          numberOfMainSteps: 0,
+        },
+      },
+    },
+    provider: 'VIVA',
+    details: {
+      workflowId: workflowId,
+      period: {
+        startDate: 1,
+        endDate: 2,
+      },
+      completions: {
+        requested: [],
+        description: null,
+        receivedDate: null,
+        dueDate: null,
+        attachmentUploaded: [],
+        isCompleted: false,
+        isRandomCheck: false,
+        isAttachmentPending: false,
+        isDueDateExpired: false,
+      },
+    },
+  };
+}
+
+it('Syncs cases', async () => {
+  const syncWorkflowSuccess = jest.fn().mockResolvedValue(undefined);
+
+  const workflow = makeWorkflow();
+
+  const result = await syncWorkflow(input, {
+    getSubmittedOrProcessingOrOngoingCases: () => Promise.resolve({ Items: [makeCase()] }),
+    syncWorkflowSuccess: syncWorkflowSuccess,
+    updateCaseDetailsWorkflow: () => Promise.resolve(),
+    getWorkflow: () => Promise.resolve(workflow),
+  });
+
+  expect(result).toBe(true);
+  expect(syncWorkflowSuccess).toHaveBeenCalledWith({ caseKeys, workflow });
+});
+
+it("Doesn't sync cases with non-matching status", async () => {
+  const syncWorkflowSuccess = jest.fn().mockResolvedValue(undefined);
+
+  const workflow = makeWorkflow();
+
+  const result = await syncWorkflow(input, {
+    getSubmittedOrProcessingOrOngoingCases: () => Promise.resolve({ Items: [] }),
+    syncWorkflowSuccess: syncWorkflowSuccess,
+    updateCaseDetailsWorkflow: () => Promise.resolve(),
+    getWorkflow: () => Promise.resolve(workflow),
+  });
+
+  expect(result).toBe(true);
+  expect(syncWorkflowSuccess).not.toHaveBeenCalled();
+});
+
+it("Doesn't sync cases without workflow id", async () => {
+  const syncWorkflowSuccess = jest.fn().mockResolvedValue(undefined);
+
+  const workflow = makeWorkflow();
+
+  const result = await syncWorkflow(input, {
+    getSubmittedOrProcessingOrOngoingCases: () => Promise.resolve({ Items: [makeCase('')] }),
+    syncWorkflowSuccess: syncWorkflowSuccess,
+    updateCaseDetailsWorkflow: () => Promise.resolve(),
+    getWorkflow: () => Promise.resolve(workflow),
+  });
+
+  expect(result).toBe(true);
+  expect(syncWorkflowSuccess).not.toHaveBeenCalled();
+});
