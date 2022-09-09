f89e890a621f98f6d3409b84c536430d8f52137c
MasterKenth <MasterKenth@users.noreply.github.com>
2022-09-09 08:58:04 +0200

    refactor(applicationstatus): rewrite to ts using new structure (#434)

---
+++ services/viva/microservice/src/lambdas/applicationStatus.ts
@@ -0,0 +1,43 @@
+import log from '../libs/logs';
+
+import putVivaMsEvent from '../helpers/putVivaMsEvent';
+import vivaAdapter from '../helpers/vivaAdapterRequestClient';
+import { VivaApplicationStatus } from '../types/vivaMyPages';
+
+interface User {
+  personalNumber: string;
+}
+
+export interface LambdaRequest {
+  detail: User;
+}
+
+interface SuccessEvent {
+  user: User;
+  status: VivaApplicationStatus[];
+}
+
+interface Dependencies {
+  getStatus: (personalNumber: string) => Promise<VivaApplicationStatus[]>;
+  putSuccessEvent: (event: SuccessEvent) => Promise<void>;
+}
+
+export async function checkApplicationStatus(
+  input: LambdaRequest,
+  dependencies: Dependencies
+): Promise<boolean> {
+  const user = input.detail;
+
+  const status = await dependencies.getStatus(user.personalNumber);
+
+  await dependencies.putSuccessEvent({ user, status });
+
+  return true;
+}
+
+export const main = log.wrap(event =>
+  checkApplicationStatus(event, {
+    getStatus: vivaAdapter.application.status,
+    putSuccessEvent: putVivaMsEvent.applicationStatusSuccess,
+  })
+);
