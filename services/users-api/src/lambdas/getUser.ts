a9c749f23ae187dd97893ce6387da7cae2de0e74
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-09-15 14:45:07 +0200

    refactor(getuser): rewrite with typescript and added tests

--- services/users-api/src/lambdas/getUser.ts
+++ services/users-api/src/lambdas/getUser.ts
@@ -1,8 +1,4 @@
-import to from 'await-to-js';
-import {
-  InternalServerError,
-  ResourceNotFoundError,
-} from '@helsingborg-stad/npm-api-error-handling';
+import { ResourceNotFoundError } from '@helsingborg-stad/npm-api-error-handling';
 
 import config from '../libs/config';
 import * as response from '../libs/response';
@@ -10,26 +6,45 @@ import * as dynamoDb from '../libs/dynamoDb';
 import { decodeToken } from '../libs/token';
 import log from '../libs/logs';
 
-export async function main(event, context) {
-  const decodedToken = decodeToken(event);
+import type { CaseUser } from '../helpers/types';
 
-  const [getUserError, getUserResponse] = await to(getUser(decodedToken.personalNumber));
-  if (getUserError) {
-    log.error(
-      'Get user request error',
-      context.awsRequestId,
-      'service-users-api-getUser-001',
-      getUserError
-    );
+interface GetUserResponse {
+  Item: CaseUser;
+}
 
-    return response.failure(new InternalServerError(getUserError));
-  }
+interface HttpHeaders {
+  Authorization: string;
+}
 
+export interface LambdaRequest {
+  headers: HttpHeaders;
+}
+
+export interface Dependencies {
+  decodeToken: (httpEvent: LambdaRequest) => { personalNumber: string };
+  getUser: (personalNumber: string) => Promise<GetUserResponse>;
+}
+
+function fetchUser(personalNumber: string): Promise<GetUserResponse> {
+  const getParams = {
+    TableName: config.users.tableName,
+    Key: {
+      personalNumber,
+    },
+  };
+
+  return dynamoDb.call('get', getParams);
+}
+
+export async function getUser(input: LambdaRequest, dependencies: Dependencies, requestId: string) {
+  const decodedToken = dependencies.decodeToken(input);
+
+  const getUserResponse = await dependencies.getUser(decodedToken.personalNumber);
   const user = getUserResponse.Item;
 
   if (!user) {
     const errorMessage = 'No user with provided personal number found in the users table';
-    log.warn(errorMessage, context.awsRequestId, 'services-users-api-getUser-001', errorMessage);
+    log.warn(errorMessage, requestId, 'services-users-api-getUser-001', errorMessage);
 
     return response.failure(new ResourceNotFoundError(errorMessage));
   }
@@ -40,15 +55,15 @@ export async function main(event, context) {
       item: user,
     },
   });
-};
-
-function getUser(personalNumber) {
-  const getParams = {
-    TableName: config.users.tableName,
-    Key: {
-      personalNumber
-    },
-  };
-
-  return dynamoDb.call('get', getParams);
 }
+
+export const main = log.wrap((event, context) => {
+  return getUser(
+    event,
+    {
+      decodeToken,
+      getUser: fetchUser,
+    },
+    context.awsRequestId
+  );
+});
