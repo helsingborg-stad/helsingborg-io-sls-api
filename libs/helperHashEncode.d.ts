2b7d7312b661e39287f06df2e109703e2ed47e7d
MrSpex <mail@dannilsson.se>
2022-10-03 10:32:19 +0200

    refactor: (libs) hashid

--- libs/helperHashEncode.d.ts
+++ libs/helperHashEncode.d.ts
@@ -2,12 +2,5 @@ export default hash;
 declare namespace hash {
   export { encode };
 }
-/**
- * Helper function to hash encode personal number
- *
- * @param {unknown} number Personal number to be hashed
- * @param {string} hashSalt Hash security salt
- * @param {number} hashSaltLength Hash security salt length
- * @returns {string} Hashed personal number
- */
-declare function encode(number: unknown, hashSalt: string, hashSaltLength: number): string;
+
+declare function encode(value: unknown, hashSalt: string, hashSaltLength: number): string;
--- libs/helperHashEncode.js
+++ libs/helperHashEncode.js
@@ -1,18 +1,13 @@
 import Hashids from 'hashids';
 
-/**
- * Helper function to hash encode personal number
- *
- * @param {number} number Personal number to be hashed
- * @param {string} hashSalt Hash security salt
- * @param {number} hashSaltLength Hash security salt length
- * @returns {string} Hashed personal number
- */
-function encode(number, hashSalt, hashSaltLength) {
-  typeof number === 'number' || (number = parseInt(number, 10));
+function encode(value, hashSalt, hashSaltLength) {
+  let valueToEncode = value;
+  if (typeof valueToEncode !== 'number') {
+    valueToEncode = parseInt(valueToEncode, 10);
+  }
+
   const hashids = new Hashids(hashSalt, hashSaltLength);
-  const encodedNumber = hashids.encode(number);
-  return encodedNumber;
+  return hashids.encode(valueToEncode);
 }
 
 const hash = {
