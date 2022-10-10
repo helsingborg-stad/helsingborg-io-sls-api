0389397dcddd5f9d411a6e07921a4fb229c6288c
Dan Nilsson <mail@dannilsson.se>
2022-10-10 20:48:46 +0200

    test: (viva-ms) add more tests

--- services/viva/microservice/test/lambdas/generateRecurringCaseHtml.test.ts
+++ services/viva/microservice/test/lambdas/generateRecurringCaseHtml.test.ts
@@ -22,35 +22,17 @@ const input: LambdaRequest = {
   },
 };
 
-function createGetCaseReponse(): [unknown, { Item: CaseItem }] {
-  return [
-    undefined,
-    {
-      Item: {
-        PK: caseKeys.PK,
-        SK: caseKeys.SK,
-        currentFormId: 'recurringFormId',
-        forms: {
-          recurringFormId: {
-            answers: [
-              {
-                field: {
-                  id: 'myFieldId',
-                  tags: ['myTag'],
-                },
-                value: 'myValue',
-              },
-            ],
-          },
-        },
-      } as unknown as CaseItem,
-    },
-  ];
+function createGetCaseReponse(partialCaseItem?: Partial<CaseItem>): CaseItem {
+  return {
+    PK: caseKeys.PK,
+    SK: caseKeys.SK,
+    ...partialCaseItem,
+  } as CaseItem;
 }
 
 function createDependencies(partialDependencies?: Partial<Dependencies>): Dependencies {
   return {
-    getCase: jest.fn().mockResolvedValue(createGetCaseReponse()),
+    getCase: jest.fn().mockResolvedValue(undefined),
     readParams: () =>
       Promise.resolve({
         recurringFormId: 'recurringFormId',
@@ -70,8 +52,19 @@ function createDependencies(partialDependencies?: Partial<Dependencies>): Depend
   };
 }
 
-it(`generates html using handlebar template: ${S3_HANDLEBAR_TEMPLATE_V2_DEPRECATED}`, async () => {
-  const dependencies = createDependencies();
+it(`generates html using handlebar template: ${S3_HANDLEBAR_TEMPLATE_V2_DEPRECATED} - if recurring application`, async () => {
+  const dependencies = createDependencies({
+    getCase: jest.fn().mockResolvedValue(
+      createGetCaseReponse({
+        currentFormId: 'recurringFormId',
+        forms: {
+          recurringFormId: {
+            answers: [],
+          },
+        },
+      } as unknown as CaseItem)
+    ),
+  });
 
   const result = await generateRecurringCaseHtml(input, dependencies);
 
@@ -79,8 +72,38 @@ it(`generates html using handlebar template: ${S3_HANDLEBAR_TEMPLATE_V2_DEPRECAT
   expect(dependencies.getFile).toHaveBeenCalledWith('', S3_HANDLEBAR_TEMPLATE_V2_DEPRECATED);
 });
 
+it(`generates html using handlebar template: ${S3_HANDLEBAR_TEMPLATE_V3} - if new application`, async () => {
+  const dependencies = createDependencies({
+    getCase: jest.fn().mockResolvedValue(
+      createGetCaseReponse({
+        currentFormId: 'newApplicationFormId',
+        forms: {
+          newApplicationFormId: {
+            answers: [],
+          },
+        },
+      } as unknown as CaseItem)
+    ),
+  });
+
+  const result = await generateRecurringCaseHtml(input, dependencies);
+
+  expect(result).toBe(true);
+  expect(dependencies.getFile).toHaveBeenCalledWith('', S3_HANDLEBAR_TEMPLATE_V3);
+});
+
 it(`generates html using handlebar template: ${S3_HANDLEBAR_TEMPLATE_V2_DEPRECATED} - if closed case was found and it is of type new application`, async () => {
   const dependencies = createDependencies({
+    getCase: jest.fn().mockResolvedValue(
+      createGetCaseReponse({
+        currentFormId: 'recurringFormId',
+        forms: {
+          recurringFormId: {
+            answers: [],
+          },
+        },
+      } as unknown as CaseItem)
+    ),
     getClosedCases: jest.fn().mockResolvedValue({
       Items: [
         {
@@ -99,4 +122,64 @@ it(`generates html using handlebar template: ${S3_HANDLEBAR_TEMPLATE_V2_DEPRECAT
 
   expect(result).toBe(true);
   expect(dependencies.getFile).toHaveBeenCalledWith('', S3_HANDLEBAR_TEMPLATE_V2_DEPRECATED);
+});
+
+it(`generates html using handlebar template: ${S3_HANDLEBAR_TEMPLATE_V2_DEPRECATED} - if closed case was found and it is of type recurring application`, async () => {
+  const dependencies = createDependencies({
+    getCase: jest.fn().mockResolvedValue(
+      createGetCaseReponse({
+        currentFormId: 'recurringFormId',
+        forms: {
+          recurringFormId: {
+            answers: [],
+          },
+        },
+      } as unknown as CaseItem)
+    ),
+    getClosedCases: jest.fn().mockResolvedValue({
+      Items: [
+        {
+          currentFormId: 'recurringFormId',
+          forms: {
+            recurringFormId: {
+              answers: [],
+            },
+          },
+        },
+      ],
+    }),
+  });
+
+  const result = await generateRecurringCaseHtml(input, dependencies);
+
+  expect(result).toBe(true);
+  expect(dependencies.getFile).toHaveBeenCalledWith('', S3_HANDLEBAR_TEMPLATE_V2_DEPRECATED);
+});
+
+it('returns false if template files was not found', async () => {
+  const dependencies = createDependencies({
+    getCase: jest.fn().mockResolvedValue(
+      createGetCaseReponse({
+        currentFormId: 'recurringFormId',
+        forms: {
+          recurringFormId: {
+            answers: [],
+          },
+        },
+      } as unknown as CaseItem)
+    ),
+    getFile: jest.fn().mockReturnValue(undefined),
+  });
+
+  const result = await generateRecurringCaseHtml(input, dependencies);
+
+  expect(result).toBe(false);
+});
+
+it('returns false if getCase was not found', async () => {
+  const dependencies = createDependencies();
+
+  const result = await generateRecurringCaseHtml(input, dependencies);
+
+  expect(result).toBe(false);
 });
