a66d132b8243e79068298e78021b561599b9fd15
Karl Mösenbacher <karlmosenbacher@MacBook-Pro-som-tillhor-Karl.local>
2022-10-11 16:51:51 +0200

    refactor(collect.test.ts): fix tests

--- services/bankid-api/test/lambdas/collect.test.ts
+++ services/bankid-api/test/lambdas/collect.test.ts
@@ -1,8 +1,6 @@
 import { collect } from '../../src/lambdas/collect';
 
-import * as response from '../../../../libs/response';
-
-import type { LambdaRequest, Dependencies } from '../../src/lambdas/collect';
+import type { FunctionInput, Dependencies } from '../../src/lambdas/collect';
 
 const mockUser = {
   personalNumber: '199009111111',
@@ -21,35 +19,11 @@ function createDependencies(partialDependencies: Partial<Dependencies> = {}): De
   };
 }
 
-function createInput(): LambdaRequest {
+function createInput(): FunctionInput {
   return {
     body: mockBody,
     headers: mockHeaders,
   };
-}
-
-function createSuccessResponse(
-  partialBankIdResponse: Partial<{
-    authorizationCode: string;
-    status: string;
-  }> = {}
-) {
-  return response.success(200, {
-    type: 'bankIdCollect',
-    attributes: {
-      ...(partialBankIdResponse.authorizationCode && { authorizationCode: mockAuthorizationCode }),
-      status: partialBankIdResponse.status ?? 'pending',
-      completionData: {
-        user: mockUser,
-      },
-    },
-  });
-}
-
-function createFailureResponse(error: { status: number; message: string }) {
-  return response.failure(error, {
-    type: 'bankIdCollect',
-  });
 }
 
 it('returns collect data successfully for bankId status pending', async () => {
@@ -64,7 +38,12 @@ it('returns collect data successfully for bankId status pending', async () => {
     })
   );
 
-  expect(result).toEqual(createSuccessResponse());
+  expect(result).toEqual({
+    completionData: {
+      user: mockUser,
+    },
+    status: 'pending',
+  });
   expect(sendBankIdStatusCompleteEventMock).not.toHaveBeenCalled();
 });
 
@@ -78,45 +57,12 @@ it('returns collect data successfully for bankId status complete', async () => {
     })
   );
 
-  expect(result).toEqual(
-    createSuccessResponse({
-      authorizationCode: mockAuthorizationCode,
-      status: 'complete',
-    })
-  );
+  expect(result).toEqual({
+    authorizationCode: mockAuthorizationCode,
+    completionData: {
+      user: mockUser,
+    },
+    status: 'complete',
+  });
   expect(sendBankIdStatusCompleteEventMock).toHaveBeenCalled();
-});
-
-it('returns failure for failing sendBankIdCollectRequest', async () => {
-  const mockErrorObject = {
-    status: 404,
-    message: 'not found',
-  };
-  const sendBankIdCollectRequestMock = jest.fn().mockRejectedValueOnce(mockErrorObject);
-
-  const result = await collect(
-    createInput(),
-    createDependencies({
-      sendBankIdCollectRequest: sendBankIdCollectRequestMock,
-    })
-  );
-
-  expect(result).toEqual(createFailureResponse(mockErrorObject));
-});
-
-it('returns failure for failing generateAuthorizationCode request', async () => {
-  const mockErrorObject = {
-    status: 500,
-    message: 'internal server error',
-  };
-  const generateAuthorizationCodeMock = jest.fn().mockRejectedValueOnce(mockErrorObject);
-
-  const result = await collect(
-    createInput(),
-    createDependencies({
-      generateAuthorizationCode: generateAuthorizationCodeMock,
-    })
-  );
-
-  expect(result).toEqual(createFailureResponse(mockErrorObject));
 });
