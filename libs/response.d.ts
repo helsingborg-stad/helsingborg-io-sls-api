43278e7ceaf0f649cf6de2ff3140b382452947a5
MasterKenth <MasterKenth@users.noreply.github.com>
2022-11-01 12:16:19 +0100

    feat(response): enforce statusCode as number, add Content-Type, add raw response function

--- libs/response.d.ts
+++ libs/response.d.ts
@@ -1,37 +1,79 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+export function buildResponse(
+  statusCode: number,
+  contentType: string,
+  body: string
+): {
+  statusCode: number;
+  headers: {
+    'Access-Control-Allow-Origin': string;
+    'Access-Control-Allow-Credentials': boolean;
+    'Content-Type': string;
+  };
+  body: string;
+  isBase64Encoded: boolean;
+};
+export function buildResponseRaw(
+  statusCode: number,
+  rawbody: string
+): {
+  statusCode: number;
+  headers: {
+    'Access-Control-Allow-Origin': string;
+    'Access-Control-Allow-Credentials': boolean;
+    'Content-Type': 'text/plain';
+  };
+  body: string;
+  isBase64Encoded: boolean;
+};
+export function buildResponseJSON(
+  statusCode: number,
+  body: unknown
+): {
+  statusCode: number;
+  headers: {
+    'Access-Control-Allow-Origin': string;
+    'Access-Control-Allow-Credentials': boolean;
+    'Content-Type': 'application/json';
+  };
+  body: string;
+  isBase64Encoded: boolean;
+};
 export function success(
   statusCode: number,
-  body: any
+  body: unknown
 ): {
-  statusCode: any;
+  statusCode: number;
   headers: {
     'Access-Control-Allow-Origin': string;
     'Access-Control-Allow-Credentials': boolean;
+    'Content-Type': 'application/json';
   };
   body: string;
-  isBase64Encoded: boolen;
+  isBase64Encoded: boolean;
+};
+export function successRaw(
+  statusCode: number,
+  rawbody: string
+): {
+  statusCode: number;
+  headers: {
+    'Access-Control-Allow-Origin': string;
+    'Access-Control-Allow-Credentials': boolean;
+    'Content-Type': 'text/plain';
+  };
+  body: string;
+  isBase64Encoded: boolean;
 };
 export function failure(
-  error: any,
-  stackTrace?: any
+  error: unknown,
+  stackTrace?: unknown
 ): {
-  statusCode: any;
+  statusCode: number;
   headers: {
     'Access-Control-Allow-Origin': string;
     'Access-Control-Allow-Credentials': boolean;
+    'Content-Type': 'application/json';
   };
   body: string;
-  isBase64Encoded: boolen;
-};
-export function buildResponse(
-  statusCode: any,
-  body: any
-): {
-  statusCode: any;
-  headers: {
-    'Access-Control-Allow-Origin': string;
-    'Access-Control-Allow-Credentials': boolean;
-  };
-  body: string;
-  isBase64Encoded: boolen;
+  isBase64Encoded: boolean;
 };
