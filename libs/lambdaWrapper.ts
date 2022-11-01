e6d557b64cb489982e6e42f611a49084494f05df
MasterKenth <MasterKenth@users.noreply.github.com>
2022-11-01 16:21:34 +0100

    refactor(lambdawrapper): separate into type and helper files

--- libs/lambdaWrapper.ts
+++ libs/lambdaWrapper.ts
@@ -1,9 +1,15 @@
 import type { Context, APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-import type { LambdaError } from './errorTransformerFactory';
+import { successHandlers, errorHandlers, inputTransformers } from './lambdaWrappers.helpers';
 import transformError from './errorTransformerFactory';
 import * as response from './response';
 import log from './logs';
+import type {
+  LambdaInvocationConfig,
+  LambdaWrapper,
+  LambdaFunction,
+  LambdaMainExport,
+} from './lambdaWrapper.types';
 
 /** @deprecated */
 function httpErrorWrapper<LambdaEvent, LambdaResponse>(
@@ -52,69 +58,6 @@ function lambdaWrapper<LambdaEvent, LambdaResponse, Dependencies>(
 }
 
 export default lambdaWrapper;
-
-type LambdaMainExport<TEventType, TReturnType> = (
-  event: TEventType,
-  context: Context
-) => Promise<TReturnType>;
-
-type LambdaFunction<TLambdaInput, TDependencies, TLambdaOutput> = (
-  input: TLambdaInput,
-  dependencies: TDependencies
-) => Promise<TLambdaOutput>;
-
-interface LambdaWrapper<TEventType, TReturnType, TLambdaOutput> {
-  wrap<TLambdaInput, TDependencies>(
-    lambda: LambdaFunction<TLambdaInput, TDependencies, TLambdaOutput>,
-    dependencies: TDependencies
-  ): LambdaMainExport<TEventType, TReturnType>;
-}
-
-interface LambdaInvocationConfig<
-  TEventType,
-  TDependencies,
-  TLambdaOutput,
-  TLambdaInput = TEventType,
-  TEventResult = TLambdaOutput
-> {
-  lambda: LambdaFunction<TLambdaInput, TDependencies, TLambdaOutput>;
-  event: TEventType;
-  context: Context;
-  dependencies: TDependencies;
-  onSuccess: (result: TLambdaOutput) => TEventResult;
-  onError: (error: LambdaError) => TEventResult | Error;
-  inputTransformer?: (event: TEventType) => TLambdaInput;
-}
-
-const inputTransformers = {
-  fromApiGatewayEvent<TOutputType>(event: APIGatewayEvent): TOutputType {
-    return {
-      ...JSON.parse(event.body ?? '{}'),
-      ...event.queryStringParameters,
-    } as unknown as TOutputType;
-  },
-};
-
-const successHandlers = {
-  returnAsIs<TValue>(value: TValue): TValue {
-    return value;
-  },
-  asApiResponse<TValue>(value: TValue): APIGatewayProxyResult {
-    return response.success(200, value);
-  },
-  asRawApiResponse(value: string): APIGatewayProxyResult {
-    return response.successRaw(200, value);
-  },
-};
-
-const errorHandlers = {
-  returnAsIs<TError extends LambdaError>(error: TError): LambdaError {
-    return error;
-  },
-  asApiResponse<TError extends LambdaError>(error: TError): APIGatewayProxyResult {
-    return response.failure(error);
-  },
-};
 
 async function invokeLambda<
   TEventType,
