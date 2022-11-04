1461c43eeadd73bb314ff888487a94e8fd2f5c83
Karl Mösenbacher <karlmosenbacher@MacBook-Pro-som-tillhor-Karl.local>
2022-11-04 14:49:41 +0100

    refactor(uploadattachment): store file under unique id only

--- services/users-api/src/lambdas/uploadAttachment.ts
+++ services/users-api/src/lambdas/uploadAttachment.ts
@@ -1,100 +1,65 @@
 import uuid from 'uuid';
-import { BadRequestError } from '@helsingborg-stad/npm-api-error-handling';
 import S3 from '../libs/S3';
-import log from '../libs/logs';
-import * as response from '../libs/response';
-import { decodeToken, Token } from '../libs/token';
-import { getUniqueFileName } from '../helpers/files';
+import type { Token } from '../libs/token';
+import { decodeToken } from '../libs/token';
+import { wrappers } from '../libs/lambdaWrapper';
+
+type SupportedMimeTypes = 'image/jpeg' | 'image/png' | 'image/jpg' | 'application/pdf';
 
 export interface UploadAttachmentRequest {
-  readonly fileName?: string;
-  readonly mime?: string;
+  readonly mime: SupportedMimeTypes;
 }
 
-interface SignedUrlParameters {
-  ContentType: string;
-  ACL: string;
-  Key: string;
-  Expires: number;
+interface LambdaResponse {
+  uploadUrl: string;
+  id: string;
 }
 
 export interface Dependencies {
   decodeToken: (event: LambdaRequest) => Token;
-  getSignedUrl: (
-    bucketName?: string,
-    method?: string,
-    params?: SignedUrlParameters
-  ) => string | undefined;
-  createUniqueFileName: (file: string, separator: string, uuidGenerator: () => string) => string;
+  getUploadUrl: (key: string, mime: SupportedMimeTypes) => string;
+  getUniqueId: () => string;
 }
 
 export interface LambdaRequest {
-  readonly body?: string;
+  readonly body: string;
   readonly headers?: {
     readonly Authorization?: string;
   };
 }
 
-const allowedFileType = ['image/jpeg', 'image/png', 'image/jpg', 'application/pdf'];
+function getUploadUrl(key: string, mime: SupportedMimeTypes): string {
+  const { BUCKET_NAME = '' } = process.env;
 
-export async function uploadAttachment(input: LambdaRequest, dependencies: Dependencies) {
-  const decodedToken = dependencies.decodeToken(input);
-
-  if (!input?.body) {
-    const errorMessage = 'Body data is missing in request';
-    log.writeError(`service-users-api-uploadAttachment-000: ${errorMessage}`);
-    return response.failure(new BadRequestError(errorMessage));
-  }
-
-  const { fileName, mime } = JSON.parse(input.body) as UploadAttachmentRequest;
-
-  if (!fileName) {
-    const errorMessage = 'Could not find key "fileName" in request body';
-    log.writeError(`service-users-api-uploadAttachment-001: ${errorMessage}`);
-    return response.failure(new BadRequestError(errorMessage));
-  }
-
-  if (!mime) {
-    const errorMessage = 'Could not find key "mime" in request body';
-    log.writeError(`service-users-api-uploadAttachment-002: ${errorMessage}`);
-    return response.failure(new BadRequestError(errorMessage));
-  }
-
-  if (!allowedFileType.includes(mime)) {
-    const errorMessage = `The mimeType ${mime} is not allowed`;
-    log.writeError(`service-users-api-uploadAttachment-003: ${errorMessage}`);
-    return response.failure(new BadRequestError(errorMessage));
-  }
-
-  const uniquefileName = dependencies.createUniqueFileName(fileName, '_', uuid.v4);
-  const fileStorageKeyName = `${decodedToken.personalNumber}/${uniquefileName}`;
-  const params: SignedUrlParameters = {
+  return S3.getSignedUrl(BUCKET_NAME, 'putObject', {
     ContentType: mime,
     ACL: 'public-read',
-    Key: fileStorageKeyName,
-    Expires: 60 * 10,
-  };
-
-  const uploadUrl = dependencies.getSignedUrl(process.env.BUCKET_NAME, 'putObject', params);
-  if (!uploadUrl) {
-    const errorMessage = 'Get signed url error';
-    log.writeError(errorMessage);
-    return response.failure(new BadRequestError(errorMessage));
-  }
-
-  return response.success(200, {
-    type: 'userAttachment',
-    attributes: {
-      uploadUrl,
-      fileName: uniquefileName,
-    },
+    Key: key,
+    Expires: 60 * 5,
   });
 }
 
-export const main = log.wrap(event => {
-  return uploadAttachment(event, {
-    decodeToken,
-    getSignedUrl: S3.getSignedUrl,
-    createUniqueFileName: getUniqueFileName,
-  });
+export async function uploadAttachment(
+  input: LambdaRequest,
+  dependencies: Dependencies
+): Promise<LambdaResponse> {
+  const { personalNumber } = dependencies.decodeToken(input);
+
+  const { mime } = JSON.parse(input.body) as UploadAttachmentRequest;
+
+  const uniqueUploadId = dependencies.getUniqueId();
+  const fileStorageKeyName = `${personalNumber}/${uniqueUploadId}`;
+
+  const uploadUrl = dependencies.getUploadUrl(fileStorageKeyName, mime);
+
+  return {
+    id: uniqueUploadId,
+    uploadUrl,
+  };
+}
+
+export const main = wrappers.restJSON.wrap(uploadAttachment, {
+  decodeToken,
+  getUploadUrl,
+  getUniqueId: uuid.v4,
 });
--- services/users-api/test/uploadAttachment.test.ts
+++ services/users-api/test/uploadAttachment.test.ts
@@ -1,109 +1,51 @@
-import { uploadAttachment, Dependencies } from '../src/lambdas/uploadAttachment';
+import type { Dependencies, LambdaRequest } from '../src/lambdas/uploadAttachment';
+import { uploadAttachment } from '../src/lambdas/uploadAttachment';
 
-const dependencies: Dependencies = {
-  decodeToken: () => ({
-    personalNumber: 'my:decoded:token',
-  }),
-  getSignedUrl: () => 'my:signed:url',
-  createUniqueFileName: () => 'myfile_00000000-0000-0000-0000-000000000000.jpg',
-};
+const defaultUploadUrl = 'https://example.com';
+const defaultPersonalNumber = '199707077777';
+const defaultMime = 'image/jpeg';
+const defaultUniqueId = 'UNIQUE_ID';
 
-const mockInput = {
-  body: JSON.stringify({
-    fileName: 'myfile.jpg',
-    mime: 'image/jpeg',
-  }),
-  headers: {
-    Authorization: 'Bearer 01234567890',
-  },
-};
+function createDependencies(partialDependencies: Partial<Dependencies> = {}) {
+  return {
+    getUniqueId: () => defaultUniqueId,
+    decodeToken: jest.fn().mockReturnValue({
+      personalNumber: defaultPersonalNumber,
+    }),
+    getUploadUrl: jest.fn().mockReturnValue(defaultUploadUrl),
+    ...partialDependencies,
+  };
+}
 
-test('successful request should return expected structure', async () => {
-  const result = await uploadAttachment(mockInput, dependencies);
-
-  expect(result.statusCode).toBe(200);
-  expect(JSON.parse(result.body)).toEqual({
-    jsonapi: {
-      version: '1.0',
+function createInput(): LambdaRequest {
+  return {
+    body: JSON.stringify({
+      mime: defaultMime,
+    }),
+    headers: {
+      Authorization: 'Bearer 01234567890',
     },
-    data: {
-      type: 'userAttachment',
-      attributes: {
-        uploadUrl: 'my:signed:url',
-        fileName: 'myfile_00000000-0000-0000-0000-000000000000.jpg',
-      },
-    },
-  });
-});
+  };
+}
 
-test('event body missing should return status 400', async () => {
-  const result = await uploadAttachment({}, dependencies);
-  expect(result.statusCode).toBe(400);
-});
+it('successful request should return expected structure', async () => {
+  const getUploadUrlMock = jest.fn().mockReturnValueOnce(defaultUploadUrl);
 
-test('fileName missing should return status 400', async () => {
   const result = await uploadAttachment(
-    {
-      body: JSON.stringify({
-        mime: 'image/jpg',
-      }),
-    },
-    dependencies
-  );
-
-  expect(result.statusCode).toBe(400);
-});
-
-test('mimeType missing should return 400', async () => {
-  const result = await uploadAttachment(
-    {
-      body: JSON.stringify({
-        fileName: 'myfile.jpg',
-      }),
-    },
-    dependencies
-  );
-
-  expect(result.statusCode).toBe(400);
-});
-
-test('mimeType unknown should return 400', async () => {
-  const result = await uploadAttachment(
-    {
-      body: JSON.stringify({
-        fileName: 'myfile.jpg',
-        mime: 'my/mime-type',
-      }),
-    },
-    dependencies
-  );
-
-  expect(result.statusCode).toBe(400);
-});
-
-test('failure to retreive signed URL should return 400', async () => {
-  const result = await uploadAttachment(mockInput, {
-    ...dependencies,
-    getSignedUrl: () => undefined,
-  });
-
-  expect(result.statusCode).toBe(400);
-});
-
-test('signed URL is called with the correct parameters', async () => {
-  const getSignedUrl = jest.fn().mockResolvedValue('');
-
-  await uploadAttachment(mockInput, {
-    ...dependencies,
-    getSignedUrl,
-  });
-
-  expect(getSignedUrl).toHaveBeenCalledWith(
-    undefined,
-    expect.anything(),
-    expect.objectContaining({
-      ContentType: 'image/jpeg',
-      Key: 'my:decoded:token/myfile_00000000-0000-0000-0000-000000000000.jpg',
+    createInput(),
+    createDependencies({
+      getUploadUrl: getUploadUrlMock,
     })
+  );
+
+  expect(result).toEqual(
+    expect.objectContaining({
+      uploadUrl: defaultUploadUrl,
+      id: defaultUniqueId,
+    })
+  );
+  expect(getUploadUrlMock).toHaveBeenCalledWith(
+    expect.stringContaining(defaultPersonalNumber),
+    defaultMime
   );
 });
