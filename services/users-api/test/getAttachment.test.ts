93cf7d01a40941aeb7774547a150b47eb3798329
Karl Mösenbacher <karlmosenbacher@hotmail.com>
2022-11-04 13:15:35 +0100

    Feat/get attachment by presigned url (#467)
    * fix(s3): fix type of getsignedurl response
    
    * refactor(getattachment): return presigned url instead of buffer from lambda
    
    Co-authored-by: Karl Mösenbacher <karlmosenbacher@MacBook-Pro-som-tillhor-Karl.local>

--- services/users-api/test/getAttachment.test.ts
+++ services/users-api/test/getAttachment.test.ts
@@ -1,12 +1,9 @@
-import { ResourceNotFoundError } from '@helsingborg-stad/npm-api-error-handling';
-
 import { getAttachment } from '../src/lambdas/getAttachment';
-
-import * as response from '../src/libs/response';
 
 import type { LambdaRequest, Dependencies } from '../src/lambdas/getAttachment';
 
 const defaultPersonalNumber = '197001011234';
+const defaultFileUrl = 'https://example.com';
 const defaultFilename = 'file';
 
 function createInput(): LambdaRequest {
@@ -23,54 +20,23 @@ function createInput(): LambdaRequest {
 function createDependencies(partialDependencies: Partial<Dependencies> = {}): Dependencies {
   return {
     decodeToken: () => ({ personalNumber: defaultPersonalNumber }),
-    getFile: () => Promise.resolve({ Body: Buffer.alloc(1), ContentType: 'file' }),
+    getFileUrl: () => defaultFileUrl,
     ...partialDependencies,
   };
 }
 
-function createHttpSuccessResponse(buffer: Buffer) {
-  return {
-    statusCode: 200,
-    isBase64Encoded: true,
-    headers: {
-      'Content-Type': 'file',
-    },
-    body: buffer.toString('base64'),
-  };
-}
+it('succesfully fetches file url for user', async () => {
+  const getFileUrlMock = jest.fn().mockReturnValue(defaultFileUrl);
 
-function createHttpFilesNotFoundResponse() {
-  return response.failure(new ResourceNotFoundError('No file found for user'));
-}
+  const result = await getAttachment(
+    createInput(),
+    createDependencies({
+      getFileUrl: getFileUrlMock,
+    })
+  );
 
-it('succesfully fetches file for user', async () => {
-  const buffer = Buffer.alloc(1);
-  const getFileMock = jest.fn().mockResolvedValueOnce({
-    Body: buffer,
-    ContentType: 'file',
+  expect(getFileUrlMock).toHaveBeenCalledWith(`${defaultPersonalNumber}/${defaultFilename}`);
+  expect(result).toEqual({
+    fileUrl: defaultFileUrl,
   });
-
-  const result = await getAttachment(
-    createInput(),
-    createDependencies({
-      getFile: getFileMock,
-    })
-  );
-
-  expect(getFileMock).toHaveBeenCalledWith(`${defaultPersonalNumber}/${defaultFilename}`);
-  expect(result).toEqual(createHttpSuccessResponse(buffer));
-});
-
-it('returns failure if no file is found', async () => {
-  const getFileMock = jest.fn().mockResolvedValueOnce(null);
-
-  const result = await getAttachment(
-    createInput(),
-    createDependencies({
-      getFile: getFileMock,
-    })
-  );
-
-  expect(getFileMock).toHaveBeenCalledWith(`${defaultPersonalNumber}/${defaultFilename}`);
-  expect(result).toEqual(createHttpFilesNotFoundResponse());
 });
