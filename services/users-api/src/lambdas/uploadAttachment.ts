f896ec591ba938bcbe8d0bde9fb8e504849d2a00
Karl Mösenbacher <karlmosenbacher@hotmail.com>
2022-11-07 13:32:48 +0100

    refactor(uploadattachment): store file under unique id only (#468)
    * refactor(uploadattachment): store file under unique id only
    
    * refactor(serverless.yml): remove api gateway binary configuration
    
    * fix(uploadattachment): adapt to new lambda input format
    
    * fix(uploadattachment): fix review comments
    
    Co-authored-by: Karl Mösenbacher <karlmosenbacher@MacBook-Pro-som-tillhor-Karl.local>

--- services/users-api/src/lambdas/uploadAttachment.ts
+++ services/users-api/src/lambdas/uploadAttachment.ts
@@ -1,100 +1,51 @@
 import uuid from 'uuid';
-import { BadRequestError } from '@helsingborg-stad/npm-api-error-handling';
 import S3 from '../libs/S3';
-import log from '../libs/logs';
-import * as response from '../libs/response';
-import { decodeToken, Token } from '../libs/token';
-import { getUniqueFileName } from '../helpers/files';
+import { wrappers } from '../libs/lambdaWrapper';
 
-export interface UploadAttachmentRequest {
-  readonly fileName?: string;
-  readonly mime?: string;
-}
+type SupportedMimeTypes = 'image/jpeg' | 'image/png' | 'image/jpg' | 'application/pdf';
 
-interface SignedUrlParameters {
-  ContentType: string;
-  ACL: string;
-  Key: string;
-  Expires: number;
+interface FunctionResponse {
+  uploadUrl: string;
+  id: string;
 }
 
 export interface Dependencies {
-  decodeToken: (event: LambdaRequest) => Token;
-  getSignedUrl: (
-    bucketName?: string,
-    method?: string,
-    params?: SignedUrlParameters
-  ) => string | undefined;
-  createUniqueFileName: (file: string, separator: string, uuidGenerator: () => string) => string;
+  getUploadUrl: (key: string, mime: SupportedMimeTypes) => string;
+  getUniqueId: () => string;
 }
 
-export interface LambdaRequest {
-  readonly body?: string;
-  readonly headers?: {
-    readonly Authorization?: string;
-  };
+export interface FunctionInput {
+  mime: SupportedMimeTypes;
+  personalNumber: string;
 }
 
-const allowedFileType = ['image/jpeg', 'image/png', 'image/jpg', 'application/pdf'];
+function getUploadUrl(key: string, mime: SupportedMimeTypes): string {
+  const { BUCKET_NAME = '' } = process.env;
 
-export async function uploadAttachment(input: LambdaRequest, dependencies: Dependencies) {
-  const decodedToken = dependencies.decodeToken(input);
-
-  if (!input?.body) {
-    const errorMessage = 'Body data is missing in request';
-    log.writeError(`service-users-api-uploadAttachment-000: ${errorMessage}`);
-    return response.failure(new BadRequestError(errorMessage));
-  }
-
-  const { fileName, mime } = JSON.parse(input.body) as UploadAttachmentRequest;
-
-  if (!fileName) {
-    const errorMessage = 'Could not find key "fileName" in request body';
-    log.writeError(`service-users-api-uploadAttachment-001: ${errorMessage}`);
-    return response.failure(new BadRequestError(errorMessage));
-  }
-
-  if (!mime) {
-    const errorMessage = 'Could not find key "mime" in request body';
-    log.writeError(`service-users-api-uploadAttachment-002: ${errorMessage}`);
-    return response.failure(new BadRequestError(errorMessage));
-  }
-
-  if (!allowedFileType.includes(mime)) {
-    const errorMessage = `The mimeType ${mime} is not allowed`;
-    log.writeError(`service-users-api-uploadAttachment-003: ${errorMessage}`);
-    return response.failure(new BadRequestError(errorMessage));
-  }
-
-  const uniquefileName = dependencies.createUniqueFileName(fileName, '_', uuid.v4);
-  const fileStorageKeyName = `${decodedToken.personalNumber}/${uniquefileName}`;
-  const params: SignedUrlParameters = {
+  return S3.getSignedUrl(BUCKET_NAME, 'putObject', {
     ContentType: mime,
     ACL: 'public-read',
-    Key: fileStorageKeyName,
-    Expires: 60 * 10,
-  };
-
-  const uploadUrl = dependencies.getSignedUrl(process.env.BUCKET_NAME, 'putObject', params);
-  if (!uploadUrl) {
-    const errorMessage = 'Get signed url error';
-    log.writeError(errorMessage);
-    return response.failure(new BadRequestError(errorMessage));
-  }
-
-  return response.success(200, {
-    type: 'userAttachment',
-    attributes: {
-      uploadUrl,
-      fileName: uniquefileName,
-    },
+    Key: key,
+    Expires: 60 * 5,
   });
 }
 
-export const main = log.wrap(event => {
-  return uploadAttachment(event, {
-    decodeToken,
-    getSignedUrl: S3.getSignedUrl,
-    createUniqueFileName: getUniqueFileName,
-  });
+export async function uploadAttachment(
+  input: FunctionInput,
+  dependencies: Dependencies
+): Promise<FunctionResponse> {
+  const uniqueUploadId = dependencies.getUniqueId();
+  const fileStorageKeyName = `${input.personalNumber}/${uniqueUploadId}`;
+
+  const uploadUrl = dependencies.getUploadUrl(fileStorageKeyName, input.mime);
+
+  return {
+    id: uniqueUploadId,
+    uploadUrl,
+  };
+}
+
+export const main = wrappers.restJSON.wrap(uploadAttachment, {
+  getUploadUrl,
+  getUniqueId: uuid.v4,
 });
