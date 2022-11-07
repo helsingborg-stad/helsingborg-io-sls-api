d3c1880958290b930ad903c54155b41c99cb8be0
Karl Mösenbacher <karlmosenbacher@hotmail.com>
2022-11-07 14:01:49 +0100

    feat(copyattachment): copy attachment to co applicant (#464)
    * feat(copyattachment): copy attachment to co applicant
    
    * fix(copyattachment, copyattachment.test.ts): fix logic and test
    
    * refactor(copyattachment.ts): rename applicant to person
    
    * refactor(copyattachment): handle multiple records and personal numbers
    
    * refactor(copyattachment): redo for eventbridge events
    
    * fix(copyattachment): fix response for getlatestupdatedcase dependency
    
    * fix(copyattachment): fix review comments
    
    * fix(copyattachment): add newline
    
    * fix(copyattachment): fix types
    
    * fix(copyattachment): fix filename split
    
    Co-authored-by: Karl Mösenbacher <karlmosenbacher@MacBook-Pro-som-tillhor-Karl.local>

---
+++ services/viva/microservice/src/lambdas/copyAttachment.ts
@@ -0,0 +1,85 @@
+import { wrappers } from '../libs/lambdaWrapper';
+import * as dynamoDb from '../libs/dynamoDb';
+import config from '../libs/config';
+import S3 from '../libs/S3';
+
+import { CasePersonRole } from '../types/caseItem';
+import type { CaseItem } from '../types/caseItem';
+
+interface GetCasesResponse {
+  Items: CaseItem[];
+}
+
+interface S3EventObject {
+  key: string;
+}
+
+export interface EventDetail {
+  object: S3EventObject;
+}
+
+export interface Dependencies {
+  copyFile: (sourceKey: string, destinationKey: string) => Promise<void>;
+  getLatestUpdatedCase: (personalNumber: string) => Promise<CaseItem | undefined>;
+}
+
+export interface FunctionInput {
+  detail: EventDetail;
+}
+
+type FunctionResponse = Promise<boolean>;
+
+function copyFile(sourceKey: string, targetKey: string): Promise<void> {
+  const { BUCKET_NAME = '' } = process.env;
+
+  return S3.copyFileWithinBucket(BUCKET_NAME, sourceKey, targetKey);
+}
+
+async function getLatestUpdatedCase(personalNumber: string): Promise<CaseItem | undefined> {
+  const PK = `USER#${personalNumber}`;
+
+  const queryParams = {
+    TableName: config.cases.tableName,
+    KeyConditionExpression: 'PK = :pk',
+    ExpressionAttributeValues: {
+      ':pk': PK,
+    },
+    ProjectionExpression: 'persons, updatedAt',
+  };
+
+  const result = (await dynamoDb.call('query', queryParams)) as GetCasesResponse;
+  const [latestCase] = (result.Items ?? []).sort((a, b) => a.updatedAt - b.updatedAt);
+
+  return latestCase;
+}
+
+export async function copyAttachment(
+  input: FunctionInput,
+  dependencies: Dependencies
+): FunctionResponse {
+  const [personalNumber, ...rest] = input.detail.object.key.split('/');
+  const filename = rest.join('/');
+
+  const caseItem = await dependencies.getLatestUpdatedCase(personalNumber);
+
+  const coApplicant = caseItem?.persons.find(
+    applicant =>
+      applicant.role === CasePersonRole.CoApplicant && applicant.personalNumber !== personalNumber
+  );
+
+  if (!coApplicant) {
+    return false;
+  }
+
+  await dependencies.copyFile(
+    `${personalNumber}/${filename}`,
+    `${coApplicant.personalNumber}/${filename}`
+  );
+
+  return true;
+}
+
+export const main = wrappers.event.wrap(copyAttachment, {
+  copyFile,
+  getLatestUpdatedCase,
+});
