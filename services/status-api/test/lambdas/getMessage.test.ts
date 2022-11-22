a4fb247548f7c5d20883dbc17714200e7accf576
MrSpex <mail@dannilsson.se>
2022-11-22 18:31:48 +0100

    test: add mock timer

--- services/status-api/test/lambdas/getMessage.test.ts
+++ services/status-api/test/lambdas/getMessage.test.ts
@@ -1,4 +1,5 @@
-import { getMessages } from '../../src/lambdas/getMessages';
+import { MessageType, getMessages } from '../../src/lambdas/getMessages';
+import type { MessageItem } from '../../src/lambdas/getMessages';
 
 const mockHeaders = {
   'Access-Control-Allow-Credentials': true,
@@ -8,50 +9,135 @@ const mockHeaders = {
 
 const mockJsonApi = { version: '1.0' };
 
-// it('returns messages', async () => {
-//   const expectedMessages = [
-//     {
-//       message: {
-//         title: 'Hello',
-//         text: 'World',
-//       },
-//       type: 'info',
-//       start: '2022-11-14T00:00:00.000+01:00',
-//       expiry: '2022-11-14T23:59:59.999+01:00',
-//     },
-//   ];
+it('returns messages between current start date and expire date', async () => {
+  jest.useFakeTimers('modern').setSystemTime(new Date('2022-01-01T02:00:00.000'));
 
-//   const result = await getMessages();
-
-//   expect(result).toEqual(
-//     expect.objectContaining({
-//       statusCode: 200,
-//       headers: mockHeaders,
-//       body: JSON.stringify({
-//         jsonapi: mockJsonApi,
-//         data: {
-//           messages: expectedMessages,
-//         },
-//       }),
-//       isBase64Encoded: false,
-//     })
-//   );
-// });
-
-it('returns messages for the correct date', async () => {
-  const expectedMessages = [
+  const mockMessages: MessageItem[] = [
     {
       message: {
         title: 'Hello',
         text: 'World',
       },
-      type: 'info',
-      start: '2022-11-22T15:00:00.000+01:00',
-      expiry: '2022-11-22T16:00:00.000+01:00',
+      type: MessageType.Info,
+      start: '2022-01-01T00:00:00.000+01:00',
+      expiry: '2022-01-01T23:59:59.999+01:00',
+    },
+    {
+      message: {
+        title: 'Hello2',
+        text: 'World2',
+      },
+      type: MessageType.Maintenance,
+      start: '2022-01-20T00:00:00.000+01:00',
+      expiry: '2022-01-20T23:59:59.999+01:00',
     },
   ];
 
-  const result = await getMessages();
+  const expectedMessages: MessageItem[] = [
+    {
+      message: {
+        title: 'Hello',
+        text: 'World',
+      },
+      type: MessageType.Info,
+      start: '2022-01-01T00:00:00.000+01:00',
+      expiry: '2022-01-01T23:59:59.999+01:00',
+    },
+  ];
+
+  const result = await getMessages({
+    readParams: jest.fn().mockResolvedValueOnce(mockMessages),
+  });
+
+  expect(result).toEqual(
+    expect.objectContaining({
+      statusCode: 200,
+      headers: mockHeaders,
+      body: JSON.stringify({
+        jsonapi: mockJsonApi,
+        data: {
+          messages: expectedMessages,
+        },
+      }),
+      isBase64Encoded: false,
+    })
+  );
+});
+
+it('returns empty messages list if dates is expired', async () => {
+  jest.useFakeTimers('modern').setSystemTime(new Date('2022-02-01T00:00:00.000'));
+
+  const mockMessages: MessageItem[] = [
+    {
+      message: {
+        title: 'Hello',
+        text: 'World',
+      },
+      type: MessageType.Info,
+      start: '2022-01-01T00:00:00.000+01:00',
+      expiry: '2022-01-01T23:59:59.999+01:00',
+    },
+    {
+      message: {
+        title: 'Hello2',
+        text: 'World2',
+      },
+      type: MessageType.Maintenance,
+      start: '2022-01-20T00:00:00.000+01:00',
+      expiry: '2022-01-20T23:59:59.999+01:00',
+    },
+  ];
+
+  const expectedMessages: MessageItem[] = [];
+
+  const result = await getMessages({
+    readParams: jest.fn().mockResolvedValueOnce(mockMessages),
+  });
+
+  expect(result).toEqual(
+    expect.objectContaining({
+      statusCode: 200,
+      headers: mockHeaders,
+      body: JSON.stringify({
+        jsonapi: mockJsonApi,
+        data: {
+          messages: expectedMessages,
+        },
+      }),
+      isBase64Encoded: false,
+    })
+  );
+});
+
+it('returns empty messages list if dates is not started', async () => {
+  jest.useFakeTimers('modern').setSystemTime(new Date('2021-01-01T00:00:00.000'));
+
+  const mockMessages: MessageItem[] = [
+    {
+      message: {
+        title: 'Hello',
+        text: 'World',
+      },
+      type: MessageType.Info,
+      start: '2022-01-01T00:00:00.000+01:00',
+      expiry: '2022-01-01T23:59:59.999+01:00',
+    },
+    {
+      message: {
+        title: 'Hello2',
+        text: 'World2',
+      },
+      type: MessageType.Maintenance,
+      start: '2022-01-20T00:00:00.000+01:00',
+      expiry: '2022-01-20T23:59:59.999+01:00',
+    },
+  ];
+
+  const expectedMessages: MessageItem[] = [];
+
+  const result = await getMessages({
+    readParams: jest.fn().mockResolvedValueOnce(mockMessages),
+  });
 
   expect(result).toEqual(
     expect.objectContaining({
