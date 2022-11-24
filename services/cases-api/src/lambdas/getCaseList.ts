054c0202b6535846ce3e4d7cf4dcb2fd53df12c1
Karl Mösenbacher <karlmosenbacher@hotmail.com>
2022-11-24 09:18:34 +0100

    refactor(getcaselist): fix getCaseList return format (#479)
    * refactor(getcaselist): refactor to agreed format
    
    * fix(types): fix types for case
    
    * refactor(getcaselist.ts): fetch cases in parallel
    
    * fix(getcaselist.test.ts): clarify test description
    
    Co-authored-by: Karl Mösenbacher <karlmosenbacher@MacBook-Pro-som-tillhor-Karl.local>

---
+++ services/cases-api/src/lambdas/getCaseList.ts
@@ -0,0 +1,91 @@
+import { objectWithoutProperties } from '../libs/objects';
+import { putEvent } from '../libs/awsEventBridge';
+import { wrappers } from '../libs/lambdaWrapper';
+import * as dynamoDb from '../libs/dynamoDb';
+import config from '../libs/config';
+
+import type { CaseItem } from '../types/case';
+
+export type CaseWithOmittedProperties = Omit<CaseItem, 'PK' | 'SK' | 'GS1I' | 'PDF'>;
+
+export interface FunctionResponse {
+  attributes: { cases: CaseWithOmittedProperties[] };
+}
+
+export interface Dependencies {
+  putSuccessEvent: (personalNumber: string) => void;
+  getCases: (personalNumber: string) => Promise<CaseItem[]>;
+}
+
+interface FunctionInput {
+  personalNumber: string;
+}
+
+function putSuccessEvent(personalNumber: string): void {
+  putEvent({ personalNumber }, 'casesApiInvokeSuccess', 'casesApi.getCaseList');
+}
+
+function getUserCoApplicantCaseList(personalNumber: string): Promise<{ Items: CaseItem[] }> {
+  const GSI1 = `USER#${personalNumber}`;
+  const SK = 'CASE#';
+
+  const queryParams = {
+    TableName: config.cases.tableName,
+    IndexName: 'GSI1-SK-index',
+    KeyConditionExpression: 'GSI1 = :gsi1 AND begins_with(SK, :sk)',
+    ExpressionAttributeValues: {
+      ':gsi1': GSI1,
+      ':sk': SK,
+    },
+  };
+
+  return dynamoDb.call('query', queryParams);
+}
+
+function getUserApplicantCaseList(personalNumber: string): Promise<{ Items: CaseItem[] }> {
+  const PK = `USER#${personalNumber}`;
+  const SK = 'CASE#';
+
+  const queryParams = {
+    TableName: config.cases.tableName,
+    KeyConditionExpression: 'PK = :pk AND begins_with(SK, :sk)',
+    ExpressionAttributeValues: {
+      ':pk': PK,
+      ':sk': SK,
+    },
+  };
+
+  return dynamoDb.call('query', queryParams);
+}
+
+async function getUserCaseList(personalNumber: string): Promise<CaseItem[]> {
+  const cases = await Promise.all([
+    (await getUserApplicantCaseList(personalNumber)).Items,
+    (await getUserCoApplicantCaseList(personalNumber)).Items,
+  ]);
+
+  return [...new Set(cases.flat())];
+}
+
+export async function getCaseList(
+  input: FunctionInput,
+  dependencies: Dependencies
+): Promise<FunctionResponse> {
+  dependencies.putSuccessEvent(input.personalNumber);
+
+  const cases = await dependencies.getCases(input.personalNumber);
+  const casesWithoutProperties = cases.map(item =>
+    objectWithoutProperties(item, ['PK', 'SK', 'GSI1', 'PDF'])
+  );
+
+  return {
+    attributes: {
+      cases: casesWithoutProperties as unknown as CaseWithOmittedProperties[],
+    },
+  };
+}
+
+export const main = wrappers.restJSON.wrap(getCaseList, {
+  putSuccessEvent,
+  getCases: getUserCaseList,
+});
---
+++ services/cases-api/test/lambdas/getCaseList.test.ts
@@ -0,0 +1,68 @@
+import { getCaseList } from '../../src/lambdas/getCaseList';
+
+import type {
+  Dependencies,
+  CaseWithOmittedProperties,
+  FunctionResponse,
+} from '../../src/lambdas/getCaseList';
+
+import type { CaseItem } from '../../src/types/case';
+
+const defaultCases = [{ id: '1' }, { id: '2' }] as CaseWithOmittedProperties[];
+const defaultPersonalNumber = '1234567890';
+
+function getDependencies(partialDependencies: Partial<Dependencies> = {}): Dependencies {
+  return {
+    putSuccessEvent: jest.fn(),
+    getCases: () => Promise.resolve(defaultCases as CaseItem[]),
+    ...partialDependencies,
+  };
+}
+
+function createFunctionResponse(cases: CaseWithOmittedProperties[]): FunctionResponse {
+  return {
+    attributes: {
+      cases,
+    },
+  };
+}
+
+it('returns a list of cases', async () => {
+  const getCasesMock = jest.fn().mockResolvedValueOnce(defaultCases);
+
+  const result = await getCaseList(
+    {
+      personalNumber: defaultPersonalNumber,
+    },
+    getDependencies({
+      getCases: getCasesMock,
+    })
+  );
+
+  expect(getCasesMock).toHaveBeenCalledWith(defaultPersonalNumber);
+  expect(result).toEqual(createFunctionResponse(defaultCases));
+});
+
+it('removes "PK", "SK", "GSI1" and "PDF" properties from cases', async () => {
+  const getCasesMock = jest.fn().mockResolvedValueOnce([
+    {
+      id: 'id',
+      PK: '1',
+      SK: '2',
+      GSI1: '3',
+      PDF: '4',
+    },
+  ]);
+
+  const result = await getCaseList(
+    {
+      personalNumber: defaultPersonalNumber,
+    },
+    getDependencies({
+      getCases: getCasesMock,
+    })
+  );
+
+  expect(getCasesMock).toHaveBeenCalledWith(defaultPersonalNumber);
+  expect(result).toEqual(createFunctionResponse([{ id: 'id' }] as CaseWithOmittedProperties[]));
+});
