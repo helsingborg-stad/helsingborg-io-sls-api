b154135586eb0143137de08082dac3258375c35b
MrSpex <mail@dannilsson.se>
2023-01-17 14:19:28 +0100

    test: fix broken tests

--- services/viva/microservice/test/lambdas/createVivaCase.test.ts
+++ services/viva/microservice/test/lambdas/createVivaCase.test.ts
@@ -1,20 +1,45 @@
 import { CASE_PROVIDER_VIVA, VIVA_CASE_CREATED, NOT_STARTED_VIVA } from '../../src/libs/constants';
 import { getStatusByType } from '../../src/libs/caseStatuses';
 
-import { createVivaCase, LambdaRequest, DynamoDbPutParams } from '../../src/lambdas/createVivaCase';
-import { EncryptionType, CasePersonRole, CaseForm } from '../../src/types/caseItem';
-import { VivaPersonsPerson, VivaPersonType } from '../../src/types/vivaMyPages';
+import type { LambdaRequest, DynamoDbPutParams } from '../../src/lambdas/createVivaCase';
+import { createVivaCase } from '../../src/lambdas/createVivaCase';
+import type { CaseItem, CaseForm, CaseUser, CasePerson } from '../../src/types/caseItem';
+import { EncryptionType, CasePersonRole } from '../../src/types/caseItem';
+import type { VivaPersonsPerson } from '../../src/types/vivaMyPages';
+import { VivaPersonType } from '../../src/types/vivaMyPages';
 import { DEFAULT_CURRENT_POSITION } from '../../src/helpers/constants';
+import formRecurring from '../mock/formRecurring.json';
 
 jest.useFakeTimers('modern').setSystemTime(1640995200000);
 
 const mockUuid = '00000000-0000-0000-0000-000000000000';
 jest.mock('uuid', () => ({ v4: () => mockUuid }));
 
-const user = {
-  firstName: 'First',
-  lastName: 'LastName',
+const user: CaseUser = {
   personalNumber: '199402011234',
+  firstName: 'FirstName',
+  lastName: 'LastName',
+  mobilePhone: '0701234567',
+  email: 'someMail@example.com',
+  address: {
+    city: 'KIRUNA',
+    street: 'MANGIGATAN 2',
+    postalCode: '98133',
+  },
+};
+
+const partner: CasePerson = {
+  personalNumber: '197601011234',
+  firstName: 'PartnerFirstName',
+  lastName: 'PartnerLastName',
+  role: CasePersonRole.CoApplicant,
+};
+
+const children: CasePerson = {
+  personalNumber: '200112011234',
+  firstName: 'ChildrenFirstName',
+  lastName: 'ChildrenLastName',
+  role: CasePersonRole.Children,
 };
 
 const readParametersResponse = {
@@ -28,7 +53,7 @@ const readParametersResponse = {
 
 const defaultFormProperties: CaseForm = {
   answers: [],
-  currentPosition: DEFAULT_CURRENT_POSITION,
+  currentPosition: { ...DEFAULT_CURRENT_POSITION },
   encryption: {
     symmetricKeyName: mockUuid,
     encryptionKeyId: mockUuid,
@@ -37,24 +62,92 @@ const defaultFormProperties: CaseForm = {
 };
 
 const partnerFormProperties = {
-  answers: [],
-  currentPosition: DEFAULT_CURRENT_POSITION,
+  answers: [
+    {
+      field: {
+        id: 'housingInfo.streetAddress',
+        tags: [],
+      },
+      value: 'MANGIGATAN 2',
+    },
+    {
+      field: {
+        id: 'housingInfo.postalCode',
+        tags: [],
+      },
+      value: '98133',
+    },
+    {
+      field: {
+        id: 'housingInfo.postalAddress',
+        tags: [],
+      },
+      value: 'KIRUNA',
+    },
+  ],
+  currentPosition: { ...DEFAULT_CURRENT_POSITION },
   encryption: {
-    type: EncryptionType.Decrypted,
-    encryptionKeyId: mockUuid,
     symmetricKeyName: mockUuid,
+    encryptionKeyId: mockUuid,
+    type: EncryptionType.Decrypted,
+  },
+};
+
+const applicantFormProperties = {
+  answers: [
+    {
+      field: {
+        id: 'personalInfo.telephone',
+        tags: ['phonenumber', 'applicant'],
+      },
+      value: '0701234567',
+    },
+    {
+      field: {
+        id: 'personalInfo.email',
+        tags: ['email', 'applicant'],
+      },
+      value: 'someMail@example.com',
+    },
+    {
+      field: {
+        id: 'housingInfo.streetAddress',
+        tags: [],
+      },
+      value: 'MANGIGATAN 2',
+    },
+    {
+      field: {
+        id: 'housingInfo.postalCode',
+        tags: [],
+      },
+      value: '98133',
+    },
+    {
+      field: {
+        id: 'housingInfo.postalAdress',
+        tags: [],
+      },
+      value: 'KIRUNA',
+    },
+  ],
+  currentPosition: { ...DEFAULT_CURRENT_POSITION },
+  encryption: {
+    symmetricKeyName: mockUuid,
+    encryptionKeyId: mockUuid,
+    type: EncryptionType.Decrypted,
   },
 };
 
 function createLambdaInput(persons: VivaPersonsPerson | null = null): LambdaRequest {
   return {
     detail: {
-      clientUser: user,
+      clientUser: { ...user },
       myPages: {
         client: {
-          pnumber: '199402011234',
-          fname: 'First',
-          lname: 'LastName',
+          pnumber: user.personalNumber,
+          fname: user.firstName,
+          lname: user.lastName,
         },
         persons,
       },
@@ -75,6 +168,8 @@ it('successfully creates a recurring application case', async () => {
       id: mockUuid,
       PK: `USER#${user.personalNumber}`,
       SK: `CASE#${mockUuid}`,
+      GSI2PK: 'METRIC#period',
+      GSI2SK: 'YEAR#2022#MONTH#01',
       currentFormId: readParametersResponse.recurringFormId,
       details: {
         period: {
@@ -112,10 +207,91 @@ it('successfully creates a recurring application case', async () => {
 
   const result = await createVivaCase(lambdaInput, {
     createCase: createCaseMock,
-    readParams: () => Promise.resolve(readParametersResponse),
+    getRecurringFormId: () => Promise.resolve(readParametersResponse.recurringFormId),
     getLastUpdatedCase: () => Promise.resolve(undefined),
     getCaseListByPeriod: () => Promise.resolve({ Items: [], Count: 0, ScannedCount: 1 }),
     getFormTemplates: () => Promise.resolve({}),
+    createInitialForms: () =>
+      Promise.resolve({
+        [readParametersResponse.recurringFormId]: defaultFormProperties,
+        [readParametersResponse.randomCheckFormId]: defaultFormProperties,
+        [readParametersResponse.completionFormId]: defaultFormProperties,
+      }),
+  });
+
+  expect(result).toBe(true);
+  expect(createCaseMock).toHaveBeenCalledWith(expectedParameters);
+});
+
+it('successfully creates a prepopulated recurring application case', async () => {
+  const expectedParameters = {
+    TableName: 'cases',
+    Item: {
+      id: mockUuid,
+      PK: `USER#${user.personalNumber}`,
+      SK: `CASE#${mockUuid}`,
+      GSI2PK: 'METRIC#period',
+      GSI2SK: 'YEAR#2022#MONTH#01',
+      currentFormId: readParametersResponse.recurringFormId,
+      details: {
+        period: {
+          startDate: 1640995200000,
+          endDate: 1643587200000,
+        },
+        workflowId: null,
+        completions: null,
+      },
+      forms: {
+        [readParametersResponse.recurringFormId]: applicantFormProperties,
+        [readParametersResponse.randomCheckFormId]: defaultFormProperties,
+        [readParametersResponse.completionFormId]: defaultFormProperties,
+      },
+      persons: [
+        {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          hasSigned: false,
+          personalNumber: user.personalNumber,
+          role: CasePersonRole.Applicant,
+        },
+      ],
+      provider: CASE_PROVIDER_VIVA,
+      state: VIVA_CASE_CREATED,
+      status: getStatusByType(NOT_STARTED_VIVA),
+      updatedAt: 0,
+      createdAt: 1640995200000,
+      expirationTime: 1641038400,
+    },
+  };
+
+  const latestCaseItem = {
+    ...expectedParameters.Item,
+    forms: {
+      [readParametersResponse.recurringFormId]: applicantFormProperties,
+    },
+  } as CaseItem;
+
+  const lambdaInput = createLambdaInput();
+  const createCaseMock = jest.fn();
+
+  const result = await createVivaCase(lambdaInput, {
+    createCase: createCaseMock,
+    getRecurringFormId: () => Promise.resolve(readParametersResponse.recurringFormId),
+    getLastUpdatedCase: () => Promise.resolve(latestCaseItem),
+    getCaseListByPeriod: () =>
+      Promise.resolve({
+        Items: [],
+        Count: 0,
+        ScannedCount: 1,
+      }),
+    getFormTemplates: () =>
+      Promise.resolve({ [readParametersResponse.recurringFormId]: formRecurring }),
+    createInitialForms: () =>
+      Promise.resolve({
+        [readParametersResponse.recurringFormId]: defaultFormProperties,
+        [readParametersResponse.randomCheckFormId]: defaultFormProperties,
+        [readParametersResponse.completionFormId]: defaultFormProperties,
+      }),
   });
 
   expect(result).toBe(true);
@@ -129,7 +305,9 @@ it('successfully creates a recurring application case with partner', async () =>
       id: mockUuid,
       PK: `USER#${user.personalNumber}`,
       SK: `CASE#${mockUuid}`,
-      GSI1: `USER#${user.personalNumber}`,
+      GSI1: `USER#${partner.personalNumber}`,
+      GSI2PK: 'METRIC#period',
+      GSI2SK: 'YEAR#2022#MONTH#01',
       currentFormId: readParametersResponse.recurringFormId,
       details: {
         period: {
@@ -153,11 +331,17 @@ it('successfully creates a recurring application case with partner', async () =>
           role: CasePersonRole.Applicant,
         },
         {
-          firstName: user.firstName,
-          lastName: user.lastName,
+          firstName: partner.firstName,
+          lastName: partner.lastName,
           hasSigned: false,
-          personalNumber: user.personalNumber,
+          personalNumber: partner.personalNumber,
           role: CasePersonRole.CoApplicant,
+        },
+        {
+          firstName: children.firstName,
+          lastName: children.lastName,
+          personalNumber: children.personalNumber,
+          role: CasePersonRole.Children,
         },
       ],
       provider: CASE_PROVIDER_VIVA,
@@ -170,23 +354,41 @@ it('successfully creates a recurring application case with partner', async () =>
   };
 
   const persons: VivaPersonsPerson = {
-    person: {
-      pnumber: user.personalNumber,
-      fname: user.firstName,
-      lname: user.lastName,
-      type: VivaPersonType.Partner,
-    },
+    person: [
+      {
+        pnumber: partner.personalNumber,
+        fname: partner.firstName,
+        lname: partner.lastName,
+        type: VivaPersonType.Partner,
+      },
+      {
+        pnumber: children.personalNumber,
+        fname: children.firstName,
+        lname: children.lastName,
+        type: VivaPersonType.Child,
+      },
+    ],
   };
+
   const lambdaInput = createLambdaInput(persons);
 
   const createCaseMock = jest.fn();
 
   const result = await createVivaCase(lambdaInput, {
     createCase: createCaseMock,
-    readParams: () => Promise.resolve(readParametersResponse),
+    getRecurringFormId: () => Promise.resolve(readParametersResponse.recurringFormId),
     getLastUpdatedCase: () => Promise.resolve(undefined),
     getCaseListByPeriod: () => Promise.resolve({ Items: [], Count: 0, ScannedCount: 1 }),
-    getFormTemplates: () => Promise.resolve({}),
+    getFormTemplates: () =>
+      Promise.resolve({
+        [readParametersResponse.recurringFormId]: partnerFormProperties,
+      }),
+    createInitialForms: () =>
+      Promise.resolve({
+        [readParametersResponse.recurringFormId]: partnerFormProperties,
+        [readParametersResponse.randomCheckFormId]: partnerFormProperties,
+        [readParametersResponse.completionFormId]: partnerFormProperties,
+      }),
   });
 
   expect(result).toBe(true);
@@ -199,10 +401,11 @@ it('stops execution when case exists based on the same Viva application period',
 
   const result = await createVivaCase(lambdaInput, {
     createCase: createCaseMock,
-    readParams: () => Promise.resolve(readParametersResponse),
+    getRecurringFormId: () => Promise.resolve(readParametersResponse.recurringFormId),
     getLastUpdatedCase: () => Promise.resolve(undefined),
     getCaseListByPeriod: () => Promise.resolve({ Items: [], Count: 1, ScannedCount: 1 }),
     getFormTemplates: () => Promise.resolve({}),
+    createInitialForms: () => Promise.resolve({}),
   });
 
   expect(result).toBe(true);
