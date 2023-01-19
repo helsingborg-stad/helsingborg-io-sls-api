819848d9983eb4cb85585cc5542ec0b50bcbbd3a
MrSpex <mail@dannilsson.se>
2023-01-19 22:57:08 +0100

    tests: use constants

--- services/metrics-api/test/helpers/collectors/ekbMetricsCollector.test.ts
+++ services/metrics-api/test/helpers/collectors/ekbMetricsCollector.test.ts
@@ -1,3 +1,4 @@
+import { ACTIVE_ONGOING } from '../../../src/libs/constants';
 import { createCaseMetricValue } from '../../../src/helpers/collectors/ekbMetricsCollector';
 import type { CaseItem } from '../../../src/helpers/query/cases/types';
 
@@ -5,25 +6,27 @@ const cases: CaseItem[] = [
   {
     GSI2PK: 'CREATED#202201',
     status: {
-      type: 'ongoing',
+      type: 'active:ongoing',
     },
   },
   {
     GSI2PK: 'CREATED#202201',
     status: {
-      type: 'ongoing',
+      type: 'active:ongoing:randomCheck',
     },
   },
 ];
 
-it('should return ekb case metric value', async () => {
-  const expected = {
-    value: 2,
-    meta: {
-      status: 'ongoing',
-    },
-  };
+describe('createCaseMetricValue', () => {
+  it(`should return 2 ekb case metric value if status contains: ${ACTIVE_ONGOING}`, async () => {
+    const expected = {
+      value: 2,
+      meta: {
+        status: ACTIVE_ONGOING,
+      },
+    };
 
-  const result = createCaseMetricValue('ongoing', cases);
-  expect(result).toEqual(expected);
+    const result = createCaseMetricValue(ACTIVE_ONGOING, cases);
+    expect(result).toEqual(expected);
+  });
 });
