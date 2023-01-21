b2f0db962cfa73aefebbc6f8d4527aa614fff61b
MrSpex <mail@dannilsson.se>
2023-01-21 19:53:41 +0100

    refactor: implement dynamic creation of cases metrics

--- services/metrics-api/src/helpers/collectors/ekbMetricsCollector.ts
+++ services/metrics-api/src/helpers/collectors/ekbMetricsCollector.ts
@@ -1,41 +1,38 @@
-import { NOT_STARTED, ACTIVE_ONGOING, ACTIVE_SIGNATURE_PENDING } from '../../libs/constants';
-import casesRepository from '../cases';
+import casesRepository, { createStatusCollection } from '../cases';
 import MetricBuilder from '../metricBuilder';
-import { MetricType } from '../metrics.constants';
-import type { Metric, MetricValue } from '../metrics.types';
+import { MetricsHelperText, MetricType } from '../metrics.constants';
+import type { StatusCollection } from '../cases';
+import type { Metric } from '../metrics.types';
 import type { MetricsCollector } from './metricsCollector';
-import type { CaseItem } from '../query/cases/types';
 
 type CaseMeta = {
   status: string;
 };
 
-export function createCaseMetricValue(status: string, cases: CaseItem[]): MetricValue<CaseMeta> {
-  const value: number = cases.reduce(
-    (count, { status: { type } }) => (type.includes(status) ? ++count : count),
-    0
-  );
-  return { value, meta: { status } };
+function createCasesMetrics(collection: StatusCollection) {
+  const metrics = Object.entries(collection).map(([metricName, values]) => {
+    const builder = new MetricBuilder<CaseMeta>(metricName);
+
+    builder.setHelp(MetricsHelperText[metricName]);
+    builder.setType(MetricType.GAUGE);
+
+    Object.entries(values).forEach(([status, value]) => {
+      builder.addValue({ value, meta: { status } });
+    });
+
+    return builder.getMetric();
+  });
+
+  return metrics;
 }
 
-async function createCasesMetrics(cases: CaseItem[]): Promise<Metric<CaseMeta>[]> {
-  const casesMetrics: Metric<CaseMeta> = new MetricBuilder<CaseMeta>('ekb_cases_open_total')
-    .setHelp('Total number of open cases')
-    .setType(MetricType.GAUGE)
-    .addValue(createCaseMetricValue(NOT_STARTED, cases))
-    .addValue(createCaseMetricValue(ACTIVE_ONGOING, cases))
-    .addValue(createCaseMetricValue(ACTIVE_SIGNATURE_PENDING, cases))
-    .getMetric();
-
-  return [casesMetrics];
-}
-
-const ekbMetricsCollector: MetricsCollector<CaseMeta> = {
+const ekb: MetricsCollector<CaseMeta> = {
   async collect(): Promise<Metric<CaseMeta>[]> {
-    const cases: CaseItem[] = await casesRepository.get();
-    const casesMetrics: Metric<CaseMeta>[] = await createCasesMetrics(cases);
-    return [...casesMetrics];
+    const cases = await casesRepository.get();
+    const statusCollection = createStatusCollection(cases);
+    const casesMetrics = createCasesMetrics(statusCollection);
+    return casesMetrics;
   },
 };
 
-export default ekbMetricsCollector;
+export default ekb;
