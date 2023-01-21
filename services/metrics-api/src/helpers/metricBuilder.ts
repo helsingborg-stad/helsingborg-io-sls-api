bdfd413660f7c86790e5a63bd6a36ae303790f63
MrSpex <mail@dannilsson.se>
2023-01-21 19:14:18 +0100

    refactor: use private statement over hash tag
    This is more in line with common OOP syntax

--- services/metrics-api/src/helpers/metricBuilder.ts
+++ services/metrics-api/src/helpers/metricBuilder.ts
@@ -4,48 +4,48 @@ import type { MetricType } from './metrics.constants';
 import type { MaybeMetricMeta, Metric, MetricValue } from './metrics.types';
 
 export default class MetricBuilder<TMeta extends MaybeMetricMeta = void> {
-  #metric: Metric<TMeta>;
+  private metric: Metric<TMeta>;
 
   constructor(name: string) {
-    this.#metric = {
+    this.metric = {
       name,
       values: [],
     };
   }
 
   setHelp(help: string): MetricBuilder<TMeta> {
-    this.#metric.help = help;
+    this.metric.help = help;
     return this;
   }
 
   setType(type: MetricType): MetricBuilder<TMeta> {
-    this.#metric.type = type;
+    this.metric.type = type;
     return this;
   }
 
   addValue(value: MetricValue<TMeta>): MetricBuilder<TMeta> {
     const isMetricMetaless = !isMetricValueWithMeta(value);
-    const hasValuesAlready = this.#metric.values.length > 0;
-    const hasMetalessValues = this.#metric.values.some(value => !isMetricValueWithMeta(value));
+    const hasValuesAlready = this.metric.values.length > 0;
+    const hasMetalessValues = this.metric.values.some(value => !isMetricValueWithMeta(value));
 
     const reachedMetalessLimit = (isMetricMetaless && hasValuesAlready) || hasMetalessValues;
 
     if (reachedMetalessLimit) {
       throw new Error(
-        `Metaless metric cannot contain more than 1 value (metric ${this.#metric.name})`
+        `Metaless metric cannot contain more than 1 value (metric ${this.metric.name})`
       );
     }
 
-    this.#metric.values.push(value);
+    this.metric.values.push(value);
 
     return this;
   }
 
   getMetric(): Metric<TMeta> {
-    if (this.#metric.values.length == 0) {
-      throw new Error(`No values specified for metric ${this.#metric.name}`);
+    if (this.metric.values.length == 0) {
+      throw new Error(`No values specified for metric ${this.metric.name}`);
     }
 
-    return this.#metric;
+    return this.metric;
   }
 }
