2dcee9579e130eb52aad1b14ce2b282e34f91918
MrSpex <mail@dannilsson.se>
2023-03-02 20:43:10 +0100

    refactor(viva-ms): event params, typing

--- services/viva/microservice/src/lambdas/decideCaseStatus.ts
+++ services/viva/microservice/src/lambdas/decideCaseStatus.ts
@@ -1,14 +1,13 @@
 import config from '../libs/config';
-
 import * as dynamoDb from '../libs/dynamoDb';
 import log from '../libs/logs';
 import { getStatusByType } from '../libs/caseStatuses';
-
 import putVivaMsEvent from '../helpers/putVivaMsEvent';
 import { decideNewCaseStatus, decideNewState } from '../helpers/caseDecision';
+import { cases } from '../helpers/query';
+import type { CaseItem, CaseStatus } from '../types/caseItem';
 
-import type { CaseStatus } from '../types/caseItem';
-import type { VivaWorkflow } from '../types/vivaWorkflow';
+type SuccessEvent = LambdaDetail;
 
 interface CaseKeys {
   PK: string;
@@ -17,19 +16,16 @@ interface CaseKeys {
 
 interface LambdaDetail {
   caseKeys: CaseKeys;
-  workflow: VivaWorkflow;
+  caseState: string;
 }
 
 export interface LambdaRequest {
   detail: LambdaDetail;
 }
 
-interface SuccessEvent {
-  caseKeys: CaseKeys;
-}
-
 export interface Dependencies {
-  putSuccessEvent: (params: SuccessEvent) => Promise<void>;
+  getCase: (keys: CaseKeys) => Promise<CaseItem>;
+  triggerEvent: (params: SuccessEvent) => Promise<void>;
   updateCase: (keys: CaseKeys, newStatus: CaseStatus, newState: string) => Promise<void>;
 }
 
@@ -60,27 +56,30 @@ export async function decideCaseStatus(
   input: LambdaRequest,
   dependencies: Dependencies
 ): Promise<boolean> {
-  const { caseKeys, workflow } = input.detail;
+  const { caseKeys, caseState } = input.detail;
 
-  const newStatusType = decideNewCaseStatus(workflow);
-  const newState = decideNewState(workflow);
+  const caseItem = await dependencies.getCase(caseKeys);
+  const newStatusType = decideNewCaseStatus(caseItem.details?.workflow);
+  const newState = decideNewState(caseItem.details?.workflow);
 
   const isStatusStateUndefined = !(newStatusType && newState);
   if (isStatusStateUndefined) {
+    await dependencies.triggerEvent({ caseKeys, caseState });
     return true;
   }
 
   const newStatus = getStatusByType(newStatusType);
 
   await dependencies.updateCase(caseKeys, newStatus, newState);
-  await dependencies.putSuccessEvent({ caseKeys });
+  await dependencies.triggerEvent({ caseKeys, caseState: newState });
 
   return true;
 }
 
 export const main = log.wrap(event => {
   return decideCaseStatus(event, {
-    putSuccessEvent: putVivaMsEvent.decideCaseStatusSuccess,
+    getCase: cases.get,
     updateCase: updateCaseStatusAndState,
+    triggerEvent: putVivaMsEvent.decideCaseStatusSuccess,
   });
 });
