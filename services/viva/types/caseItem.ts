35ea1b4afcd396a0e9c6daabaeb02dedd9d58340
MrSpex <mail@dannilsson.se>
2023-03-02 16:41:05 +0100

    refactor(viva-ms): set params as optional null

--- services/viva/types/caseItem.ts
+++ services/viva/types/caseItem.ts
@@ -5,18 +5,18 @@ export interface CaseUser {
   readonly personalNumber: string;
   readonly firstName: string;
   readonly lastName: string;
-  readonly civilStatus?: string;
-  readonly mobilePhone?: string;
-  readonly email?: string;
-  readonly address?: CaseUserAddress;
-  readonly uuid?: string;
-  readonly createdAt?: number;
+  readonly civilStatus: string | null;
+  readonly mobilePhone: string | null;
+  readonly email: string | null;
+  readonly address: CaseUserAddress;
+  readonly uuid: string;
+  readonly createdAt: number;
 }
 
 export interface CaseUserAddress {
-  readonly city?: string;
-  readonly street?: string;
-  readonly postalCode?: string;
+  readonly city: string | null;
+  readonly street: string | null;
+  readonly postalCode: string | null;
 }
 
 export interface RequestedCaseCompletions {
