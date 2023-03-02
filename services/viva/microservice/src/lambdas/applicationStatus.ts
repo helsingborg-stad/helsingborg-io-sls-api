e8e48b515980eee206c235c613ef624b923c9f33
MrSpex <mail@dannilsson.se>
2023-03-02 17:27:14 +0100

    refactor(viva-ms): add user object to details

--- services/viva/microservice/src/lambdas/applicationStatus.ts
+++ services/viva/microservice/src/lambdas/applicationStatus.ts
@@ -2,18 +2,22 @@ import log from '../libs/logs';
 import putVivaMsEvent from '../helpers/putVivaMsEvent';
 import vivaAdapter from '../helpers/vivaAdapterRequestClient';
 import type { VivaApplicationsStatusItem } from '../types/vivaApplicationsStatus';
-import type { CaseUser } from 'types/caseItem';
+import type { CaseUser } from '../types/caseItem';
 
-interface SuccessEvent {
+export interface SuccessEvent {
   user: CaseUser;
   status: VivaApplicationsStatusItem[];
 }
 
-export interface LambdaRequest {
-  detail: CaseUser;
+export interface LambdaDetail {
+  user: CaseUser;
 }
 
-interface Dependencies {
+export interface LambdaRequest {
+  detail: LambdaDetail;
+}
+
+export interface Dependencies {
   getStatus: (personalNumber: string) => Promise<VivaApplicationsStatusItem[]>;
   triggerEvent: (params: SuccessEvent) => Promise<void>;
 }
@@ -22,7 +26,7 @@ export async function checkApplicationStatus(
   input: LambdaRequest,
   dependencies: Dependencies
 ): Promise<boolean> {
-  const user = input.detail;
+  const { user } = input.detail;
 
   const status = await dependencies.getStatus(user.personalNumber);
 
