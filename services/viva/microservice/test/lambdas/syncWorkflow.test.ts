2c37be7bafc4a21ea353565c84a95c3978fb5639
MrSpex <mail@dannilsson.se>
2023-03-15 18:36:19 +0100

    tests(viva-ms): add missing attributes to user obj

--- services/viva/microservice/test/lambdas/syncWorkflow.test.ts
+++ services/viva/microservice/test/lambdas/syncWorkflow.test.ts
@@ -25,6 +25,18 @@ const input: LambdaRequest = {
   detail: {
     user: {
       personalNumber: '199801011234',
+      firstName: 'MyFirstName',
+      lastName: 'MyLastName',
+      civilStatus: 'MyCivilStatus',
+      mobilePhone: 'MyMobilePhone',
+      email: null,
+      address: {
+        city: 'MyCity',
+        street: 'MyStreet',
+        postalCode: 'MyPostalCode',
+      },
+      uuid: 'MyUuid',
+      createdAt: 123,
     },
   },
 };
