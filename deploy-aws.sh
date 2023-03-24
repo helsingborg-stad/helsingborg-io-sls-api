8bd72165d05fd8e9ccaa4091db8a10fe69260b13
MrSpex <mail@dannilsson.se>
2023-03-24 23:12:30 +0100

    ci: tidy deploy command
    fix: revet .npmrc
    
    fix: remove unrecognized option
    
    ci: up compute type
    
    ci: trying to fix failing build
    
    ci: tweek sls deploy max concurrency setting

--- deploy-aws.sh
+++ deploy-aws.sh
@@ -1,33 +1,12 @@
 #!/usr/bin/env bash
+set -e
 
-# DESCRIPTION
-# Run this script to execute 'yarn install' & 'sls deploy' in dirs with serverless.yml file
+CMD="yarn install"
 
-if [[ ! -d "./services" ]]; then
-    echo "Could not find ./services directory, try running the script again from the project root."
-    exit 1
-fi
-
-echo "Proceed to bulk deploy cloud services n' sh*t to AWS? 🚀 [y/n]: "
-read INPUT_CONFIRMATION
-
-if [[ "$INPUT_CONFIRMATION" != "y" ]]; then
-    echo 'Bye'
-    exit 1
-fi
-
-echo "Run yarn install in each directory? [y/n]: "
-read INPUT_YARN_INSTALL
-
-if [[ "$INPUT_YARN_INSTALL" != "n" ]]; then
-    echo $(yarn install)
-fi
-
-for SERVICE_PATH in $(find . -name "serverless.yml" -not -path "*/node_modules/*" -type f -exec dirname {} \;); do
+$CMD
+for SERVICE_PATH in $(find . -name "package.json" -path "*/services/*" -not -path "*/node_modules/*" -type f -exec dirname {} \;)
+do
     pushd $SERVICE_PATH
-    if [[ -f "$SERVICE_PATH/package.json" && "$INPUT_YARN_INSTALL" != "n" ]]; then
-        yarn install
-    fi
-    sls deploy
+    $CMD
     popd
 done
